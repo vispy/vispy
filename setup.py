bff23de0ca44f2f36f26d524c8ea59e72651b564
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-08-16 17:06:55 +1000

    Fix version import in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@
 from distutils.core import setup
 import sys
 
-from pygly import __version__
+# get pygly's version but don't import it
+# or we'll need our dependencies already installed
+# https://github.com/todddeluca/happybase/commit/63573cdaefe3a2b98ece87e19d9ceb18f00bc0d9
+execfile('pygly/version.py')
 
 os_x_requires = ['pyglet(>=1.2)', 'pyobjc(==2.2)']
 other_requires = ['pyglet']
