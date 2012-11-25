518dec9e74223e4ee897ed3b1379ccd9fa5fa2ed
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-11-25 13:10:59 +1100

    Remove long_description.
    If this isn't set, it will use README.md which is preferable.

--- setup.py
+++ setup.py
@@ -15,9 +15,6 @@ setup(
     name = 'pygly',
     version = __version__,
     description = 'Pyglet based 3D Framework',
-    long_description = """An OpenGL framework designed for flexbility
-        and power. PyGLy provides a number of tools to let you
-        do what you want, how you want.""",
     license = 'BSD',
     author = 'Adam Griffiths',
     author_email = 'adam.lw.griffiths@gmail.com',
