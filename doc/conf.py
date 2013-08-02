a3ec11c440a80e9ec5f732a91236526064cf117a
Almar Klein <almar.klein@gmail.com>
2013-08-02 18:21:47 +0200

    Will doc build pass now?

--- doc/conf.py
+++ doc/conf.py
@@ -13,14 +13,15 @@
 
 import sys, os
 
-# Generate the examples
-import examplesgenerator
-examplesgenerator.main()
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
+
+# Generate the examples
+import examplesgenerator
+examplesgenerator.main()
 
 # -- General configuration -----------------------------------------------------
 
