052f1af80aa4c57901e6f8401aa239f396e82a08
Almar Klein <almar.klein@gmail.com>
2013-08-23 23:44:00 +0200

    Added simplest example ever

---
+++ examples/simplest.py
@@ -0,0 +1,16 @@
+# #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Probably the simplest vispy example
+"""
+
+from vispy import app, gl
+
+c = app.Canvas(show=True)
+
+@c.connect
+def on_paint(event):
+    gl.glClearColor(0.2, 0.4, 0.6, 1.0)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+if __name__ == '__main__':
+    app.run()
