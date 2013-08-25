92da396fbe23a33fdf07d65342d5a21c53d131fa
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2013-08-25 20:53:40 +0200

    Fixed ratio

--- examples/broken-tv.py
+++ examples/broken-tv.py
@@ -1,35 +1,52 @@
-# #!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# This code of this example should be considered public domain.
-
+# -----------------------------------------------------------------------------
+# VisPy - Copyright (c) 2013, Vispy Development Team
+# All rights reserved.
+# -----------------------------------------------------------------------------
 """ 
-Example demonstrating showing a quad. Like hello_quad1.py, but now
-with Texture2D and VertexBuffer, and optionally using an ElementBuffer to
-draw the vertices.
+Example demonstrating showing a, image with a fixed ratio.
 """
 
 import time
 import numpy as np
 
+from transforms import ortho
 from vispy import oogl
 from vispy import app
 from vispy import gl
 
 
-data = np.zeros(4, dtype=[   ('a_position', np.float32, 3), 
-                             ('a_texcoord', np.float32, 2) ])
-data['a_position'] = np.array([ [-1, -1, 0], [1, -1, 0],  
-                                [-1,  1, 0], [1,  1, 0] ])
-data['a_texcoord'] = np.array([ [0, 0], [0, 1], [1, 0], [1, 1] ])
+# Image to be displayed
+W,H = 64,48
+I = np.random.uniform(0,1,(W,H)).astype(np.float32)
+
+# A simple texture quad
+data = np.zeros(4, dtype=[ ('a_position', np.float32, 2), 
+                           ('a_texcoord', np.float32, 2) ])
+data['a_position'] = np.array([[0, 0], [W, 0], [0, H], [W, H]])
+data['a_texcoord'] = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 
 
 VERT_SHADER = """
-attribute vec3 a_position;
+// Uniforms
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_projection;
+uniform float u_antialias;
+
+// Attributes
+attribute vec2 a_position;
 attribute vec2 a_texcoord;
+
+// Varyings
 varying vec2 v_texcoord;
-void main (void) {
+
+// Main
+void main (void)
+{
     v_texcoord = a_texcoord;
-    gl_Position = vec4(a_position,1);
+    gl_Position = u_projection * u_view * u_model * vec4(a_position,0.0,1.0);
 }
 """
 
@@ -49,33 +66,54 @@ class Canvas(app.Canvas):
     
     def __init__(self):
         app.Canvas.__init__(self)
-        
-        self._program = oogl.ShaderProgram( oogl.VertexShader(VERT_SHADER), 
+
+        self.geometry = (0,0,W*5,H*5)
+
+        self.program = oogl.ShaderProgram( oogl.VertexShader(VERT_SHADER), 
                                             oogl.FragmentShader(FRAG_SHADER) )
-        self._vbo = oogl.VertexBuffer(data)
-
-        im = np.random.uniform(0,1,(256,256)).astype(np.float32)
-        self._texture = oogl.Texture2D(im)
-
-        self._program.uniforms['u_texture'] = self._texture
-        self._program.attributes['a_position'] = self._vbo['a_position']
-        self._program.attributes['a_texcoord'] = self._vbo['a_texcoord']
+        self.texture = oogl.Texture2D(I)
+        self.texture.set_filter(gl.GL_NEAREST, gl.GL_NEAREST)
         
+        self.program.uniforms['u_texture'] = self.texture
+        self.program.attributes['a_position'] = data['a_position']
+        self.program.attributes['a_texcoord'] = data['a_texcoord']
+
+        self.view = np.eye(4,dtype=np.float32)
+        self.model = np.eye(4,dtype=np.float32)
+        self.projection = np.eye(4,dtype=np.float32)
+
+        self.program.uniforms['u_model'] = self.model
+        self.program.uniforms['u_view'] = self.view
+        self.projection = ortho(0, W, 0, H, -1, 1)
+        self.program.uniforms['u_projection'] = self.projection
     
     def on_initialize(self, event):
         gl.glClearColor(1,1,1,1)
     
-    
     def on_resize(self, event):
         width, height = event.size
         gl.glViewport(0, 0, width, height)
-    
+        self.projection = ortho( 0, width, 0, height, -100, 100 )
+        self.program.uniforms['u_projection'] = self.projection
+
+        # Compute thje new size of the quad 
+        r = width/float(height)
+        R = W/float(H)
+        if r < R:
+            w,h = width, width/R
+            x,y = 0, int((height-h)/2)
+        else:
+            w,h = height*R, height
+            x,y = int((width-w)/2), 0
+        data['a_position'] = np.array([[x, y], [x+w, y], [x, y+h], [x+w, y+h]])
+        self.program.attributes['a_position'] = data['a_position']
+
     
     def on_paint(self, event):
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-        with self._program as prog:
-            im = np.random.uniform(0,1,(256,256)).astype('float32')
-            self._texture.set_data(im)
+        with self.program as prog:
+            I[...] = np.random.uniform(0,1,(W,H)).astype(np.float32)
+            self.texture.set_data(I)
             prog.draw_arrays(gl.GL_TRIANGLE_STRIP)
         self.update()
 
