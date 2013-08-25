fb6358e56627644db087fb01591c3c0af75ed08d
Almar Klein <almar.klein@gmail.com>
2013-08-25 09:04:41 +0200

    Maybe this fixes search?

--- doc/ext/scriptnamemangler.py
+++ doc/ext/scriptnamemangler.py
@@ -17,11 +17,11 @@ def init():
 def clean(app, *args):
     # Rename doctools.js
     shutil.copy(os.path.join(HTMLDIR, '_static', 'doctools.js'),
-                os.path.join(HTMLDIR, '_static', '_doctools.js'))
+                os.path.join(HTMLDIR, '_static', 'doctools_.js'))
     # Change reference
     search_html = os.path.join(HTMLDIR, 'search.html')
     text = open(search_html, 'rt').read()
-    text = text.replace('/doctools.js', '/_doctools.js')
+    text = text.replace('/doctools.js', '/doctools_.js')
     open(search_html, 'wt').write(text)
 
 
