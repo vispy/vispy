168315a946ef21ab19fb8ba940a0ab1a40d57349
Eric Larson <larson.eric.d@gmail.com>
2014-01-17 10:22:36 -0800

    ENH: Test timer

--- vispy/app/tests/test_app.py
+++ vispy/app/tests/test_app.py
@@ -1,7 +1,8 @@
 import numpy as np
+import time
 from nose.tools import assert_equal, assert_true
 
-from vispy.app import Application, Canvas
+from vispy.app import Application, Canvas, Timer
 from vispy.app.backends import has_qt, has_pyglet
 
 
@@ -20,6 +21,8 @@ def _test_application(backend):
     app.use(backend)
     app.process_events()
     assert_equal(app.backend_name, backend)
+
+    # Canvas
     canvas = Canvas(title='me', app=app, show=True, position=[0, 0, 1, 1])
     assert_true(canvas.app is app)
     assert_true(canvas.native)
@@ -35,6 +38,17 @@ def _test_application(backend):
     canvas.swap_buffers()
     canvas.update()
     canvas.close()
+
+    # Timer
+    timer = Timer(interval=0.001, connect=on_mouse_move, iterations=2,
+                  start=True, app=app)
+    timer.interval = 0.002
+    assert_equal(timer.interval, 0.002)
+    assert_true(timer.running)
+    timer.stop()
+    assert_true(not timer.running)
+    assert_true(timer.native)
+    timer.disconnect()
     app.quit()
 
 
