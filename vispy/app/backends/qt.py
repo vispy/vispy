623d92648a398bc6a1594897ea60f37557109e59
Eric Larson <larson.eric.d@gmail.com>
2014-02-05 12:37:03 -0800

    FIX: Remove user-level Qt backend mod

--- vispy/app/backends/qt.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2014, Vispy Development Team.
-# Distributed under the (new) BSD License. See LICENSE.txt for more info.
-
-"""
-Link to real _qt.py module for backward compatibility.
-"""
-
-from ._qt import *  # noqa
