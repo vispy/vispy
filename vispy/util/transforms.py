vispy/vispy

commit 76a4e924de4ece56658eb24f8d1d3a1636a8274d

WIP: Cleaner style, test transforms

vispy/util/transforms.py
@@ -4,88 +4,102 @@
 Very simple transformation library that is needed for some examples.
 """
 
-import math
-import numpy
 import numpy as np
 
 
 def translate(M, x, y=None, z=None):
-    """
-    translate produces a translation by (x, y, z) . 
-    
+    """Translate by an offset (x, y, z) .
+
     Parameters
     ----------
-    x, y, z
-        Specify the x, y, and z coordinates of a translation vector.
+    M : array
+        Original transformation (4x4).
+    x : float
+        X coordinate of a translation vector.
+    y : float | None
+        Y coordinate of translation vector. If None, `x` will be used.
+    z : float | None
+        Z coordinate of translation vector. If None, `x` will be used.
+
+    Returns
+    -------
+    M : array
+        Updated transformation (4x4). Note that this function operates
+        in-place to modify M.
     """
-    if y is None: y = x
-    if z is None: z = x
-    T = [[ 1, 0, 0, x],
-         [ 0, 1, 0, y],
-         [ 0, 0, 1, z],
-         [ 0, 0, 0, 1]]
-    T = np.array(T, dtype=np.float32).T
-    M[...] = np.dot(M,T)
+    y = x if y is None else y
+    z = x if z is None else z
+    move = np.array([x, y, z], dtype=M.dtype)
+    M[:3, 3] += move
+    return M
 
 
 def scale(M, x, y=None, z=None):
-    """
-    scale produces a non uniform scaling along the x, y, and z axes. The three
-    parameters indicate the desired scale factor along each of the three axes.
+    """Non-uniform scaling along the x, y, and z axes
 
     Parameters
     ----------
-    x, y, z
-        Specify scale factors along the x, y, and z axes, respectively.
+    M : array
+        Original transformation (4x4).
+    x : float
+        X coordinate of a translation vector.
+    y : float | None
+        Y coordinate of translation vector. If None, `x` will be used.
+    z : float | None
+        Z coordinate of translation vector. If None, `x` will be used.
+
+    Returns
+    -------
+    M : array
+        Updated transformation (4x4). Note that this function operates
+        in-place to modify M.
     """
-    if y is None: y = x
-    if z is None: z = x
-    S = [[ x, 0, 0, 0],
-         [ 0, y, 0, 0],
-         [ 0, 0, z, 0],
-         [ 0, 0, 0, 1]]
-    S = np.array(S,dtype=np.float32).T
-    M[...] = np.dot(M,S)
+    y = x if y is None else y
+    z = x if z is None else z
+    M *= np.array([x, y, z, 1], dtype=M.dtype)[np.newaxis, :]
+    return M
 
 
 def xrotate(M,theta):
-    t = math.pi*theta/180
-    cosT = math.cos( t )
-    sinT = math.sin( t )
-    R = numpy.array(
-        [[ 1.0,  0.0,  0.0, 0.0 ],
-         [ 0.0, cosT,-sinT, 0.0 ],
-         [ 0.0, sinT, cosT, 0.0 ],
-         [ 0.0,  0.0,  0.0, 1.0 ]], dtype=np.float32)
-    M[...] = np.dot(M,R)
+    t = np.pi * theta / 180.
+    cosT = np.cos(t)
+    sinT = np.sin(t)
+    R = np.array([[ 1.0,  0.0,  0.0, 0.0 ],
+                  [ 0.0, cosT,-sinT, 0.0 ],
+                  [ 0.0, sinT, cosT, 0.0 ],
+                  [ 0.0,  0.0,  0.0, 1.0 ]], dtype=M.dtype)
+    M[...] = np.dot(M, R)
+    return M
+
 
 def yrotate(M,theta):
-    t = math.pi*theta/180
-    cosT = math.cos( t )
-    sinT = math.sin( t )
-    R = numpy.array(
+    t = np.pi*theta/180
+    cosT = np.cos( t )
+    sinT = np.sin( t )
+    R = np.array(
         [[ cosT,  0.0, sinT, 0.0 ],
          [ 0.0,   1.0,  0.0, 0.0 ],
          [-sinT,  0.0, cosT, 0.0 ],
-         [ 0.0,  0.0,  0.0, 1.0 ]], dtype=np.float32)
+         [ 0.0,  0.0,  0.0, 1.0 ]], dtype=M.dtype)
     M[...] = np.dot(M,R)
 
+
 def zrotate(M,theta):
-    t = math.pi*theta/180
-    cosT = math.cos( t )
-    sinT = math.sin( t )
-    R = numpy.array(
+    t = np.pi*theta/180
+    cosT = np.cos( t )
+    sinT = np.sin( t )
+    R = np.array(
         [[ cosT,-sinT, 0.0, 0.0 ],
          [ sinT, cosT, 0.0, 0.0 ],
          [ 0.0,  0.0,  1.0, 0.0 ],
-         [ 0.0,  0.0,  0.0, 1.0 ]], dtype=np.float32)
+         [ 0.0,  0.0,  0.0, 1.0 ]], dtype=M.dtype)
     M[...] = np.dot(M,R)
 
 
 def rotate(M, angle, x, y, z, point=None):
     """
     rotate produces a rotation of angle degrees around the vector (x, y, z).
-    
+
     Parameters
     ----------
     M
@@ -97,17 +111,17 @@ def rotate(M, angle, x, y, z, point=None):
     x, y, z
         Specify the x, y, and z coordinates of a vector, respectively.
     """
-    angle = math.pi*angle/180
-    c,s = math.cos(angle), math.sin(angle)
-    n = math.sqrt(x*x+y*y+z*z)
+    angle = np.pi*angle/180
+    c,s = np.cos(angle), np.sin(angle)
+    n = np.sqrt(x*x+y*y+z*z)
     x /= n
     y /= n
     z /= n
     cx,cy,cz = (1-c)*x, (1-c)*y, (1-c)*z
-    R = numpy.array([[ cx*x + c  , cy*x - z*s, cz*x + y*s, 0],
+    R = np.array([[ cx*x + c  , cy*x - z*s, cz*x + y*s, 0],
                      [ cx*y + z*s, cy*y + c  , cz*y - x*s, 0],
                      [ cx*z - y*s, cy*z + x*s, cz*z + c,   0],
-                     [          0,          0,        0,   1]]).T
+                     [          0,          0,        0,   1]], dtype=M.dtype).T
     M[...] = np.dot(M,R)
 
 
@@ -115,7 +129,7 @@ def ortho( left, right, bottom, top, znear, zfar ):
     assert( right  != left )
     assert( bottom != top  )
     assert( znear  != zfar )
-    
+
     M = np.zeros((4,4), dtype=np.float32)
     M[0,0] = +2.0/(right-left)
     M[3,0] = -(right+left)/float(right-left)
@@ -125,7 +139,7 @@ def ortho( left, right, bottom, top, znear, zfar ):
     M[3,2] = -(zfar+znear)/float(zfar-znear)
     M[3,3] = 1.0
     return M
-        
+
 def frustum( left, right, bottom, top, znear, zfar ):
     assert( right  != left )
     assert( bottom != top  )

vispy/app/tests/test_app.py
@@ -7,20 +7,23 @@ from vispy.app.backends import has_qt, has_pyglet
 
 requires_qt = np.testing.dec.skipif(not has_qt(), 'Requires QT')
 requires_pyglet = np.testing.dec.skipif(not has_pyglet(), 'Requires QT-UIC')
+bad_glut = np.testing.dec.skipif(True, 'GLUT window causes segfaults on 2.7 '
+                                 'and fails on 2.6')  # XXX should fix
 
 
 def _test_application(backend):
     """Test application running"""
     app = Application()
     app.use(backend)
+    app.process_events()
     assert_equal(app.backend_name, backend)
     canvas = Canvas(app=app)
     canvas.show()
     canvas.close()
+    app.quit()
 
 
-@np.testing.dec.skipif(True, 'GLUT window causes segfaults on 2.7 and '
-                       'fails on 2.6')
+@bad_glut
 def test_glut():
     """Test GLUT application"""
     _test_application('Glut')

vispy/util/tests/test_transforms.py
@@ -1,0 +1,14 @@
+import numpy as np
+from nose.tools import assert_equal
+from numpy.testing import assert_allclose
+
+from vispy.util.transforms import (translate, scale, xrotate, yrotate,
+                                   zrotate, rotate, ortho, frustum,
+                                   perspective)
+
+
+def test_transforms():
+    """Test basic transforms"""
+    xfm = np.random.randn(4, 4)
+    new_xfm = xrotate(xrotate(xfm, 90), -90)
+    assert_allclose(xfm, new_xfm)

commit b6be5f157bd719fdcdb340b1adeaa03ab482cf07

Fixing transforms.py
Apparently there was an error introduced in commit
https://github.com/vispy/vispy/commit/76a4e924de4ece56658eb24f8d1d3a1636a8274d

vispy/util/transforms.py
@@ -29,8 +29,11 @@ def translate(M, x, y=None, z=None):
     """
     y = x if y is None else y
     z = x if z is None else z
-    move = np.array([x, y, z, 1], dtype=M.dtype)
-    M[:, 3] = np.dot(M, move)
+    T = np.array([[ 1.0, 0.0, 0.0, x],
+                  [ 0.0, 1.0, 0.0, y],
+                  [ 0.0, 0.0, 1.0, z],
+                  [ 0.0, 0.0, 0.0, 1.0]], dtype=M.dtype).T
+    M[...] = np.dot(M, T)
     return M
 
 
@@ -56,7 +59,11 @@ def scale(M, x, y=None, z=None):
     """
     y = x if y is None else y
     z = x if z is None else z
-    M *= np.array([x, y, z, 1], dtype=M.dtype)[np.newaxis, :]
+    S = np.array([[ x, 0.0, 0.0, 0.0],
+                  [ 0.0, y, 0.0, 0.0],
+                  [ 0.0, 0.0, z, 0.0],
+                  [ 0.0, 0.0, 0.0, 1.0]], dtype=M.dtype).T
+    M[...] = np.dot(M, S)
     return M
 
 