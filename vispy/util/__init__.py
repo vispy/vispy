320ab4a4da6b048b754a05de01c1dc399a2a546d
Eric Larson <larson.eric.d@gmail.com>
2014-02-10 13:44:21 -0800

    FIX: Newline

--- vispy/util/__init__.py
+++ vispy/util/__init__.py
@@ -10,4 +10,3 @@ from .misc import (_TempDir, is_string, parse_command_line_arguments,  # noqa
                    config)  # noqa
 from ._logging import logger, set_log_level, use_log_level  # noqa
 from . import keys  # noqa
-
