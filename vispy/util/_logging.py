07e27789b8a4c731c5fa803797f373735bf45e02
Eric Larson <larson.eric.d@gmail.com>
2014-02-10 17:52:31 -0800

    ENH: Better logging

--- vispy/util/_logging.py
+++ vispy/util/_logging.py
@@ -4,11 +4,26 @@
 
 import logging
 import sys
+import inspect
+import re
 
 from .six import string_types
 
 ###############################################################################
 # LOGGING (some adapted from mne-python)
+
+
+def _get_vispy_caller():
+    records = inspect.stack()
+    # first few records are vispy-based logging calls
+    for record in records[5:]:
+        module = record[0].f_globals['__name__']
+        if module.startswith('vispy'):
+            line = str(record[0].f_lineno)
+            func = record[3]
+            caller = "{:s}:{:s}({:s}): ".format(module, func, line)
+            return caller
+    return 'unknown'
 
 
 class _WrapStdOut(object):
@@ -19,10 +34,70 @@ class _WrapStdOut(object):
         return getattr(sys.stdout, name)
 
 
+class _VispyFormatter(logging.Formatter):
+    def __init__(self):
+        logging.Formatter.__init__(self, '%(levelname)s: %(message)s')
+
+    def format(self, record):
+        out = logging.Formatter.format(self, record)
+        if logger.level <= logging.DEBUG:
+            prepend = _get_vispy_caller()
+        else:
+            prepend = ''
+        return prepend + out
+
+
+class _VispyStreamHandler(logging.StreamHandler):
+    """Stream handler allowing matching and recording
+
+    Recording is only done when ``match`` is not None.
+    """
+    def __init__(self):
+        logging.StreamHandler.__init__(self, _WrapStdOut())
+        self._vispy_formatter = _VispyFormatter()
+        self.setFormatter(self._vispy_formatter)
+        self._vispy_match = None
+        self._vispy_emit_list = list()
+        self._vispy_set_emit_record(False)
+        self._vispy_set_match(None)
+
+    def _vispy_emit_match(self, record):
+        test = record.getMessage()
+        match = self._vispy_match
+        if match is None or len(re.findall(match, test)) > 0:
+            if self._vispy_emit_record:
+                fmt_rec = self._vispy_formatter.format(record)
+                self._vispy_emit_list.append(fmt_rec)
+            return logging.StreamHandler.emit(self, record)
+
+    def _vispy_emit(self, record):
+        return logging.StreamHandler.emit(self, record)
+
+    def _vispy_set_match(self, match):
+        old_match = self._vispy_match
+        self._vispy_match = match
+        # Triage here to avoid a bunch of if's later (more efficient)
+        if match is not None or self._vispy_emit_record:
+            self.emit = self._vispy_emit_match
+        else:
+            self.emit = self._vispy_emit
+        return old_match
+
+    def _vispy_set_emit_record(self, record):
+        self._vispy_emit_record = record
+        match = self._vispy_match
+        # Triage here to avoid a bunch of if's later (more efficient)
+        if match is not None or self._vispy_emit_record:
+            self.emit = self._vispy_emit_match
+        else:
+            self.emit = self._vispy_emit
+
+    def _vispy_reset_list(self):
+        self._vispy_emit_list = list()
+
+
 logger = logging.getLogger('vispy')
-_output_format = '%(message)s'
-_lh = logging.StreamHandler(_WrapStdOut())
-_lh.setFormatter(logging.Formatter(_output_format))
+_lh = _VispyStreamHandler()
 logger.addHandler(_lh)
 
 
@@ -31,7 +106,7 @@ logging_types = dict(debug=logging.DEBUG, info=logging.INFO,
                      critical=logging.CRITICAL)
 
 
-def set_log_level(verbose, return_old_level=False):
+def set_log_level(verbose, match=None, return_old=False):
     """Convenience function for setting the logging level
 
     Parameters
@@ -41,6 +116,10 @@ def set_log_level(verbose, return_old_level=False):
         INFO, WARNING, ERROR, or CRITICAL. Note that these are for
         convenience and are equivalent to passing in logging.DEBUG, etc.
         For bool, True is the same as 'INFO', False is the same as 'WARNING'.
+    match : str | None
+        String to match. Only those messages that both contain a substring
+        that regexp matches ``'match'`` (and the ``verbose`` level) will be
+        displayed.
     return_old_level : bool
         If True, return the old verbosity level.
     """
@@ -55,8 +134,12 @@ def set_log_level(verbose, return_old_level=False):
         raise TypeError('verbose must be a bool or string')
     logger = logging.getLogger('vispy')
     old_verbose = logger.level
+    old_match = _lh._vispy_set_match(match)
     logger.setLevel(verbose)
-    return (old_verbose if return_old_level else None)
+    out = None
+    if return_old:
+        out = (old_verbose, old_match)
+    return out
 
 
 class use_log_level(object):
@@ -67,16 +150,29 @@ class use_log_level(object):
     level : str
         See ``set_log_level`` for options
     """
-    def __init__(self, level):
+    def __init__(self, level, match=None, record=False):
         self._new_level = level
+        self._new_match = match
+        self._record = record
+        if match is not None and not isinstance(match, string_types):
+            raise TypeError('match must be None or str')
 
     def __enter__(self):
-        old_level = set_log_level(self._new_level, return_old_level=True)
+        old_level, old_match = set_log_level(self._new_level,
+                                             self._new_match, return_old=True)
         for key, value in logging_types.items():
             if value == old_level:
                 old_level = key
         self._old_level = old_level
-        return self
+        self._old_match = old_match
+        _lh._vispy_reset_list()
+        if self._record:
+            _lh._vispy_set_emit_record(True)
+            return _lh._vispy_emit_list
+        else:
+            return list()
 
     def __exit__(self, type, value, traceback):
-        set_log_level(self._old_level)
+        set_log_level(self._old_level, self._old_match)
+        if self._record:
+            _lh._vispy_set_emit_record(False)
--- vispy/util/tests/test_logging.py
+++ vispy/util/tests/test_logging.py
@@ -1,6 +1,7 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_true
 import logging
 
+from vispy import app
 from vispy.util import logger, use_log_level
 
 
@@ -13,3 +14,29 @@ def test_logging():
     with use_log_level('debug'):
         assert_equal(logger.level, logging.DEBUG)
     assert_equal(logger.level, ll)
+
+
+def test_debug_logging():
+    """Test advanced debugging logging"""
+    with use_log_level('debug', 'Selected', True) as l:
+        a = app.Application()
+        a.use()
+    assert_equal(len(l), 1)
+    assert_true('vispy.app.application' in l[0])
+
+    with use_log_level('debug', record=True) as l:
+        a = app.Application()
+        a.use()
+    assert_equal(len(l), 1)
+    assert_true('vispy.app.application' in l[0])
+
+    with use_log_level('debug', 'foo', True) as l:
+        a = app.Application()
+        a.use()
+    assert_equal(len(l), 0)
+
+    with use_log_level('info', record=True) as l:
+        a = app.Application()
+        a.use()
+    assert_equal(len(l), 1)
+    assert_true('vispy.app.application' not in l[0])
