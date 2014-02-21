bf5b95ad825a5df6be48d9f293837dcd4cbe3101
Kieffer Jerome <kieffer@Kieffers-MacBook-Pro.local>
2014-02-21 18:16:10 +0100

    working but not yet perfect

--- vispy/openCL/test/openCL_context.py
+++ vispy/openCL/test/openCL_context.py
@@ -7,20 +7,11 @@
 # -----------------------------------------------------------------------------
 from vispy import app
 from vispy import gloo
-# from vispy.gloo import gl
 from vispy import openCL
 import numpy
+import time
 import pyopencl, pyopencl.array
-N = 1024
-
-
-#data = np.zeros(N, np.float32)
-#vbo = gloo.VertexBuffer(data)
-
-# Question: How do I actually send this to the GPU ?
-#vbo.activate()
-#print(dir(vbo))
-#print(vbo._handle, type(vbo._handle))
+N = 2048
 
 
 src = """
@@ -37,18 +28,35 @@ __kernel void buf_to_tex( global float *ary,
   if ((x>=width)||(y>=height)) return;
   write_imagef( texture, (int2)(x,y), data);
 }
-"""
+    __kernel void
+    u16_to_float(__global unsigned short  *array_int,
+    __global float *array_float,
+    const int IMAGE_W,
+    const int IMAGE_H
+    )
+    {
+    //Global memory guard for padding
+    if ((get_global_id(0)<IMAGE_W) && (get_global_id(1) < IMAGE_H)){
+    int i = get_global_id(0) + IMAGE_W * get_global_id(1);
+    array_float[i]=(float)array_int[i];
+    }
+    }//end kernel
+    """
+#//uniform sampler1D u_colormap;
 
 fragment = """ // texture fragment shader
 uniform sampler2D u_texture1;
-uniform sampler1D u_colormap;
 varying vec2 v_texcoord;
 void main()
 {
     float clr1 = texture2D(u_texture1, v_texcoord).r;
-    vec4 color = vec4(texture1d(u_colormap, value).rgb,1.0);
-    gl_FragColor = color;
-    //vec4(vec3(clr1),1.0);
+    //vec4 color = vec4(texture1d(u_colormap, clr1).rgb,1.0);
+    //gl_FragColor = color;
+    gl_FragColor = vec4(vec3(clr1),1.0);
+//    if (clr1>0.5)
+//        gl_FragColor = vec4(vec3(1.0),1.0);
+//    else
+//        gl_FragColor = vec4(vec3(0.0),1.0);
 }
 """
 vertex = """
@@ -68,11 +76,14 @@ class Canvas(app.Canvas):
         app.Canvas.__init__(self)
         self.tex_size = tex_size
         self.gl_program = None
+        self.ocl_prg = None
+        self.last_start = 0.0
+    
     
     def init_gl(self):
         # Create program
         self.gl_program = gloo.Program(vertex, fragment)
-        self.gl_tex =  gloo.Texture2D(numpy.zeros((self.tex_size,self.tex_size),dtype=numpy.float32))
+        self.gl_tex =  gloo.Texture2D(numpy.zeros((self.tex_size,self.tex_size),dtype=numpy.float32)+0.5)
         self.gl_tex.activate()
         print("activated")
         # Set uniforms and samplers
@@ -83,17 +94,6 @@ class Canvas(app.Canvas):
         self.gl_program['u_texture1'] = self.gl_tex
         self.gl_program['position'] = gloo.VertexBuffer(positions)
         self.gl_program['texcoord'] = gloo.VertexBuffer(texcoords)
-        colormap = numpy.empty((512,3),dtype=numpy.float32)
-        colormap[:,0] = numpy.interp(numpy.arange(512),
-                              [0, 171, 342, 512],
-                              [0,1,1,1])
-        colormap[:,1] = numpy.interp(numpy.arange(512),
-                              [0, 171, 342, 512],
-                              [0,0,1,1])
-        colormap[:,2] = numpy.interp(numpy.arange(512),
-                              [0, 171, 342, 512],
-                              [0,0,0,1])
-        self.gl_program['u_colormap'] = colormap
     
     def on_initialize(self, event):
         gloo.gl.glClearColor(1, 1, 1, 1)
@@ -112,46 +112,53 @@ class Canvas(app.Canvas):
             self.gl_program.draw(gloo.gl.GL_TRIANGLE_STRIP)
         self.swap_buffers()
 
-    def init_openCL(self):
-        
-#canvas = app.Canvas(size=(512, 512), title="VisPy - OpenCL")
-#canvas.show()
 
+    def init_openCL(self):
         self.gl_program.draw(gloo.gl.GL_TRIANGLE_STRIP)
-#vbo = gloo.VertexBuffer(
         self.ocl_ctx = openCL.get_OCL_context()
         print("OpenCL context on device: %s" % self.ocl_ctx.devices[0])
         self.queue = pyopencl.CommandQueue(self.ocl_ctx)
         self.ocl_prg = pyopencl.Program(self.ocl_ctx, src).build()
-#print(ctx)
-
-        img = numpy.random.randint(0, 65000, N*N).astype(numpy.float32).reshape((N,N))
-        self.ocl_ary = pyopencl.array.to_device(self.queue, img)
-        #print ary
-        #mf = pyopencl.mem_flags
-        #fmt = pyopencl.ImageFormat(pyopencl.channel_order.INTENSITY, pyopencl.channel_type.FLOAT)
-        #img2d = pyopencl.Image(ctx, mf.WRITE_ONLY, fmt, shape=(N,N), buffer=ary.data)
-        #img2d = pyopencl.image_from_array(ctx, img, num_channels=None, mode="w", norm_int=True)
-        #print img2d
+        self.ary_float = pyopencl.array.empty(self.queue, shape=(self.tex_size,self.tex_size),dtype=numpy.float32)
+    
         self.ocl_tex = pyopencl.GLTexture(self.ocl_ctx, pyopencl.mem_flags.READ_WRITE,
                         gloo.gl.GL_TEXTURE_2D, 0, int(self.gl_tex.handle), 2)
-        #print clTex
-        #pyopencl.enqueue_acquire_gl_objects(queue, [clTex], wait_for=True)
-        e=self.ocl_prg.buf_to_tex(self.queue, (N,N), (8,8),
-                                self.ocl_ary.data, numpy.int32(N), numpy.int32(N),
-                                numpy.float32(0.0), numpy.float32(65500), self.ocl_tex)
-        e.wait()
 
-        #clTex = pyopencl.GLTexture(ctx,img2d, dims=2)
-        #pyopencl.GLBuffer(ctx, pyopencl.mem_flags.READ_WRITE, int(vbo._handle))
-        self.gl_program.draw(gloo.gl.GL_TRIANGLE_STRIP)
-
+        img = numpy.random.randint(0,65000,self.tex_size**2).reshape((self.tex_size,self.tex_size)).astype(numpy.uint16)
+        self.ocl_ary = pyopencl.array.to_device(self.queue, img)
+    
+        self.new_image(img)
+    
+    def new_image(self, img):
+        if self.ocl_prg is None:
+            self.init_openCL()
+        self.ocl_ary.set(img)
+        pyopencl.enqueue_acquire_gl_objects(self.queue, [self.ocl_tex])
+        self.ocl_prg.u16_to_float(self.queue, (self.tex_size,self.tex_size), (8,8),
+                                self.ocl_ary.data,
+                                self.ary_float.data,
+                                numpy.int32(self.tex_size),
+                                numpy.int32(self.tex_size))
+        self.ocl_prg.buf_to_tex(self.queue, (self.tex_size,self.tex_size), (8,8),
+                                  self.ocl_ary.data, numpy.int32(self.tex_size), numpy.int32(self.tex_size),
+                                  numpy.float32(0.0), numpy.float32(65000.0), self.ocl_tex)
+        pyopencl.enqueue_release_gl_objects(self.queue, [self.ocl_tex]).wait()
+        t = time.time()
+        print("pyopencl program called fps= %.3fs"%(1.0/(t-self.last_start)))
+        self.last_start = t
+        self.on_paint(None)
 
 
 if __name__ == '__main__':
+    img1 = numpy.random.randint(0,65000,N**2).reshape((N,N)).astype(numpy.uint16)
+    img2 = numpy.random.randint(0,65000,N**2).reshape((N,N)).astype(numpy.uint16)
     c = Canvas(N)
+    c.measure_fps(1)
     c.show()
     c.init_gl()
+    c.show()
     c.init_openCL()
-    print c.ocl_ary.get()
+    while True:
+        c.new_image(img1)
+        c.new_image(img2)
     app.run()
