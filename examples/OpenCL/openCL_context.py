3d2a3511f9abc73bb6de2617853101ac9ea4df25
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-02-24 14:55:43 +0100

    move demo

--- examples/OpenCL/openCL_context.py
+++ examples/OpenCL/openCL_context.py
@@ -10,6 +10,7 @@ from vispy import gloo
 from vispy import openCL
 import numpy
 import time
+import threading
 import pyopencl, pyopencl.array
 N = 2048
 
@@ -78,12 +79,12 @@ class Canvas(app.Canvas):
         self.gl_program = None
         self.ocl_prg = None
 #        self.last_start = 0.0
-    
-    
+
+
     def init_gl(self):
         # Create program
         self.gl_program = gloo.Program(vertex, fragment)
-        self.gl_tex =  gloo.Texture2D(numpy.zeros((self.tex_size,self.tex_size),dtype=numpy.float32)+0.5)
+        self.gl_tex = gloo.Texture2D(numpy.zeros((self.tex_size, self.tex_size), dtype=numpy.float32) + 0.5)
         self.gl_tex.activate()
         print("activated")
         # Set uniforms and samplers
@@ -94,7 +95,7 @@ class Canvas(app.Canvas):
         self.gl_program['u_texture1'] = self.gl_tex
         self.gl_program['position'] = gloo.VertexBuffer(positions)
         self.gl_program['texcoord'] = gloo.VertexBuffer(texcoords)
-    
+
     def on_initialize(self, event):
         gloo.gl.glClearColor(1, 1, 1, 1)
 
@@ -115,34 +116,35 @@ class Canvas(app.Canvas):
 
     def init_openCL(self):
         self.gl_program.draw(gloo.gl.GL_TRIANGLE_STRIP)
-        import pyopencl
+#        import pyopencl
 #        self.ocl_ctx = pyopencl.Context(devices=[(pyopencl.get_platforms()[0]).get_devices()[1]],
-#                                        properties=pyopencl.tools.get_gl_sharing_context_properties())
+#                                        properties=pyopencl.tools.get_gl_sharing_context_properties() +
+#                                        [(pyopencl.context_properties.PLATFORM, pyopencl.get_platforms()[0])])
         self.ocl_ctx = openCL.get_OCL_context()
         print("OpenCL context on device: %s" % self.ocl_ctx.devices[0])
         self.queue = pyopencl.CommandQueue(self.ocl_ctx)
         self.ocl_prg = pyopencl.Program(self.ocl_ctx, src).build()
-        self.ary_float = pyopencl.array.empty(self.queue, shape=(self.tex_size,self.tex_size),dtype=numpy.float32)
-    
+        self.ary_float = pyopencl.array.empty(self.queue, shape=(self.tex_size, self.tex_size), dtype=numpy.float32)
+
         self.ocl_tex = pyopencl.GLTexture(self.ocl_ctx, pyopencl.mem_flags.READ_WRITE,
                         gloo.gl.GL_TEXTURE_2D, 0, int(self.gl_tex.handle), 2)
 
-        img = numpy.random.randint(0,65000,self.tex_size**2).reshape((self.tex_size,self.tex_size)).astype(numpy.uint16)
+        img = numpy.random.randint(0, 65000, self.tex_size ** 2).reshape((self.tex_size, self.tex_size)).astype(numpy.uint16)
         self.ocl_ary = pyopencl.array.to_device(self.queue, img)
-    
+
         self.new_image(img)
-    
+
     def new_image(self, img):
         if self.ocl_prg is None:
             self.init_openCL()
         self.ocl_ary.set(img)
         pyopencl.enqueue_acquire_gl_objects(self.queue, [self.ocl_tex])
-        self.ocl_prg.u16_to_float(self.queue, (self.tex_size,self.tex_size), (8,8),
+        self.ocl_prg.u16_to_float(self.queue, (self.tex_size, self.tex_size), (8, 8),
                                 self.ocl_ary.data,
                                 self.ary_float.data,
                                 numpy.int32(self.tex_size),
                                 numpy.int32(self.tex_size))
-        self.ocl_prg.buf_to_tex(self.queue, (self.tex_size,self.tex_size), (8,8),
+        self.ocl_prg.buf_to_tex(self.queue, (self.tex_size, self.tex_size), (8, 8),
                                   self.ary_float.data, numpy.int32(self.tex_size), numpy.int32(self.tex_size),
                                   numpy.float32(0.0), numpy.float32(65000.0), self.ocl_tex)
         pyopencl.enqueue_release_gl_objects(self.queue, [self.ocl_tex]).wait()
@@ -151,20 +153,28 @@ class Canvas(app.Canvas):
 #        self.last_start = t
         self.on_paint(None)
 
+    def benchmark(self, number=10):
+        """
+        This is run after a while ... to let the application initialize
+        """
+        imgs = [numpy.random.randint(0, 65000, N ** 2).reshape((N, N)).astype(numpy.uint16) for i in range(number) ]
+        print("Starting benchmark")
+        t1 = time.time()
+        while True:
+            t0 = t1
+            for i in imgs:
+                c.new_image(i)
+            t1 = time.time()
+            print("Rendering at %4.2f fps (average of %i)" % (number / (t1 - t0), number))
 
 if __name__ == '__main__':
-    imgs = [numpy.random.randint(0, 65000, N ** 2).reshape((N, N)).astype(numpy.uint16) for i in range(10) ]
+
     c = Canvas(N)
 #    c.measure_fps(1)
     c.show()
     c.init_gl()
     c.show()
     c.init_openCL()
-    t1 = time.time()
-    while True:
-        t0 = t1
-        for i in imgs:
-            c.new_image(i)
-        t1 = time.time()
-        print("Rendering at %4.2f fps (average of %i)" % (len(imgs) / (t1 - t0), len(imgs)))
+    print("Starting benchmark in 1 seconds ...")
+    threading.Timer(1.0, c.benchmark).start()
     app.run()
