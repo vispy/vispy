ae81094d35dbe8ea73768deceaa4a23137a7d846
Luke Campagnola <luke.campagnola@gmail.com>
2014-03-17 10:17:58 -0400

    Flakify.

--- vispy/gloo/util.py
+++ vispy/gloo/util.py
@@ -27,22 +27,24 @@ def _screenshot(viewport=None):
     return im
 
 
-KEYWORDS = set(['active', 'asm', 'cast', 'class', 'common', 'default', 'double', 
-            'dvec2', 'dvec3', 'dvec4', 'enum', 'extern', 'external', 'filter', 
-            'fixed', 'flat', 'fvec2', 'fvec3', 'fvec4', 'goto', 'half', 'hvec2', 
-            'hvec3', 'hvec4', 'iimage1D', 'iimage1DArray', 'iimage2D', 
-            'iimage2DArray', 'iimage3D', 'iimageBuffer', 'iimageCube', 
-            'image1D', 'image1DArray', 'image1DArrayShadow', 'image1DShadow', 
-            'image2D', 'image2DArray', 'image2DArrayShadow', 'image2DShadow', 
-            'image3D', 'imageBuffer', 'imageCube', 'inline', 'input', 
-            'interface', 'long', 'namespace', 'noinline', 'output', 'packed', 
-            'partition', 'public', 'row_major', 'sampler1D', 'sampler1DShadow', 
-            'sampler2DRect', 'sampler2DRectShadow', 'sampler2DShadow', 
-            'sampler3D', 'sampler3DRect', 'short', 'sizeof', 'static', 'superp',
-            'switch', 'template', 'this', 'typedef', 'uimage1D', 
-            'uimage1DArray', 'uimage2D', 'uimage2DArray', 'uimage3D', 
-            'uimageBuffer', 'uimageCube', 'union', 'unsigned', 'using', 
-            'volatile'])
+KEYWORDS = set(['active', 'asm', 'cast', 'class', 'common', 'default',
+                'double', 'dvec2', 'dvec3', 'dvec4', 'enum', 'extern',
+                'external', 'filter', 'fixed', 'flat', 'fvec2', 'fvec3',
+                'fvec4', 'goto', 'half', 'hvec2', 'hvec3', 'hvec4', 'iimage1D',
+                'iimage1DArray', 'iimage2D', 'iimage2DArray', 'iimage3D',
+                'iimageBuffer', 'iimageCube', 'image1D', 'image1DArray',
+                'image1DArrayShadow', 'image1DShadow', 'image2D',
+                'image2DArray', 'image2DArrayShadow', 'image2DShadow',
+                'image3D', 'imageBuffer', 'imageCube', 'inline', 'input',
+                'interface', 'long', 'namespace', 'noinline', 'output',
+                'packed', 'partition', 'public', 'row_major', 'sampler1D',
+                'sampler1DShadow', 'sampler2DRect', 'sampler2DRectShadow',
+                'sampler2DShadow', 'sampler3D', 'sampler3DRect', 'short',
+                'sizeof', 'static', 'superp', 'switch', 'template', 'this',
+                'typedef', 'uimage1D', 'uimage1DArray', 'uimage2D',
+                'uimage2DArray', 'uimage3D', 'uimageBuffer', 'uimageCube',
+                'union', 'unsigned', 'using', 'volatile'])
+
 
 def check_variable(name):
     """
@@ -51,9 +53,11 @@ def check_variable(name):
     """
     # Limit imposed by glGetActive* in pyopengl
     if len(name) > 31:
-        return "Variable names >31 characters may not function on some systems."
+        return ("Variable names >31 characters may not function on some "
+                "systems.")
     
     return check_identifier(name)
+
 
 def check_identifier(name):
     if '__' in name:
