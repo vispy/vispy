20f5e1bf91aa6b91e73082006b00256c70a1cf1d
Luke Campagnola <luke.campagnola@gmail.com>
2014-03-20 10:04:17 -0400

    Mesh example: lay out meshes in a grid

--- examples/visuals/mesh_visual.py
+++ examples/visuals/mesh_visual.py
@@ -24,17 +24,14 @@ class Canvas(vispy.app.Canvas):
         
         # Generate some data to work with
         global mdata
-        mdata = MeshData.sphere(20, 40, 0.3)
+        mdata = MeshData.sphere(20, 40, 1.0)
         
-        # Center: Mesh with pre-indexed vertexes, uniform color
+        # Mesh with pre-indexed vertexes, uniform color
         verts = mdata.vertexes(indexed='faces')
         mesh = MeshVisual(pos=verts, color=(1, 0, 0, 1))
-        mesh.transform = ChainTransform([self.rotation,
-                                         STTransform(translate=(0.0, 0.0)),
-                                         ], simplify=False)
         self.meshes.append(mesh)
         
-        # Top-left: Mesh with pre-indexed vertexes, per-face color
+        # Mesh with pre-indexed vertexes, per-face color
         #   Because vertexes are pre-indexed, we get a different color
         #   every time a vertex is visited, resulting in sharp color differences
         #   between edges.
@@ -44,12 +41,9 @@ class Canvas(vispy.app.Canvas):
         fcolor[...,1] = np.linspace(1, 0, nf)[:, np.newaxis]
         fcolor[...,2] = np.random.normal(size=nf)[:, np.newaxis]
         mesh = MeshVisual(pos=verts, color=fcolor)
-        mesh.transform = ChainTransform([self.rotation,
-                                         STTransform(translate=(-0.5, 0.5)),
-                                         ], simplify=False)
         self.meshes.append(mesh)        
 
-        # Top-right: Mesh with unindexed vertexes, per-vertex color
+        # Mesh with unindexed vertexes, per-vertex color
         #   Because vertexes are unindexed, we get the same color
         #   every time a vertex is visited, resulting in no color differences
         #   between edges.
@@ -61,26 +55,17 @@ class Canvas(vispy.app.Canvas):
         vcolor[:,1] = np.linspace(1, 0, nv)
         vcolor[:,2] = np.random.normal(size=nv)
         mesh = MeshVisual(pos=verts, faces=faces, color=vcolor)
-        mesh.transform = ChainTransform([self.rotation,
-                                         STTransform(translate=(0.5, 0.5)),
-                                         ], simplify=False)
         self.meshes.append(mesh)
         
-        # Bottom-left: Mesh colored by vertexes + grid contours
+        # Mesh colored by vertexes + grid contours
         mesh = MeshVisual(pos=verts, faces=faces)
-        mesh.transform = ChainTransform([self.rotation,
-                                         STTransform(translate=(-0.5, -0.5)),
-                                         ], simplify=False)
         mesh.fragment_components = [VertexColorComponent(vcolor), 
                                     GridContourComponent(
                                         spacing=(0.13, 0.13, 0.13))]
         self.meshes.append(mesh)
         
-        # Bottom-right: Phong shaded mesh
+        # Phong shaded mesh
         mesh = MeshVisual(pos=verts, faces=faces)
-        mesh.transform = ChainTransform([self.rotation,
-                                         STTransform(translate=(0.5, -0.5)),
-                                         ], simplify=False)
         normal_comp = VertexNormalComponent(mdata)
         mesh.fragment_components = [VertexColorComponent(vcolor), 
                                     normal_comp, # TODO: should the shading component take care of this?
@@ -91,6 +76,18 @@ class Canvas(vispy.app.Canvas):
                                         lights=[((-1, 1, -1), (1.0, 1.0, 1.0))],
                                         ambient=0.2)]
         self.meshes.append(mesh)
+
+
+        # Lay out meshes in a grid
+        grid = (3,3)
+        s = 0.8 / max(grid)
+        for i, mesh in enumerate(self.meshes):
+            x = 2.0 * (i % grid[0]) / grid[0] + 4.0 / grid[0] - 2
+            y = - 2.0 * (i // grid[1]) / grid[1] - 4.0 / grid[1] + 2
+            mesh.transform = ChainTransform([self.rotation,
+                                         STTransform(translate=(x, y), 
+                                                     scale=(s,s,s)),
+                                         ], simplify=False)
         
         vispy.app.Canvas.__init__(self)
         self.size = (800, 800)
