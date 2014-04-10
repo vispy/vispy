94d932108d363b1703574c28c935d8aa3bac4509
Eric Larson <larson.eric.d@gmail.com>
2014-04-10 11:42:40 -0700

    FIX: Add doc

--- vispy/app/canvas.py
+++ vispy/app/canvas.py
@@ -230,7 +230,13 @@ class Canvas(object):
             return
 
     def close(self):
-        """ Close the canvas """
+        """ Close the canvas
+
+        Note: This will usually destroy the GL context. For Qt, the context
+        (and widget) will be destroyed only if the widget is top-level.
+        To avoid having the widget destroyed (more like standard Qt
+        behavior, consider making the widget a sub-widget.
+        """
         if self._backend is not None:
             self._backend._vispy_close()
 
