6ab3e524a5cebab3c07e9f894b16e9863a981765
M S Suraj <mssurajkaiga@gmail.com>
2014-06-12 21:48:04 +0530

    Added basic PolygonVisual without border along with example

---
+++ examples/visuals/polygon_visual.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+
+"""
+Simple demonstration of PolygonVisual. 
+"""
+
+import numpy as np
+import vispy.app
+from vispy import gloo
+from vispy.visuals.polygon import PolygonVisual
+
+# vertex positions of data to draw
+pos = np.array([[0, 0, 0],
+       [0.5, 0.12, 0],
+       [0.25, 0.5, 0],
+       [0, 0.5, 0],
+       [-0.25, 0.25, 0]])
+
+class Canvas(vispy.app.Canvas):
+    def __init__(self):
+        self.polygon = PolygonVisual(pos=pos, color=(1,0,0,1))
+        
+        vispy.app.Canvas.__init__(self)
+        self.size = (800, 800)
+        self.show()
+        
+    def on_paint(self, ev):
+        gloo.set_clear_color((0, 0, 0, 1))
+        gloo.clear()
+        gloo.set_viewport(0, 0, *self.size)
+        self.polygon.paint()
+        
+
+if __name__ == '__main__':
+    win = Canvas() 
+    import sys
+    if sys.flags.interactive != 1:
+        vispy.app.run()
