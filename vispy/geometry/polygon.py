7c34c73b04692c6294cd2de69eef77b2e9fb40c2
M S Suraj <mssurajkaiga@gmail.com>
2014-06-14 03:03:22 +0530

    Basic polygon data handling framework

---
+++ vispy/geometry/polygon.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014, Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
+
+import numpy as np
+
+
+class Polygon(object):
+    """
+    Polygon class for data handling
+    """
+    def __init__(self, vertices=None, edges=None, faces=None):
+        """
+        Parameters
+        ----------
+        vertexes        (Nv, 3) array of vertex coordinates.
+                        If faces is not specified, then this will instead be
+                        interpreted as (Nf, 3, 3) array of coordinates.
+        faces           (Nf, 3) array of indexes into the vertex array.
+        edges           (Nv, 2) array of constraining edges specified by vertex
+                        indices.
+        
+        All arguments are optional.
+        """
+        self._vertices = vertices
+        self._edges = edges
+        self._faces = faces
+        self._convex_hull = None
+
+    @property  
+    def faces(self):
+        """Return an array (Nf, 3) of vertex indexes, three per triangular
+        face in the mesh.
+        
+        If faces have not been computed for this mesh, the function
+        computes them.
+        If no vertices or faces are specified, the function returns None.
+        """
+
+        if self._faces is None:
+            if self._vertices is None:
+                return None
+            self.triangulate()
+        return self._faces
+
+    @faces.setter
+    def faces(self, f):
+        """
+        If vertices and faces are incompatible, this will generate vertices
+        from these faces and set them.
+        """
+        self._faces = f
+
+    @property  
+    def vertices(self):
+        """Return an array (Nf, 3) of vertices.
+        
+        If only faces exist, the function computes the vertices and
+        returns them.
+        If no vertices or faces are specified, the function returns None.
+        """
+
+        if self._faces is None:
+            if self._vertices is None:
+                return None
+                self.triangulate()
+        return self._faces
+
+    @vertices.setter
+    def vertices(self, v):
+        """
+        If vertices and faces are incompatible, this will generate faces
+        from these vertices and set them.
+        """
+        self._vertices = v
+
+    @property  
+    def edges(self):
+        """Return an array (Nv, 2) of vertex indices.
+
+        If no vertices or faces are specified, the function returns None.
+        """
+        return self._edges
+
+    @edges.setter
+    def edges(self, e):
+        """
+        Ensures that all edges are valid.
+        """
+        self._edges = e
+
+    def triangulate(self):
+        """
+        Triangulates the set of vertices and stores the triangles in faces and
+        the convex hull in convex_hull.
+        """
+        pass
+
+    def add_vertex(self, vertex):
+        """
+        Adds given vertex and retriangulates to generate new faces.
+        """
+        pass
