b790d135f5fe849f97915d98edbed965d9da311b
Almar Klein <almar.klein@gmail.com>
2014-06-25 14:36:11 +0200

    add test for vispy.use

--- vispy/util/tests/test_vispy.py
+++ vispy/util/tests/test_vispy.py
@@ -1,3 +1,13 @@
 """ Tests to ensure that base vispy namespace functions correctly,
 including configuration options.
 """
+
+import vispy
+from vispy.testing import requires_application
+
+@requires_application('pyside')
+def test_use():
+    vispy.use('')  # Should do nothing
+    vispy.use('pyside')  # So this should always work
+    vispy.use('desktop')
+    vispy.use('pyside desktop')
