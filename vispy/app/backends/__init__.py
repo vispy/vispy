9c2473021640020a0e1e1e7fbc022184d16cfdfd
Almar Klein <almar.klein@gmail.com>
2014-06-30 13:50:31 +0200

    Fix docs + vispy.use() does not have to know the app backends now

--- vispy/app/backends/__init__.py
+++ vispy/app/backends/__init__.py
@@ -10,11 +10,26 @@ instance we can test whether the GUI toolkit for a backend is already
 imported. This stuff is mostly used in the Application.use method.
 """
 
-# Import names from vispy.util. Names are defined there to avoid having
-# to copy the names.
-from ...util._use import (APP_BACKENDS as BACKENDS,  # noqa
-                          APP_BACKEND_NAMES as BACKEND_NAMES,  # noqa
-                          APP_BACKENDMAP as BACKENDMAP)  # noqa
+# Define backends: name, vispy.app.backends.xxx module, native module name.
+# This is the order in which they are attempted to be imported.
+BACKENDS = [
+    ('PyQt4', '_pyqt4', 'PyQt4'),
+    ('PySide', '_pyside', 'PySide'),
+    ('Pyglet', '_pyglet', 'pyglet'),
+    ('Glfw', '_glfw', 'vispy.app.backends._libglfw'),
+    ('SDL2', '_sdl2', 'sdl2'),
+    ('Glut', '_glut', 'OpenGL.GLUT'),
+    ('_test', '_test', 'vispy.app.backends._test'),  # add one that will fail
+]
+
+BACKEND_NAMES = []
+for backend in BACKENDS:
+    if backend[1][1:] not in BACKEND_NAMES:  # remove redundant qt entries
+        BACKEND_NAMES.append(backend[1][1:])
+
+# Map of the lowercase backend names to the backend descriptions above
+# so that we can look up its properties if we only have a name.
+BACKENDMAP = dict([(be[0].lower(), be) for be in BACKENDS])
 
 # List of attempted backends. For logging.
 ATTEMPTED_BACKENDS = []
--- vispy/util/_use.py
+++ vispy/util/_use.py
@@ -3,8 +3,6 @@
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 
 """ 
-## Motivation
-
 The idea of a 'global' vispy.use function is that although vispy.app
 and vispy.gloo.gl can be used independently, they are not complely
 independent for some configureation. E.g. when using real ES 2.0,
@@ -12,43 +10,10 @@ the app backend should use EGL and not a desktop OpenGL context. Also,
 we probably want it to be easy to configure vispy to use the ipython
 notebook backend, which requires specifc config of both app and gl.
 
-
-## Implementation
-
-The user specifies a string with usage options. In that way we can
-easily add special modes of operation like 'inb' for the IPython
-notebook. The use functions evaluates the string and should know what
-to do with it. It should raise an error if an invalid usage option is
-given. Therefore, this module should be aware of the possible usage
-modes for app and gl. 
-
+This module does not have to be aware of the available app and gl
+backends, but it should be(come) aware of (in)compatibilities between
+them.
 """
-
-
-# Define backends: name, vispy.app.backends.xxx module, native module name.
-# This is the order in which they are attempted to be imported.
-APP_BACKENDS = [
-    ('PyQt4', '_pyqt4', 'PyQt4'),
-    ('PySide', '_pyside', 'PySide'),
-    ('Pyglet', '_pyglet', 'pyglet'),
-    ('Glfw', '_glfw', 'vispy.app.backends._libglfw'),
-    ('SDL2', '_sdl2', 'sdl2'),
-    ('Glut', '_glut', 'OpenGL.GLUT'),
-    ('_test', '_test', 'vispy.app.backends._test'),  # add one that will fail
-]
-
-APP_BACKEND_NAMES = []
-for backend in APP_BACKENDS:
-    if backend[1][1:] not in APP_BACKEND_NAMES:  # remove redundant qt entries
-        APP_BACKEND_NAMES.append(backend[1][1:])
-
-# Map of the lowercase backend names to the backend descriptions above
-# so that we can look up its properties if we only have a name.
-APP_BACKENDMAP = dict([(be[0].lower(), be) for be in APP_BACKENDS])
-
-
-# Define possible GL backends
-GL_BACKENDS = 'desktop', 'pyopengl', 'angle'
 
 
 def use(app=None, gl=None):
