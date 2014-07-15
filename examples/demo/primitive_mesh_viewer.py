1c4d5bc6d6835ab569ab5108619c55bcba4a5050
sylm21 <sylvain.moyne@ensta-bretagne.fr>
2014-07-15 11:28:18 +0200

    Update primitive_mesh_viewer.py
    Try to obtain travis agreement ...

--- examples/demo/primitive_mesh_viewer.py
+++ examples/demo/primitive_mesh_viewer.py
@@ -12,7 +12,7 @@ MODIFICATION :
     - le 15/07/14 force faces and edges to uint32 dtype
 """
 
-try: 
+try:
     from sip import setapi
     setapi("QDate", 2)
     setapi("QDateTime", 2)
@@ -24,20 +24,18 @@ try:
 except:
     pass
 
-
 from PyQt4 import QtGui, QtCore
 import sys
 
 import numpy as np
 from vispy import app, gloo
-from vispy.util.transforms import perspective, translate, rotate, ortho
+from vispy.util.transforms import perspective, translate, rotate
 from vispy.util import meshdata as md
 
-
-OBJECT = {'sphere': [('rows', 3, 1000, 'int', 3), 
+OBJECT = {'sphere': [('rows', 3, 1000, 'int', 3),
                      ('cols', 3, 1000, 'int', 3),
                      ('radius', 0.1, 10, 'double', 1.0)],
-          'cylinder': [('rows', 4, 1000, 'int', 4), 
+          'cylinder': [('rows', 4, 1000, 'int', 4),
                        ('cols', 4, 1000, 'int', 4),
                        ('radius', 0.1, 10, 'double', 1.0),
                        ('radius Top.', 0.1, 10, 'double', 1.0),
@@ -45,7 +43,7 @@ OBJECT = {'sphere': [('rows', 3, 1000, 'int', 3),
           'cone': [('cols', 3, 1000, 'int', 3),
                    ('radius', 0.1, 10, 'double', 1.0),
                    ('length', 0.1, 10, 'double', 1.0)],
-          'arrow': [('rows', 4, 1000, 'int', 4), 
+          'arrow': [('rows', 4, 1000, 'int', 4),
                     ('cols', 4, 1000, 'int', 4),
                     ('radius', 0.01, 10, 'double', 0.1),
                     ('length', 0.1, 10, 'double', 1.0),
@@ -89,50 +87,48 @@ void main()
 """
 
 DEFAULT_COLOR = [0, 1, 1, 1]
-
 # -----------------------------------------------------------------------------
+
+
 class MyMeshData(md.MeshData):
-    """
-    Add to Meshdata class the capability to export good data for gloo
-    """
-    
+    """ Add to Meshdata class the capability to export good data for gloo """
     def __init__(self, vertices=None, faces=None, edges=None,
                  vertex_colors=None, face_colors=None):
         md.MeshData.__init__(self, vertices=None, faces=None, edges=None,
-                 vertex_colors=None, face_colors=None)
-                     
+                             vertex_colors=None, face_colors=None)
+
     def getGLTriangles(self):
         """
         Build vertices for a colored mesh.
-    
-        V  is the vertices
-        I1 is the indices for a filled cube (use with GL_TRIANGLES)
-        I2 is the indices for an outline cube (use with GL_LINES)
+            V  is the vertices
+            I1 is the indices for a filled cube (use with GL_TRIANGLES)
+            I2 is the indices for an outline cube (use with GL_LINES)
         """
         vtype = [('a_position', np.float32, 3),
-             ('a_normal', np.float32, 3),
-             ('a_color', np.float32, 4)]
-        
+                 ('a_normal', np.float32, 3),
+                 ('a_color', np.float32, 4)]
         vertices = self.vertices()
         normals = self.vertex_normals()
         faces = np.uint32(self.faces())
-        
+
         edges = np.uint32(self.edges().reshape((-1)))
         colors = self.vertex_colors()
-             
+
         nbrVerts = vertices.shape[0]
         V = np.zeros(nbrVerts, dtype=vtype)
         V[:]['a_position'] = vertices
         V[:]['a_normal'] = normals
         V[:]['a_color'] = colors
-             
-        return V, faces.reshape((-1)), edges.reshape((-1))  
+
+        return V, faces.reshape((-1)), edges.reshape((-1))
 
 # -----------------------------------------------------------------------------
+
+
 class ObjectParam(object):
     """
-    OBJECT parameter test    
-    """               
+    OBJECT parameter test
+    """
     def __init__(self, name, listParam):
         self.name = name
         self.listParam = listParam
@@ -140,33 +136,34 @@ class ObjectParam(object):
         self.props['visible'] = True
         for nameV, minV, maxV, typeV, iniV in listParam:
             self.props[nameV] = iniV
-    
 # -----------------------------------------------------------------------------
+
+
 class ObjectWidget(QtGui.QWidget):
-    """    
+    """
     Widget for editing OBJECT parameters
-    """    
-    signalObjetChanged = QtCore.pyqtSignal(ObjectParam, name='objectChanged')    
-        
+    """
+    signalObjetChanged = QtCore.pyqtSignal(ObjectParam, name='objectChanged')
+
     def __init__(self, parent=None, param=None):
         super(ObjectWidget, self).__init__(parent)
-    
-        if param == None:
+
+        if param is None:
             self.param = ObjectParam('sphere', OBJECT['sphere'])
         else:
             self.param = param
-        
-        self.gbC = QtGui.QGroupBox(u"Hide/Show %s" % self.param.name) 
-        self.gbC.setCheckable(True)        
+
+        self.gbC = QtGui.QGroupBox(u"Hide/Show %s" % self.param.name)
+        self.gbC.setCheckable(True)
         self.gbC.setChecked(self.param.props['visible'])
         self.gbC.toggled.connect(self.updateParam)
-        
+
         lL = []
         self.sp = []
         gbC_lay = QtGui.QGridLayout()
-        for nameV, minV, maxV, typeV, iniV in self.param.listParam:                       
-            lL.append(QtGui.QLabel(nameV, self.gbC))        
-            if typeV == 'double':            
+        for nameV, minV, maxV, typeV, iniV in self.param.listParam:
+            lL.append(QtGui.QLabel(nameV, self.gbC))
+            if typeV == 'double':
                 self.sp.append(QtGui.QDoubleSpinBox(self.gbC))
                 self.sp[-1].setDecimals(2)
                 self.sp[-1].setLocale(QtCore.QLocale(QtCore.QLocale.English))
@@ -175,25 +172,24 @@ class ObjectWidget(QtGui.QWidget):
             self.sp[-1].setMinimum(minV)
             self.sp[-1].setMaximum(maxV)
             self.sp[-1].setValue(iniV)
-        
+
         #Layout
         for pos in range(len(lL)):
-            gbC_lay.addWidget(lL[pos],pos,0)
-            gbC_lay.addWidget(self.sp[pos],pos,1)
+            gbC_lay.addWidget(lL[pos], pos, 0)
+            gbC_lay.addWidget(self.sp[pos], pos, 1)
             # Les signaux
-            self.sp[pos].valueChanged.connect(self.updateParam)        
-        
+            self.sp[pos].valueChanged.connect(self.updateParam)
+
         self.gbC.setLayout(gbC_lay)
-        
-        
+
         vbox = QtGui.QVBoxLayout()
         hbox = QtGui.QHBoxLayout()
         hbox.addWidget(self.gbC)
         hbox.addStretch(1.0)
-        
+
         vbox.addLayout(hbox)
-        vbox.addStretch(1.0)        
-        
+        vbox.addStretch(1.0)
+
         self.setLayout(vbox)
 
     def updateParam(self, option):
@@ -201,14 +197,15 @@ class ObjectWidget(QtGui.QWidget):
         update param and emit a signal
         """
         self.param.props['visible'] = self.gbC.isChecked()
-        keys = map(lambda x: x[0],self.param.listParam)
+        keys = map(lambda x: x[0], self.param.listParam)
         for pos, nameV in enumerate(keys):
             self.param.props[nameV] = self.sp[pos].value()
-        # emit signal 
-        self.signalObjetChanged.emit(self.param)     
-
+        # emit signal
+        self.signalObjetChanged.emit(self.param)
 
 # -----------------------------------------------------------------------------
+
+
 class Canvas(app.Canvas):
 
     def __init__(self,):
@@ -219,14 +216,14 @@ class Canvas(app.Canvas):
         self.zfar = 10.0
         width, height = self.size
         self.aspect = width / float(height)
-        
+
         self.program = gloo.Program(vert, frag)
 
         self.view = np.eye(4, dtype=np.float32)
         self.model = np.eye(4, dtype=np.float32)
         self.projection = np.eye(4, dtype=np.float32)
         translate(self.view, 0, 0, -5.0)
-        
+
         self.program['u_model'] = self.model
         self.program['u_view'] = self.view
 
@@ -261,30 +258,30 @@ class Canvas(app.Canvas):
         self.size = event.size
         gloo.set_viewport(0, 0, width, height)
         self.aspect = width / float(height)
-        self.projection = perspective(self.fovy, width / float(height), 1.0, 
+        self.projection = perspective(self.fovy, width / float(height), 1.0,
                                       self.zfar)
         self.program['u_projection'] = self.projection
 
     # ---------------------------------
     def on_draw(self, event):
-        gloo.clear()        
+        gloo.clear()
         if self.visible:
-            # Filled cube        
-            gloo.set_state(blend=False, depth_test=True, 
+            # Filled cube
+            gloo.set_state(blend=False, depth_test=True,
                            polygon_offset_fill=True)
             self.program['u_color'] = 1, 1, 1, 1
             self.program.draw('triangles', self.filled_buf)
-    
+
             # Outline
-            gloo.set_state(blend=True, depth_test=True, 
+            gloo.set_state(blend=True, depth_test=True,
                            polygon_offset_fill=False)
             gloo.set_depth_mask(False)
             self.program['u_color'] = 0, 0, 0, 1
             self.program.draw('lines', self.outline_buf)
             gloo.set_depth_mask(True)
-        
+
     # ---------------------------------
-    def setData(self, vertices, filled, outline, diam):        
+    def setData(self, vertices, filled, outline, diam):
         self.filled_buf = gloo.IndexBuffer(filled)
         self.outline_buf = gloo.IndexBuffer(outline)
         self.vertices_buff = gloo.VertexBuffer(vertices)
@@ -297,12 +294,13 @@ class Canvas(app.Canvas):
         translate(self.view, 0, 0, -5.0-diam)
         self.program['u_view'] = self.view
         self.zfar = 100.0
-        self.projection = perspective(self.fovy,self.aspect, 1.0, self.zfar)
+        self.projection = perspective(self.fovy, self.aspect, 1.0, self.zfar)
         self.program['u_projection'] = self.projection
-        
         self.update()
-        
-# -----------------------------------------------------------------------------        
+
+# -----------------------------------------------------------------------------
+
+
 class MainWindow(QtGui.QMainWindow):
 
     def __init__(self):
@@ -313,47 +311,47 @@ class MainWindow(QtGui.QMainWindow):
 
         self.initActions()
         self.initMenus()
-        
+
         self.listObject = QtGui.QListWidget()
         self.listObject.setAlternatingRowColors(True)
         self.listObject.itemSelectionChanged.connect(self.listObjectChanged)
-        
+
         self.listObject.addItems(OBJECT.keys())
         #self.listObject.s
-        
         self.propsWidget = ObjectWidget(self)
-        self.propsWidget.signalObjetChanged.connect(self.updateView)  
-        
+        self.propsWidget.signalObjetChanged.connect(self.updateView)
+
         self.splitterV = QtGui.QSplitter(QtCore.Qt.Vertical)
-        self.splitterV.addWidget(self.listObject)          
+        self.splitterV.addWidget(self.listObject)
         self.splitterV.addWidget(self.propsWidget)
-        
-        self.canvas =  Canvas()
+
+        self.canvas = Canvas()
         self.canvas.create_native()
         self.canvas.native.setParent(self)
         self.canvas.measure_fps(0.1, self.show_fps)
 
         # Central Widget
-        splitter1 = QtGui.QSplitter(QtCore.Qt.Horizontal) 
-        splitter1.addWidget(self.splitterV )
+        splitter1 = QtGui.QSplitter(QtCore.Qt.Horizontal)
+        splitter1.addWidget(self.splitterV)
         splitter1.addWidget(self.canvas.native)
-               
+
         self.setCentralWidget(splitter1)
-                
+
         # FPS message in statusbar:
         self.status = self.statusBar()
         self.status.showMessage("...")
-        
+
         self.mesh = MyMeshData()
         self.updateView(self.propsWidget.param)
 
-    def listObjectChanged(self):        
+    def listObjectChanged(self):
         row = self.listObject.currentIndex().row()
         name = self.listObject.currentIndex().data()
-        if row !=-1:
+        if row != -1:
             self.propsWidget.deleteLater()
-            self.propsWidget = ObjectWidget(self, 
-                            param=ObjectParam(name, OBJECT[name]))
+            self.propsWidget = ObjectWidget(self,
+                                            param=ObjectParam(name,
+                                                              OBJECT[name]))
             self.splitterV.addWidget(self.propsWidget)
             self.propsWidget.signalObjetChanged.connect(self.updateView)
             self.updateView(self.propsWidget.param)
@@ -364,7 +362,6 @@ class MainWindow(QtGui.QMainWindow):
         self.exitAction.setStatusTip('Exit application')
         self.exitAction.triggered.connect(self.close)
 
-
     def initMenus(self):
         menuBar = self.menuBar()
         fileMenu = menuBar.addMenu('&File')
@@ -372,58 +369,52 @@ class MainWindow(QtGui.QMainWindow):
 
     def close(self):
         QtGui.qApp.quit()
-        
+
     def show_fps(self, fps):
         nbrTri = self.mesh.face_count()
         self.status.showMessage("FPS - %.2f and nbr Tri %s " % (fps, nbrTri))
 
-    
     def updateView(self, param):
         cols = param.props['cols']
-        print cols
-        radius = param.props['radius']  
+        radius = param.props['radius']
         #Characteristc length of the mesh
         diam = radius * 2.0
-        if param.name == 'sphere':                     
-            rows = param.props['rows']      
-            mesh =  md.sphere(cols, rows, radius=radius)
+        if param.name == 'sphere':
+            rows = param.props['rows']
+            mesh = md.sphere(cols, rows, radius=radius)
         elif param.name == 'cone':
             length = param.props['length']
-            mesh =  md.cone(cols, radius=radius, length=length)
+            mesh = md.cone(cols, radius=radius, length=length)
             diam = max(diam, length*2.0)
         elif param.name == 'cylinder':
             rows = param.props['rows']
             length = param.props['length']
             radius2 = param.props['radius Top.']
-            mesh =  md.cylinder(rows, cols, radius=[radius, radius2], 
-                                  length=length)
+            mesh = md.cylinder(rows, cols, radius=[radius, radius2],
+                               length=length)
             diam = max(diam, length*2.0, radius2*2.0)
         elif param.name == 'arrow':
             length = param.props['length']
             rows = param.props['rows']
             fRC = param.props['fRC']
             fLC = param.props['fLC']
-            mesh = md.arrow(rows, cols, radius=radius, length=length, 
+            mesh = md.arrow(rows, cols, radius=radius, length=length,
                             fRC=fRC, fLC=fLC)
             diam = max(diam, length*2.0, radius*2.0*fRC)
-                
         else:
             return
-        
+
         self.canvas.visible = param.props['visible']
         self.mesh.set_vertices(mesh.vertices())
         self.mesh.set_faces(mesh.faces())
         self.mesh.set_vertex_colors(DEFAULT_COLOR)
         vertices, filled, outline = self.mesh.getGLTriangles()
         self.canvas.setData(vertices, filled, outline, diam)
-        
 
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
-        
+
     appQt = QtGui.QApplication(sys.argv)
-    
     win = MainWindow()
     win.show()
-    
-    sys.exit(appQt.exec_())        
+    sys.exit(appQt.exec_())
