d48365b105de2fb3fd47c6332c0980d40c4755c6
Almar Klein <almar.klein@gmail.com>
2014-07-25 16:19:24 +0200

    Add examples to create Visuals using new Function class.

---
+++ examples/visuals/line_visual2.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+
+""" A Line Visual that uses the new shader Function.
+"""
+
+from __future__ import division
+
+import numpy as np
+
+#from .visual import Visual
+#from ..shader.function2 import Function, Variable
+#from ...import gloo
+
+from vispy.scene.visuals.visual import Visual
+from vispy.scene.shaders.function2 import Function, Variable
+from vispy import gloo
+
+
+## Snippet templates (defined as string to force user to create fresh Function)
+# Consider these stored in a central location in vispy ...
+
+
+vertex_template = """
+void main() {
+}
+"""
+
+fragment_template = """
+void main() {
+}
+"""
+
+
+dash_template = """
+float dash() {
+    float mod = $distance / $dash_len;
+    mod = mod - int(mod);
+    return 0.5 * sin(mod*3.141593*2) + 0.5;
+}
+"""
+            
+## Functions that can be uses as is (don't have template variables)
+# Consider these stored in a central location in vispy ...
+
+color3to4 = Function("""
+vec4 color3to4(vec3 rgb) {
+    return vec4(rgb, 1.0);
+}
+""")
+
+stub4 = Function("vec4 stub4(vec4 value) { return value; }")
+stub3 = Function("vec4 stub3(vec3 value) { return value; }")
+
+
+## Actual code
+
+class Line(Visual):
+    
+    def __init__(self, parent, data, color=None):
+        Visual.__init__(self, parent)
+        
+        # Create a program
+        self._program = gloo.Program('', '')
+        # Create vertex and fragment shaders
+        self._vert_code = Function(vertex_template)
+        self._frag_code = Function(fragment_template)
+        self._vert_code.link(self._frag_code)
+        
+        # Define how we are going to specify position and color
+        self._vert_code['gl_Position'] = 'vec4($position, 1.0)'
+        self._frag_code['gl_FragColor'] = 'vec4($color, 1.0)'
+        
+        # Set position data
+        vbo = gloo.VertexBuffer(data)
+        self._vert_code['position'] = 'attribute vec3 position', vbo
+        
+        # Create some variables related to color. We use a combination
+        # of these depending on the kind of color being set.
+        # We predefine them here so that we can re-use VBO and uniforms
+        vbo = gloo.VertexBuffer(data)
+        self._color_var = Variable('uniform vec3 color')
+        self._colors_var = Variable('attribute vec3 color', vbo)
+        self._color_varying = Variable('varying vec3 color')
+        
+        self.set_color((0, 0, 1))
+        if color is not None:
+            self.set_color(color)
+    
+    def set_data(self, data):
+        """ Set the vertex data for this line.
+        """
+        vbo = self._vert_code['position'].value
+        vbo.set_data(data)
+    
+    def set_color(self, color):
+        """ Set the color for this line. Color can be specified for the
+        whole line or per vertex.
+        
+        When the color is changed from single color to array, the shaders
+        need to be recompiled, otherwise we only need to reset the
+        uniform / attribute.
+        """
+        
+        if isinstance(color, tuple):
+            # Single value (via a uniform)
+            color = [float(v) for v in color]
+            assert len(color) == 3
+            self._color_var.value = color
+            self._frag_code['color'] = self._color_var
+        elif isinstance(color, np.ndarray):
+            # A value per vertex, via a VBO
+            assert color.shape[1] == 3
+            self._colors_var.value.set_data(color)
+            self._frag_code['color'] = self._color_varying
+            self._vert_code[self._color_varying] = self._colors_var
+        else:
+            raise ValueError('Line colors must be Nx3 array or color tuple')
+    
+    def draw(self, event=None):
+        
+        # The code updating and variable checking is something that can
+        # be abstracted away in a base class or a ModularShader 
+        # (I (AK) prefers the fisrt).
+        
+        gloo.set_state(blend=True, blend_func=('src_alpha', 'one'))
+        
+        # todo: ischanged should not iterate over the variables
+        if self._vert_code.ischanged():
+            print('updating vertex shader')
+            self._program.shaders[0].code = str(self._vert_code)
+            self._program._create_variables()  # force update
+        if self._frag_code.ischanged():
+            print('updating fragment shader')
+            self._program.shaders[1].code = str(self._frag_code)
+            self._program._create_variables()  # force update
+        
+        # todo: only do this when necesary
+        for var in self._vert_code.get_variables():
+            if var.vtype in ('attribute', 'uniform'):
+                self._program[var.name] = var.value
+        
+        # Draw
+        self._program.draw('line_strip')
+
+
+class DashedLine(Line):
+    """ This takes the Line and modifies the composition of Functions
+    to create a dashing effect.
+    """
+    def __init__(self, *args, **kwargs):
+        Line.__init__(self, *args, **kwargs)
+        
+        dasher = Function(dash_template) 
+        self._frag_code['gl_FragColor.a'] = dasher()
+        dasher['distance'] = 'varying float v_dashdist'
+        dasher['dash_len'] = '0.001'
+        self._vert_code[dasher['distance']] = 'gl_Position.x'
+
+
+## Show the visual
+
+if __name__ == '__main__':
+    from vispy import app
+    
+    # vertex positions of data to draw
+    N = 200
+    pos = np.zeros((N, 3), dtype=np.float32)
+    pos[:, 0] = np.linspace(-0.9, 0.9, N)
+    pos[:, 1] = np.random.normal(size=N, scale=0.2).astype(np.float32)
+    
+    # color array
+    color = np.ones((N, 3), dtype=np.float32)
+    color[:, 0] = np.linspace(0, 1, N)
+    color[:, 1] = color[::-1, 0]
+    
+    class Canvas(app.Canvas):
+        def __init__(self):
+            app.Canvas.__init__(self, close_keys='escape')
+            
+            self.line1 = Line(None, pos, (3, 9, 0))
+            self.line2 = DashedLine(None, pos + 0.4, color)
+    
+        def on_draw(self, ev):
+            gloo.clear((0, 0, 0, 1), True)
+            gloo.set_viewport(0, 0, *self.size)
+            self.line1.draw()
+            self.line2.draw()
+    
+    c = Canvas()
+    c.show()
+    app.run()
---
+++ examples/visuals/mesh_visual2.py
@@ -0,0 +1,282 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+
+""" A Mesh Visual that uses the new shader Function.
+"""
+
+from __future__ import division
+
+import numpy as np
+
+#from .visual import Visual
+#from ..shader.function2 import Function, Variable
+#from ...import gloo
+
+from vispy.scene.visuals.visual import Visual
+from vispy.scene.shaders.function2 import Function, Variable
+from vispy import gloo
+
+
+## Snippet templates (defined as string to force user to create fresh Function)
+# Consider these stored in a central location in vispy ...
+
+
+vertex_template = """
+void main() {
+}
+"""
+
+fragment_template = """
+void main() {
+}
+"""
+
+phong_template = """
+vec4 phong_shading(vec4 color) {
+    vec3 norm = normalize($normal.xyz);
+    vec3 light = normalize($light_dir.xyz);
+    float p = dot(light, norm);
+    p = (p < 0. ? 0. : p);
+    vec4 diffuse = $light_color * p;
+    diffuse.a = 1.0;
+    p = dot(reflect(light, norm), vec3(0,0,1));
+    if (p < 0.0) {
+        p = 0.0;
+    }
+    vec4 specular = $light_color * 5.0 * pow(p, 100.);
+    return color * ($ambient + diffuse) + specular;
+}
+"""
+
+## Functions that can be uses as is (don't have template variables)
+# Consider these stored in a central location in vispy ...
+
+color3to4 = Function("""
+vec4 color3to4(vec3 rgb) {
+    return vec4(rgb, 1.0);
+}
+""")
+
+stub4 = Function("vec4 stub4(vec4 value) { return value; }")
+stub3 = Function("vec4 stub3(vec3 value) { return value; }")
+
+
+## Actual code
+
+
+class Mesh(Visual):
+    
+    def __init__(self, parent, 
+                 vertices, faces=None, normals=None, values=None):
+        Visual.__init__(self, parent)
+        
+        # Create a program
+        self._program = gloo.Program('', '')
+        
+        # Create vertex and fragment shaders
+        self._vert_code = Function(vertex_template)
+        self._frag_code = Function(fragment_template)
+        self._vert_code.link(self._frag_code)
+        
+        # Define how we are going to specify position and color
+        self._vert_code['gl_Position'] = 'vec4($position, 1.0)'
+        self._frag_code['gl_FragColor'] = '$light($color)'
+        
+        # Define variables related to color. Only one is in use at all times
+        self._variables = {}
+        self._variables['u_color3'] = Variable('uniform vec3 u_color')
+        self._variables['u_color4'] = Variable('uniform vec4 u_color')
+        self._variables['a_color3'] = Variable('attribute vec3 a_color')
+        self._variables['a_color4'] = Variable('attribute vec4 a_color')
+        
+        # Set color to a varying
+        self._frag_code['color'] = 'varying vec4 v_color'
+        
+        # Init
+        self.shading = 'plain'
+        #
+        self.set_vertices(vertices)
+        self.set_faces(faces)
+        self.set_normals(normals)
+        self.set_values(values)
+    
+    def set_vertices(self, vertices):
+        vertices = gloo.VertexBuffer(vertices)
+        self._vert_code['position'] = 'attribute vec3 a_position', vertices
+    
+    def set_faces(self, faces):
+        if faces is not None:
+            self._faces = gloo.IndexBuffer(faces)
+        else:
+            self._faces = None
+    
+    def set_normals(self, normals):
+        self._normals = normals
+        self.shading = self.shading  # Update
+    
+    def set_values(self, values):
+        
+        # todo: reuse vertex buffer is possible (use set_data)
+        # todo: we may want to clear any color vertex buffers
+        
+        if isinstance(values, tuple):
+            # Single value (via a uniform)
+            values = [float(v) for v in values]
+            if len(values) == 3:
+                variable = self._variables['u_color3']
+                variable.value = values
+                self._frag_code['color'] = color3to4(variable)
+            elif len(values) == 4:
+                variable = self._variables['u_color4']
+                variable.value = values
+                self._frag_code['color'] = variable
+            else:
+                raise ValueError('Color tuple must have 3 or 4 values.')
+        
+        elif isinstance(values, np.ndarray):
+            # A value per vertex, via a VBO
+            
+            if values.shape[1] == 1:
+                # Look color up in a colormap
+                raise NotImplementedError()
+            
+            elif values.shape[1] == 2:
+                # Look color up in a texture
+                raise NotImplementedError()
+            
+            elif values.shape[1] == 3:
+                # Explicitly set color per vertex
+                varying = Variable('varying vec3 v_color')
+                self._frag_code['color'] = color3to4(varying)
+                variable = self._variables['a_color3']
+                variable.value = gloo.VertexBuffer(values)
+                self._vert_code[varying] = variable
+            
+            elif values.shape[1] == 4:
+                # Explicitly set color per vertex
+                # Fragment shader
+                varying = Variable('varying vec4 v_color')
+                self._frag_code['color'] = varying
+                variable = self._variables['a_color4']
+                variable.value = gloo.VertexBuffer(values)
+                self._vert_code[varying] = variable
+            
+            else:
+                raise ValueError('Mesh values must be NxM, with M 1,2,3 or 4.')
+        else:
+            raise ValueError('Mesh values must be NxM array or color tuple')
+    
+    @property
+    def shading(self):
+        """ The shading method used.
+        """
+        return self._shading
+    
+    @shading.setter
+    def shading(self, value):
+        assert value in ('plain', 'flat', 'phong')
+        self._shading = value
+        # todo: add gouroud shading
+        # todo: allow flat shading even if vertices+faces is specified.
+        if value == 'plain':
+            self._frag_code['light'] = stub4()
+        
+        elif value == 'flat':
+            pass
+            
+        elif value == 'phong':
+            assert self._normals is not None
+            # Apply phonmg function, 
+            phong = Function(phong_template)
+            self._frag_code['light'] = phong()
+            # Normal data comes via vertex shader
+            phong['normal'] = 'varying vec3 v_normal'
+            var = 'attribute vec3 a_normal', gloo.VertexBuffer(self._normals)
+            self._vert_code[phong['normal']] = var
+            # Additional phong proprties
+            phong['light_dir'] = 'const vec3 LIGHTDIR vec3(1.0, 1.0, 1.0)'
+            phong['light_color'] = 'vec4(1.0, 1.0, 1.0, 1.0)'
+            phong['ambient'] = 'vec4(0.3, 0.3, 0.3, 1.0)'
+            # todo: light properties should be queried from the SubScene
+            # instance.
+    
+    def draw(self, event):
+        
+        # todo: ischanged should not iterate over the variables
+        if self._vert_code.ischanged():
+            self._program.shaders[0].code = str(self._vert_code)
+            self._program._create_variables()  # force update
+        if self._frag_code.ischanged():
+            self._program.shaders[1].code = str(self._frag_code)
+            self._program._create_variables()  # force update
+        
+        # todo: only do this when necesary
+        for var in self._vert_code.get_variables():
+            if var.vtype in ('attribute', 'uniform'):
+                self._program[var.name] = var.value
+        
+        # Draw
+        self._program.draw('triangles', self._faces)
+
+
+## The code to show it ...
+
+if __name__ == '__main__':
+    
+    from vispy import app
+    from vispy.util.meshdata import sphere
+    
+    mdata = sphere(20, 20)
+    faces = mdata.faces()
+    verts = mdata.vertices() / 4.0
+    verts_flat = mdata.vertices(indexed='faces').reshape(-1, 3) / 4.0
+    normals = mdata.vertex_normals()
+    normals_flat = mdata.vertices(indexed='faces').reshape(-1, 3) / 4.0
+    colors = np.random.uniform(0.2, 0.8, (verts.shape[0], 3)).astype('float32')
+    colors_flat = np.random.uniform(0.2, 0.8, 
+                                    (verts_flat.shape[0], 3)).astype('float32')
+    
+    class Canvas(app.Canvas):
+        def __init__(self):
+            app.Canvas.__init__(self, None)
+            self.size = 700,    700
+            self.meshes = []
+            
+            # A plain mesh with uniform color
+            offset = np.array([-0.7, 0.7, 0.0], 'float32')
+            mesh = Mesh(None, verts+offset, faces, None,
+                        values=(1.0, 0.4, 0.0, 1.0))
+            self.meshes.append(mesh)
+            
+            # A colored mesh with one color per phase
+            offset = np.array([0.0, 0.7, 0.0], 'float32')
+            mesh = Mesh(None, verts_flat+offset, None, None,
+                        values=colors_flat)
+            self.meshes.append(mesh)
+            
+            # Same mesh but using faces, so we get interpolation of color
+            offset = np.array([0.7, 0.7, 0.0], 'float32')
+            mesh = Mesh(None, verts+offset, faces, None, values=colors)
+            self.meshes.append(mesh)
+            
+            # Flat phong shading
+            offset = np.array([0.0, 0.0, 0.0], 'float32')
+            mesh = Mesh(None, verts_flat+offset, None, normals_flat, 
+                        values=colors_flat)
+            mesh.shading = 'phong'
+            self.meshes.append(mesh)
+            
+            # Full phong shading
+            offset = np.array([0.7, 0.0, 0.0], 'float32')
+            mesh = Mesh(None, verts+offset, faces, normals, values=colors)
+            mesh.shading = 'phong'
+            self.meshes.append(mesh)
+        
+        def on_draw(self, event):
+            gloo.set_viewport(0, 0, *self.size)
+            for mesh in self.meshes:
+                mesh.draw(self)
+    
+    c = Canvas()
+    c.show()
