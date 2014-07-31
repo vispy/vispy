b5aede267db2cf9c2a920a6ee3b7fdfa9aa9e41f
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-07-31 10:35:10 +0200

    Implemented GLSL-based clipping in the fragment shader.

--- examples/demo/realtime_signals.py
+++ examples/demo/realtime_signals.py
@@ -13,8 +13,8 @@ from vispy import app
 import numpy as np
 import math
 
-nrows = 4
-ncols = 5
+nrows = 12
+ncols = 16
 m = nrows*ncols
 n = 1000
 amplitudes = .1+.2*np.random.rand(m, 1)
@@ -34,13 +34,18 @@ index = np.c_[np.repeat(np.repeat(np.arange(ncols), nrows), n),
 VERT_SHADER = """
 #version 120
 attribute vec2 a_position;
+
 attribute vec2 a_index;
 varying vec2 v_index;
+
 uniform vec2 u_scale;
 uniform vec2 u_size;
 
 attribute vec3 a_color;
-varying vec3 v_color;
+varying vec4 v_color;
+
+varying vec2 v_position;
+varying vec4 v_ab;
 
 void main() {
     float nrows = u_size.x;
@@ -49,24 +54,33 @@ void main() {
     vec2 a = vec2(1./ncols, 1./nrows)*.9;
     vec2 b = vec2(-1+2*(a_index.x+.5)/ncols, -1+2*(a_index.y+.5)/nrows);
     gl_Position = vec4(a*u_scale*a_position+b, 0.0, 1.0);
-    v_color = a_color;
+    v_color = vec4(a_color, 1.);
     v_index = a_index;
     
-    // TODO: allow simultaneous zoom with shader-based clipping.
-    // 1. Transform back the coordinates to the original coordinate system.
-    // 2. If the position is outside [-1,1]^2, pass a fract v_index to
-    //    the fragment shader so as to discard the fragment (clipping).
+    // For clipping test in the fragment shader.
+    v_position = gl_Position.xy;
+    v_ab = vec4(a, b);
 }
 """
 
 FRAG_SHADER = """
 #version 120
-varying vec3 v_color;
+
+varying vec4 v_color;
 varying vec2 v_index;
+
+varying vec2 v_position;
+varying vec4 v_ab;
+
 void main() {
-    gl_FragColor = vec4(v_color, 1.0);
+    gl_FragColor = v_color;
     if ((fract(v_index.x) > 0.) || (fract(v_index.y) > 0.))
-        gl_FragColor.a = 0.;
+        discard;
+      
+    // Clipping test.
+    vec2 test = abs((v_position.xy-v_ab.za)/v_ab.xy);
+    if ((test.x > 1) || (test.y > 1))
+        discard;
 }
 """
 
@@ -92,11 +106,20 @@ class Canvas(app.Canvas):
     def on_resize(self, event):
         self.width, self.height = event.size
         gloo.set_viewport(0, 0, self.width, self.height)
+        
+    def on_mouse_wheel(self, event):
+        dx = np.sign(event.delta[1])*.05
+        scale_x, scale_y = self.program['u_scale']     
+        scale_x_new, scale_y_new = (scale_x * math.exp(2.5*dx), 
+                                    scale_y * math.exp(0.0*dx))
+        self.program['u_scale'] = (max(1, scale_x_new), max(1, scale_y_new))
+        self.update()
 
     def on_timer(self, event):
         y = position[:,1].reshape((m, n))
-        y[:,:-10] = y[:,10:]
-        y[:,-10:] = amplitudes * np.random.randn(m, 10)
+        k = 10
+        y[:,:-k] = y[:,k:]
+        y[:,-k:] = amplitudes * np.random.randn(m, k)
         
         position[:,1] = y.ravel()
         self.program['a_position'].set_data(position)
