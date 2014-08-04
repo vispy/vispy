9493c5f7a391c872197676ead430034c3d0b8ebc
Luke Campagnola <luke.campagnola@gmail.com>
2014-08-03 21:14:41 -0400

    Fixed grid example

--- examples/scene/grid.py
+++ examples/scene/grid.py
@@ -24,19 +24,16 @@ def update_grid(event=None):
 update_grid()
 
 b1 = grid.add_view(row=0, col=0, col_span=2)
-b1.viewer.rect = (-0.5, -5), (11, 10)
+b1.camera.rect = (-0.5, -5), (11, 10)
 b1.border = (1, 0, 0, 1)
-b1.preferred_clip_method = 'viewport'
 
 b2 = grid.add_view(row=1, col=0)
-b2.viewer.rect = (-10, -5), (15, 10)
+b2.camera.rect = (-10, -5), (15, 10)
 b2.border = (1, 0, 0, 1)
-b2.preferred_clip_method = 'viewport'
 
 b3 = grid.add_view(row=1, col=1)
-b3.viewer.rect = (-5, -5), (10, 10)
+b3.camera.rect = (-5, -5), (10, 10)
 b3.border = (1, 0, 0, 1)
-b3.preferred_clip_method = 'viewport'
 
 
 # Add one line to all three boxes
@@ -51,9 +48,6 @@ color[:, 0] = np.linspace(0, 1, N)
 color[:, 1] = color[::-1, 0]
 
 l1 = scene.visuals.Line(pos=pos, color=color)
-#l1.transform = scene.transforms.AffineTransform()
-#l1.transform.scale((10, 1))
-#l1.transform.translate((20, 100))
 
 b1.add(l1)
 
@@ -66,16 +60,6 @@ tr2 = scene.visuals.Visual()
 tr2.transform = scene.transforms.PolarTransform()
 l1.add_parent(tr2)
 b3.add(tr2)
-
-
-# add image to b1
-#img_data = np.random.normal(size=(100, 100, 3), loc=128,
-#                            scale=50).astype(np.ubyte)
-
-#image = scene.visuals.Image(img_data)
-#image.transform = scene.transforms.AffineTransform()
-#image.transform.scale((1, 1))
-#b1.add(image)
 
 import sys
 if sys.flags.interactive == 0:
