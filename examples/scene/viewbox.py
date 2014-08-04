6b825f36fdd011893629194795705aa57939de06
Luke Campagnola <luke.campagnola@gmail.com>
2014-08-04 08:12:09 -0400

    Renamed examples

--- examples/scene/viewbox.py
+++ examples/scene/viewbox.py
@@ -1,49 +1,14 @@
 """
-Simple test of stacking viewboxes, demonstrating the three methods that
-can be used by a viewbox to provide clipping.
+Demonstrate ViewBox using various clipping methods.
 
-There is one root scene with an NDC camera. It contains two viewboxes.
-One with an NDC camera on the left, and one with a pixel camera on the
-right. Each of these viewboxes contains again two viewboxes. One with
-ndc camera at the bottom, and one with a pixel camera at the top.
-
-Use the global PREFER_PIXEL_GRID variables to set the clipping method
-for the two toplevel and four leaf viewbox, respectively. You can also
-manyally set the preferred_clip_method property of one or more viewboxes.
-
-This is what it should look like:
-
-The line in pixel coordinates is normally expected to have the marker
-up (since the y-axis points down). The line in 2D unit coordinates is
-normally expected to have the marker down (since the y-axis is up). But
-positioning a Viewbox is a UnitCamera2 will make it upside down.
-
-  vb1 uses NormalCamera           vb2 uses PixelCamera
-  so contents are upside-down     so contents are correct
-_______________________________________________________________
-|                               |                               |
-| long line with marker down    | short line with marker down   |
-|_______________________________|_______________________________|
-|                               |                               |
-| short line with marker up     | long line with marker up      |
-|_______________________________|_______________________________|
+Two boxes are manually positioned on the canvas; they are not updated
+when the canvas resizes.
 
 """
-
 import numpy as np
 
 from vispy import app
 from vispy import scene
-
-#gloo.gl.use('desktop debug')
-
-# <<< Change method here
-# With the none method you can see the absence of clipping.
-# With the fbo method you can see the texture interpolation (induced by
-# a delibirate mismatch in screen and textue resolution)
-# Try different combinarions, like a viewport in an fbo
-PREFER_PIXEL_GRID1 = 'viewport'  # none, viewport, fbo (fragment to come)
-PREFER_PIXEL_GRID2 = 'fbo'
 
 
 # Create lines for use in ndc and pixel coordinates
@@ -52,88 +17,63 @@ color = np.ones((N, 4), dtype=np.float32)
 color[:, 0] = np.linspace(0, 1, N)
 color[:, 1] = color[::-1, 0]
 pos = np.empty((N, 2), np.float32)
-#
+
 pos[:, 0] = np.linspace(-1., 1., N)
 pos[:, 1] = np.random.normal(0.0, 0.5, size=N)
 pos[:20, 1] = -0.5  # So we can see which side is down
 line_ndc = scene.visuals.Line(pos=pos.copy(), color=color)
-#
-pos[:, 0] = np.linspace(50, 350., N)
-pos[:, 1] = 150 + pos[:, 1] * 50
-pos[:20, 1] = 100  # So we can see which side is down
-line_pixels = scene.visuals.Line(pos=pos.copy(), color=color)
+
+#pos[:, 0] = np.linspace(50, 350., N)
+#pos[:, 1] = 150 + pos[:, 1] * 50
+#pos[:20, 1] = 100  # So we can see which side is down
+#line_pixels = scene.visuals.Line(pos=pos.copy(), color=color)
 
 # Create canvas
 canvas = scene.SceneCanvas(size=(800, 600), show=True, close_keys='escape')
-#canvas.scene.camera = scene.cameras.PixelCamera()
 
-# Create viewboxes left ...
+vb1 = scene.widgets.ViewBox(parent=canvas.scene, border=(1, 1, 0, 1))
+vb1.clip_method = 'fbo'
+vb1.pos = 20, 20
+vb1.size = canvas.size[0]/2. - 40, canvas.size[1] - 40
+vb1.camera.rect = (-1.2, -2, 2.4, 4)
 
-w, h = canvas.size
-w2 = w / 2.
-h2 = h / 2.
+line_ndc.add_parent(vb1.scene)
 
-vb1 = scene.widgets.ViewBox(parent=canvas.scene, name='vb1', margin=2, border=(1, 0, 0, 1))
-vb1.pos = 0, 0
-vb1.size = w2, h
-vb1.camera.rect = (-1, -1), (2, 2)
-vb1.camera.interactive = False
+vb2 = scene.widgets.ViewBox(parent=canvas.scene, border=(0, 0, 1, 1))
+vb2.clip_method = 'viewport'
+vb2.pos = canvas.size[0]/2. + 20, 20
+vb2.size = canvas.size[0]/2. - 40, canvas.size[1] - 40
+vb2.set_camera('turntable', mode='ortho', elevation=30, azimuth=30)
+#vb2.set_camera('turntable', mode='perspective', 
+#               distance=10, elevation=0, azimuth=0)
 
-vb11 = scene.widgets.ViewBox(parent=vb1.scene, name='vb11', margin=0.02, border=(0, 1, 0, 1))
-vb11.pos = -1.0, -1.0
-vb11.size = 2.0, 1.0
-vb11.camera.rect = (-2, -2), (4, 4)
+line_ndc.add_parent(vb2.scene)
 
-vb12 = scene.widgets.ViewBox(parent=vb1.scene, name='vb12', margin=0.02, border=(0, 0, 1, 1))
-vb12.pos = -1.0, 0.0
-vb12.size = 2.0, 1.0
-vb12.camera.rect = (0, 0), (400, 300)
+box = np.array([[0, 0, 0], 
+                [0, 1, 0], 
+                [1, 1, 0], 
+                [1, 0, 0], 
+                [0, 0, 0]], dtype=np.float32)
+z = np.array([[0, 0, 1]], dtype=np.float32)
+unit_box1 = scene.visuals.Line(pos=box, 
+                               color=(0.7, 0, 0, 1), 
+                               name='unit box')
+nd_box1 = scene.visuals.Line(pos=(box * 2 - 1), 
+                             color=(0, 0.7, 0, 1), 
+                             name='nd box')
+vb1.add(unit_box1)
+vb1.add(nd_box1)
 
-line_ndc.add_parent(vb11.scene)
-line_pixels.add_parent(vb12.scene)
-
-box = np.array([[0, 0], [0, 1], [1, 1], [1, 0], [0, 0]], dtype=np.float32)
-unit_box = scene.visuals.Line(pos=box, color=(1, 0, 0, 1), name='unit box')
-nd_box = scene.visuals.Line(pos=box*2-1, color=(0, 1, 0, 1), name='nd box')
-vb11.add(unit_box)
-vb11.add(nd_box)
-
-# Create viewboxes right ...
-
-vb2 = scene.widgets.ViewBox(parent=canvas.scene, name='vb2', margin=2, border=(1, 1, 0, 1))
-vb2.pos = w2, 0
-vb2.size = w2, h
-vb2.camera.rect = (0, 0), vb2.size
-vb2.camera.interactive = False
-
-vb21 = scene.widgets.ViewBox(parent=vb2.scene, name='vb21', margin=10, border=(1, 0, 1, 1))
-vb21.pos = 0, 0
-vb21.size = 400, 300
-vb21.camera.rect = (-1, -1), (2, 2)
-
-vb22 = scene.widgets.ViewBox(parent=vb2.scene, name='vb22', margin=10, border=(0, 1, 1, 1))
-vb22.pos = 0, 300
-vb22.size = 400, 300
-vb22.camera.rect = (0, 0), vb22.size
-
-line_ndc.add_parent(vb21.scene)
-line_pixels.add_parent(vb22.scene)
-
-# Set preferred pixel grid method
-for vb in [vb1, vb2]:
-    vb.preferred_clip_method = PREFER_PIXEL_GRID1
-for vb in [vb11, vb12, vb21, vb22]:
-    vb.preferred_clip_method = PREFER_PIXEL_GRID2
-
-
-# For testing/dev
-vb1._name = 'vb1'
-vb11._name = 'vb11'
-vb12._name = 'vb12'
-vb2._name = 'vb2'
-vb21._name = 'vb21'
-vb22._name = 'vb22'
-
+unit_box2 = scene.visuals.Line(pos=box + z, 
+                               color=(1, 0, 0, 1), 
+                               name='unit box')
+nd_box2 = scene.visuals.Line(pos=((box + z) * 2 - 1), 
+                             color=(0, 1, 0, 1), 
+                             name='nd box')
+vb2.add(unit_box1)
+vb2.add(nd_box1)
+vb2.add(unit_box2)
+vb2.add(nd_box2)
 
 if __name__ == '__main__':
     app.run()
