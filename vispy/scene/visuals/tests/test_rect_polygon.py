362a763398c0bf757dc1043358d073deee3ae967
M S Suraj <mssurajkaiga@gmail.com>
2014-08-20 22:56:09 +0530

    Added basic RectPolygon visual tests without reactive-check

---
+++ vispy/scene/visuals/tests/test_rect_polygon.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for RectPolygonVisual
+All images are of size (100,100) to keep a small file size
+"""
+
+from vispy import gloo
+from vispy.scene import visuals, transforms
+from vispy.testing import (requires_application, assert_image_equal,
+                           TestingCanvas)
+
+
+@requires_application()
+def test_rectangle_draw():
+    """Test drawing rectpolygons without transform using RectPolygonVisual"""
+    with TestingCanvas() as c:
+        rectpolygon = visuals.RectPolygon(pos=(50, 50, 0), height=40.,
+                                          width=80., color='red')
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon1.png')
+
+        gloo.clear()
+        rectpolygon = visuals.RectPolygon(pos=(50, 50, 0), height=40.,
+                                          width=80., radius=10.,
+                                          color='red')
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon2.png')
+
+        gloo.clear()
+        rectpolygon = visuals.RectPolygon(pos=(50, 50, 0), height=40.,
+                                          width=80., radius=10.,
+                                          color='red',
+                                          border_color=(0, 1, 1, 1))
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon3.png')
+
+        gloo.clear()
+        rectpolygon = visuals.RectPolygon(pos=(50, 50, 0), height=40.,
+                                          width=80., radius=10.,
+                                          border_color='white')
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon4.png')
+
+        gloo.clear()
+        rectpolygon = visuals.RectPolygon(pos=(50, 50, 0), height=60.,
+                                          width=80., radius=[25, 10, 0, 15],
+                                          color='red',
+                                          border_color=(0, 1, 1, 1))
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon5.png')
+
+
+@requires_application()
+def test_rectpolygon_draw():
+    """Test drawing transformed rectpolygons using RectPolygonVisual"""
+    with TestingCanvas() as c:
+        rectpolygon = visuals.RectPolygon(pos=(0., 0.), height=20.,
+                                          width=20., radius=10.,
+                                          color='blue')
+        rectpolygon.transform = transforms.STTransform(scale=(2.0, 3.0),
+                                                       translate=(50, 50))
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon6.png')
+
+        gloo.clear()
+        rectpolygon = visuals.RectPolygon(pos=(0., 0.), height=20.,
+                                          width=20., radius=10.,  
+                                          color='blue',
+                                          border_color='red')
+        rectpolygon.transform = transforms.STTransform(scale=(2.0, 3.0),
+                                                       translate=(50, 50))
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon7.png')
+
+        gloo.clear()
+        rectpolygon = visuals.RectPolygon(pos=(0., 0.), height=60.,
+                                          width=60., radius=10.,
+                                          border_color='red')
+        rectpolygon.transform = transforms.STTransform(scale=(1.5, 0.5),
+                                                       translate=(50, 50))
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon8.png')
+
+        gloo.clear()
+        rectpolygon = visuals.RectPolygon(pos=(0., 0.), height=60.,
+                                          width=60., radius=[25, 10, 0, 15],
+                                          color='blue',
+                                          border_color='red')
+        rectpolygon.transform = transforms.STTransform(scale=(1.5, 0.5),
+                                                       translate=(50, 50))
+        c.draw_visual(rectpolygon)
+        assert_image_equal("screenshot", 'visuals/rectpolygon9.png')
