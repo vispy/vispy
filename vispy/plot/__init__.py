f96d87a850fd012c2ae42350605e03ae063a6aa4
Luke Campagnola <luke.campagnola@gmail.com>
2014-08-25 01:48:17 -0400

    update vispy.plot

--- vispy/plot/__init__.py
+++ vispy/plot/__init__.py
@@ -1,3 +1,14 @@
-from .plot import *    
-    
-    
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+
+"""
+[Experimental] This module provides functions for displaying data from a 
+command-line interface. 
+"""
+
+from .plot import *
+from ..scene.visuals import *    
+from ..scene.widgets import *    
+from ..scene.transforms import *
+from ..scene.cameras import *    
