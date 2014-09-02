2cda08f64e2f36e844e37121710111ebc709667e
irwin <hi@irwinzaid.com>
2014-09-02 21:13:20 +0100

    Created a Volume visual

--- vispy/scene/visuals/__init__.py
+++ vispy/scene/visuals/__init__.py
@@ -15,7 +15,7 @@ images, text, etc.
 
 __all__ = ['Visual', 'Ellipse', 'GridLines', 'Image', 'Line', 'LinePlot',
            'Markers', 'marker_types', 'Mesh', 'Polygon', 'Rectangle',
-           'RegularPolygon', 'SurfacePlot', 'Text', 'XYZAxis']
+           'RegularPolygon', 'SurfacePlot', 'Text', 'Volume', 'XYZAxis']
 
 from .visual import Visual  # noqa
 from .line import Line  # noqa
@@ -31,5 +31,6 @@ from .gridlines import GridLines  # noqa
 from .surface_plot import SurfacePlot  # noqa
 from .isosurface import Isosurface  # noqa
 from .isocurve import Isocurve  # noqa
+from .volume import Volume  # noqa
 from .xyz_axis import XYZAxis  # noqa
 from .line_plot import LinePlot  # noqa
