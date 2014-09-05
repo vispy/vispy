e9df43dec8c9e052bf7ecaa5b11afcb5cd20ce13
sylm21 <sylvain.moyne@ensta-bretagne.fr>
2014-09-05 16:07:17 +0200

    Update primitive_mesh_viewer_qt.py

--- examples/demo/gloo/primitive_mesh_viewer_qt.py
+++ examples/demo/gloo/primitive_mesh_viewer_qt.py
@@ -13,14 +13,8 @@ Test the fps capability of Vispy with meshdata primitive
 """
 try:
     from sip import setapi
-    setapi("QDate", 2)
-    setapi("QDateTime", 2)
-    setapi("QTextStream", 2)
-    setapi("QTime", 2)
     setapi("QVariant", 2)
-    setapi("QString", 2)
-    setapi("QUrl", 2)
-except:
+except ImportError:
     pass
 
 from PyQt4 import QtGui, QtCore
