aee290551d8f0950c314428b7845f7b0462853bd
Almar Klein <almar.klein@gmail.com>
2014-09-05 09:46:48 +0200

    Fix tutorial examples
    Closes #525

--- examples/tutorial/gloo/textured_quad.py
+++ examples/tutorial/gloo/textured_quad.py
@@ -29,9 +29,9 @@ fragment = """
 
 
 def checkerboard(grid_num=8, grid_size=32):
-    row_even = grid_num / 2 * [0, 1]
-    row_odd = grid_num / 2 * [1, 0]
-    Z = np.row_stack(grid_num / 2 * (row_even, row_odd)).astype(np.uint8)
+    row_even = grid_num // 2 * [0, 1]
+    row_odd = grid_num // 2 * [1, 0]
+    Z = np.row_stack(grid_num // 2 * (row_even, row_odd)).astype(np.uint8)
     return 255 * Z.repeat(grid_size, axis=0).repeat(grid_size, axis=1)
 
 
--- vispy/gloo/variable.py
+++ vispy/gloo/variable.py
@@ -229,7 +229,8 @@ class Uniform(Variable):
         if self._gtype in (gl.GL_SAMPLER_2D, GL_SAMPLER_3D):
             #gl.glActiveTexture(gl.GL_TEXTURE0 + self._unit)
             if self.data is not None:
-                self.data.deactivate()
+                if isinstance(self._data, BaseTexture):
+                    self.data.deactivate()
 
     def _update(self):
 
