966a95e82f13b4350358ae62a191acc00a21a112
Eric Larson <larson.eric.d@gmail.com>
2014-09-05 09:36:34 -0700

    FIX: Fix skip

--- examples/demo/gloo/primitive_mesh_viewer_qt.py
+++ examples/demo/gloo/primitive_mesh_viewer_qt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vispy: testskip
 # -----------------------------------------------------------------------------
 # Copyright (c) 2014, Vispy Development Team. All Rights Reserved.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
