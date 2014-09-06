b7bf80906ebcdb8ca6d04a9a53bad2028e8b4485
sylm21 <sylvain.moyne@ensta-bretagne.fr>
2014-09-06 21:44:28 +0200

    some modifications ...

--- vispy/geometry/tests/test_bbox.py
+++ vispy/geometry/tests/test_bbox.py
@@ -19,8 +19,17 @@ def test_bbox():
     point = np.array([0., 0., 0.])
     cdist_res = np.array([0., 3., 3.])
     center_bsphere_res = np.array([1.5,  1.5,  0.])
-    radius_bsphere_res = 2.1213203435596424
+    # radius_bsphere_res = 2.1213203435596424
 
+    assert_array_equal(_bbox(vertices), bbox_res)
+    assert_array_equal(_mean_center(vertices), center_res)
+    assert_array_equal(_distance_from_point(point, vertices), cdist_res)
+    center, radius = _bsphere(vertices)
+    assert_array_equal(center, center_bsphere_res)
+    center, radius = _besphere(vertices)
+    assert_array_equal(center, center_bsphere_res)
+    # verices in 4D
+    vertices = np.array([[0., 0., 0., 5.], [3., 0., 0., 5.], [0., 3., 0., 5.]])
     assert_array_equal(_bbox(vertices), bbox_res)
     assert_array_equal(_mean_center(vertices), center_res)
     assert_array_equal(_distance_from_point(point, vertices), cdist_res)
