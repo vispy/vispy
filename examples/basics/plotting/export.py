45515de3204f14703b26324dc84161817b44891f
Luke Campagnola <luke.campagnola@gmail.com>
2014-09-08 23:16:58 -0400

    Example corrections for unit testing

--- examples/basics/plotting/export.py
+++ examples/basics/plotting/export.py
@@ -1,28 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+
+import sys
 import vispy.plot as vp
 
 # Create a canvas showing plot data
-plt = vp.plot([1, 6, 2, 4, 3, 8, 5, 7, 6, 3])
+canvas = vp.plot([1, 6, 2, 4, 3, 8, 5, 7, 6, 3])
 
 # Render the canvas scene to a numpy array image with higher resolution 
 # than the original canvas
 scale = 4
-image = plt.render(size=(plt.size[0]*scale, plt.size[1]*scale))
+image = canvas.render(size=(canvas.size[0]*scale, canvas.size[1]*scale))
 
 # Display the data in the array, sub-sampled down to the original canvas
 # resolution
 image = image[::scale, ::scale]
-plt2 = vp.image(image)
+canvas2 = vp.image(image)
 
 # By default, the view adds some padding when setting its range.
 # We'll remove that padding so the image looks exactly like the original 
 # canvas:
-plt2.view.camera.auto_zoom(plt2.image, padding=0)
+canvas2.view.camera.auto_zoom(canvas2.image, padding=0)
 
 # Now take a screenshot. This requires having drawn the canvas at least once,
 # so we need to call process_events first.
-plt.app.process_events()
-image = plt.screenshot()
+canvas.app.process_events()
+image = canvas.screenshot()
 
-import sys
-if sys.flags.interactive == 0:
-    plt.app.run()
+if __name__ == '__main__' and sys.flags.interactive == 0:
+    canvas.app.run()
