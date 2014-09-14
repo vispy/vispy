6254a8b64bf2b22758da1540a01fede99746d9a9
Eric Larson <larson.eric.d@gmail.com>
2014-09-13 22:29:59 -0700

    FIX: OSX works

--- vispy/util/dpi/_quartz.py
+++ vispy/util/dpi/_quartz.py
@@ -4,7 +4,12 @@
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
 
+from ...ext.cocoapy import quartz
+
 
 def get_dpi():
     """Get screen DPI from the OS"""
-    raise NotImplementedError
+    display = quartz.CGMainDisplayID()
+    mm = quartz.CGDisplayScreenSize(display)
+    px = quartz.CGDisplayBounds(display).size
+    return (px.width/mm.width + px.height/mm.height) * 0.5 * 25.4
