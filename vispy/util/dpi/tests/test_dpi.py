a042957e5c688a054db03ca582935e593cd9fbe8
Eric Larson <larson.eric.d@gmail.com>
2014-09-13 22:32:22 -0700

    FIX: Fix for Python 2.6

--- vispy/util/dpi/tests/test_dpi.py
+++ vispy/util/dpi/tests/test_dpi.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2014, Vispy Development Team.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 
-from nose.tools import assert_true, assert_is_instance
+from nose.tools import assert_true
 
 from vispy.util.dpi import get_dpi
 from vispy.testing import run_tests_if_main
@@ -12,7 +12,7 @@ def test_dpi():
     """Test dpi support"""
     dpi = get_dpi()
     assert_true(dpi > 0.)
-    assert_is_instance(dpi, float)
+    assert_true(isinstance(dpi, float))
 
 
 run_tests_if_main()
