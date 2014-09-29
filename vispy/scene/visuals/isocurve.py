58c54aea38c429a06b01d19cad3815023cbc177b
sylm21 <sylvain.moyne@ensta-bretagne.fr>
2014-09-29 08:54:57 +0200

    Correct import

--- vispy/scene/visuals/isocurve.py
+++ vispy/scene/visuals/isocurve.py
@@ -7,7 +7,7 @@ from __future__ import division
 import numpy as np
 
 from .line import Line
-from ...geometry.isocurve import isocurve, isocurve_for_trisurface
+from ...geometry.isocurve import isocurve
 
 
 class Isocurve(Line):
