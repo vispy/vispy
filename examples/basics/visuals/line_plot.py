bfde61418f933a7f2dbcfeac2c9a7936cda6a9fb
Luke Campagnola <luke.campagnola@gmail.com>
2014-10-01 14:57:32 -0400

    Clean up lineplotvisual example

--- examples/basics/visuals/line_plot.py
+++ examples/basics/visuals/line_plot.py
@@ -10,8 +10,7 @@ Simple demonstration of LinePlot visual.
 import numpy as np
 import sys
 
-import vispy.app
-from vispy import visuals
+from vispy import gloo, app, visuals
 
 # vertex positions of data to draw
 N = 20
@@ -20,15 +19,20 @@ pos[:, 0] = np.linspace(10, 790, N)
 pos[:, 1] = np.random.normal(size=N, scale=100, loc=400)
 
 
-class Canvas(vispy.app.Canvas):
+class Canvas(app.Canvas):
     def __init__(self):
-        self.line = visuals.LinePlot(pos, color='w', edge_color='w',
-                                     face_color=(0.2, 0.2, 1))
-        vispy.app.Canvas.__init__(self, keys='interactive',
-                                         size=(800, 800), show=True)
+        self.line = visuals.LinePlotVisual(pos, color='w', edge_color='w',
+                                           face_color=(0.2, 0.2, 1))
+        app.Canvas.__init__(self, keys='interactive',
+                            size=(800, 800), show=True)
+
+    def on_draw(self, event):
+        gloo.clear('black')
+        tr_sys = visuals.transforms.TransformSystem(self)
+        self.line.draw(tr_sys)
 
 
 if __name__ == '__main__':
     win = Canvas()
     if sys.flags.interactive != 1:
-        vispy.app.run()
+        app.run()
