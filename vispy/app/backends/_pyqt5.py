6b7476b97c4ef05ac569db20e34f80f140e56e0b
Almar Klein <almar.klein@gmail.com>
2014-10-01 16:03:20 +0200

    Add PyQt5 backend.
    Luke already did most of the work for this. I only added
    the necessary backend module.
    
    Someone should test this (on Windows).

---
+++ vispy/app/backends/_pyqt5.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+
+""" PyQt5 proxy backend for the qt backend. 
+"""
+
+import sys
+from .. import backends
+from ...util import logger
+
+
+try:
+    # Make sure no conflicting libraries have been imported.
+    for lib in ['PySide', 'PyQt4']:
+        if lib in sys.modules:
+            raise RuntimeError("Refusing to import PySide because %s is "
+                               "already imported." % lib)
+    # Try importing
+    from PyQt5 import QtGui, QtCore, QtOpenGL  # noqa
+except Exception as exp:
+    # Fail: this backend cannot be used
+    available, testable, why_not, which = False, False, str(exp), None
+else:
+    # Success
+    available, testable, why_not = True, True, None
+    has_uic = True
+    which = ('PyQt5', QtCore.PYQT_VERSION_STR, QtCore.QT_VERSION_STR)
+    # Remove _qt module to force an import even if it was already imported
+    sys.modules.pop(__name__.replace('_pyqt5', '_qt'), None)
+    # Import _qt. Keep a ref to the module object!
+    if backends.qt_lib is None:
+        backends.qt_lib = 'pyqt5'  # Signal to _qt what it should import
+        from . import _qt  # noqa
+        from ._qt import *  # noqa
+    else:
+        logger.info('%s already imported, cannot switch to %s'
+                    % (backends.qt_lib, 'pyqt5'))
