c84b96cefb793d5d4e9369c639e42733675cd438
Luke Campagnola <luke.campagnola@gmail.com>
2014-10-15 23:35:53 -0400

    fixed line_update example

--- examples/basics/scene/line_update.py
+++ examples/basics/scene/line_update.py
@@ -8,7 +8,7 @@ Demonstration of animated Line visual.
 
 import numpy as np
 import vispy.app
-from vispy import visuals
+from vispy import scene
 
 # vertex positions of data to draw
 N = 200
@@ -21,26 +21,21 @@ color = np.ones((N, 4), dtype=np.float32)
 color[:, 0] = np.linspace(0, 1, N)
 color[:, 1] = color[::-1, 0]
 
+canvas = scene.SceneCanvas(keys='interactive', size=(800, 800), show=True)
 
-class Canvas(vispy.app.Canvas):
-    def __init__(self):
-        vispy.app.Canvas.__init__(self, keys='interactive',
-                                  size=(800, 800), show=True)
-        self.line = visuals.Line(pos, color, parent=self.scene)
-        self.line.events.update.connect(lambda evt: self.update)
+line = scene.Line(pos, color, parent=canvas.scene)
 
 
-if __name__ == '__main__':
-    win = Canvas()
+def update(ev):
+    global pos, color, line
+    pos[:, 1] = np.random.normal(size=N, scale=100, loc=400)
+    color = np.roll(color, 1, axis=0)
+    line.set_data(pos=pos, color=color)
 
-    def update(ev):
-        pos[:, 1] = np.random.normal(size=N, scale=100, loc=400)
-        win.line.set_data(pos=pos)
+timer = vispy.app.Timer()
+timer.connect(update)
+timer.start(0)
 
-    timer = vispy.app.Timer()
-    timer.connect(update)
-    timer.start(0)
-
-    import sys
-    if sys.flags.interactive != 1:
-        vispy.app.run()
+import sys
+if sys.flags.interactive != 1:
+    vispy.app.run()
