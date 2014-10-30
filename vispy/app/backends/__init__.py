bfa58e512adf36e77a87909d9f441f71e6b6af8b
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-10-30 22:38:04 +0100

    Preparing for review.

--- vispy/app/backends/__init__.py
+++ vispy/app/backends/__init__.py
@@ -27,8 +27,8 @@ CORE_BACKENDS = [
 # Whereas core backends really represents libraries that can create a
 # canvas, the pseudo backends act more like a proxy.
 PSEUDO_BACKENDS = [
-    ('ipynb_vnc', '_ipynb_vnc', None),
-    ('ipynb_static', '_ipynb_static', None),
+    # ('ipynb_vnc', '_ipynb_vnc', None),
+    # ('ipynb_static', '_ipynb_static', None),
     ('ipynb_webgl', '_ipynb_webgl', None),
     ('_test', '_test', 'vispy.app.backends._test'),  # add one that will fail
 ]
