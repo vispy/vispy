8785a2683a004de46637c0634af5b55247b1309a
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-11-12 14:42:47 +0100

    WIP: message serialization.

--- vispy/util/serialization.py
+++ vispy/util/serialization.py
@@ -9,7 +9,7 @@ import base64
 
 import numpy as np
 
-from ..ext.six import string_types
+from ..ext.six import string_types, iteritems
 
 
 # -----------------------------------------------------------------------------
@@ -35,51 +35,115 @@ def _extract_buffers(commands):
     return commands_modified, buffers
 
 
+def _serialize_item(item):
+    """Internal function: serialize native types."""
+    # Recursively serialize lists, tuples, and dicts.
+    if isinstance(item, (list, tuple)):
+        return [_serialize_item(subitem) for subitem in item]
+    elif isinstance(item, dict):
+        return {key: _serialize_item(value)
+                for (key, value) in iteritems(item)}
+
+    # Serialize strings.
+    elif isinstance(item, string_types):
+        # Replace glSomething by something (needed for WebGL commands).
+        if item.startswith('gl'):
+            return re.sub(r'^gl([A-Z])', lambda m: m.group(1).lower(), item)
+        else:
+            return item
+
+    # Process NumPy arrays that are not buffers (typically, uniform values).
+    elif isinstance(item, np.ndarray):
+        return _serialize_item(item.ravel().tolist())
+
+    # Serialize numbers.
+    else:
+        try:
+            return np.asscalar(item)
+        except Exception:
+            return item
+
+
 def _serialize_command(command_modified):
     """Serialize a single GLIR (modified) command. The modification relates
     to the fact that buffers are replaced by pointers."""
-    # TODO
-    return
+    return _serialize_item(command_modified)
 
 
-def create_glir_message(commands):
+def _serialize_buffer(buffer, array_serialization=None):
+    """Serialize a NumPy array."""
+    if array_serialization == 'binary':
+        return {'storage_type': 'binary',
+                'buffer': buffer.ravel().tobytes()
+                }
+    elif array_serialization == 'base64':
+        return {'storage_type': 'base64',
+                'buffer': base64.b64encode(buffer).decode('ascii')
+                }
+    raise ValueError("The array serialization method should be 'binary' or "
+                     "'base64'.")
+
+
+def create_glir_message(commands, array_serialization=None):
+    """Create a JSON-serializable message of GLIR commands. NumPy arrays
+    are serialized according to the specified method.
+
+    Arguments
+    ---------
+
+    commands : list
+        List of GLIR commands.
+    array_serialization : string or None
+        Serialization method for NumPy arrays. Possible values are:
+            'binary' (default) : use a binary string
+            'base64' : base64 encoded string of the array
+
+    """
+    # Default serialization method for NumPy arrays.
+    if array_serialization is None:
+        array_serialization = 'binary'
+    # Extract the buffers.
     commands_modified, buffers = _extract_buffers(commands)
+    # Serialize the modified commands (with buffer pointers) and the buffers.
     commands_serialized = [_serialize_command(command_modified)
                            for command_modified in commands_modified]
+    buffers_serialized = [_serialize_buffer(buffer, array_serialization)
+                          for buffer in buffers]
+    # Create the final message.
     msg = {
         'msg_type': 'glir_commands',
         'commands': commands_serialized,
-        'buffers': buffers,
+        'buffers': buffers_serialized,
     }
     return msg
 
 
-def _serializable(c, serialize_array=True):
-    if isinstance(c, list):
-        return [_serializable(command, serialize_array=serialize_array)
-                for command in c]
-    if isinstance(c, tuple):
-        if c and c[0] == 'UNIFORM':
-            serialize_array = False
-        return list(_serializable(command, serialize_array=serialize_array)
-                    for command in c)
-    elif isinstance(c, np.ndarray):
-        if serialize_array:
-            # TODO: binary websocket (once the IPython PR has been merged)
-            return {
-                'storage_type': 'base64',
-                'buffer': base64.b64encode(c).decode('ascii'),
-            }
-        else:
-            return _serializable(c.ravel().tolist(), False)
-    elif isinstance(c, string_types):
-        # replace glSomething by something (needed for WebGL commands)
-        if c.startswith('gl'):
-            return re.sub(r'^gl([A-Z])', lambda m: m.group(1).lower(), c)
-        else:
-            return c
-    else:
-        try:
-            return np.asscalar(c)
-        except Exception:
-            return c
+# def _serializable(c, serialize_array=True):
+#     if isinstance(c, list):
+#         return [_serializable(command, serialize_array=serialize_array)
+#                 for command in c]
+#     if isinstance(c, tuple):
+#         if c and c[0] == 'UNIFORM':
+#             serialize_array = False
+#         return list(_serializable(command, serialize_array=serialize_array)
+#                     for command in c)
+#     elif isinstance(c, np.ndarray):
+#         if serialize_array:
+#             # TODO: binary websocket (once the IPython PR has been merged)
+#             return {
+#                 'storage_type': 'base64',
+#                 'buffer': base64.b64encode(c).decode('ascii'),
+#             }
+#         else:
+#             return _serializable(c.ravel().tolist(), False)
+#     elif isinstance(c, string_types):
+#         # replace glSomething by something (needed for WebGL commands)
+#         if c.startswith('gl'):
+#             return re.sub(r'^gl([A-Z])', lambda m: m.group(1).lower(), c)
+#         else:
+#             return c
+#     else:
+#         try:
+#             return np.asscalar(c)
+#         except Exception:
+#             return c
--- vispy/util/tests/test_serialization.py
+++ vispy/util/tests/test_serialization.py
@@ -37,4 +37,78 @@ def test_extract_buffers():
     assert_equal(commands_modified, commands_modified_expected)
     assert_equal(buffers, [arr, arr2])
 
+
+def test_serialize_command():
+    command = ('CREATE', 4, 'VertexBuffer')
+    command_serialized = serialization._serialize_command(command)
+    assert_equal(command_serialized, list(command))
+
+    command = ('UNIFORM', 4, 'u_scale', 'vec3', (1, 2, 3))
+    commands_serialized_expected = ['UNIFORM', 4, 'u_scale', 'vec3', [1, 2, 3]]
+    command_serialized = serialization._serialize_command(command)
+    assert_equal(command_serialized, commands_serialized_expected)
+
+
+def test_create_glir_message_binary():
+    arr = np.zeros((3, 2)).astype(np.float32)
+    arr2 = np.ones((4, 5)).astype(np.int16)
+
+    commands = [('CREATE', 1, 'VertexBuffer'),
+                ('UNIFORM', 2, 'u_scale', 'vec3', (1, 2, 3)),
+                ('DATA', 3, 0, arr),
+                ('UNIFORM', 4, 'u_pan', 'vec2', np.array([1, 2, 3])),
+                ('DATA', 5, 20, arr2)]
+    msg = serialization.create_glir_message(commands)
+    assert_equal(msg['msg_type'], 'glir_commands')
+
+    commands_serialized = msg['commands']
+    assert_equal(commands_serialized,
+                 [['CREATE', 1, 'VertexBuffer'],
+                  ['UNIFORM', 2, 'u_scale', 'vec3', [1, 2, 3]],
+                  ['DATA', 3, 0, {'buffer_index': 0}],
+                  ['UNIFORM', 4, 'u_pan', 'vec2', [1, 2, 3]],
+                  ['DATA', 5, 20, {'buffer_index': 1}]])
+
+    buffers_serialized = msg['buffers']
+    buf0 = buffers_serialized[0]
+    assert_equal(buf0['storage_type'], 'binary')
+    assert_equal(buf0['buffer'], b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')  # noqa
+
+    buf1 = buffers_serialized[1]
+    assert_equal(buf0['storage_type'], 'binary')
+    assert_equal(buf1['buffer'], b'\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00')  # noqa
+
+
+def test_create_glir_message_base64():
+    arr = np.zeros((3, 2)).astype(np.float32)
+    arr2 = np.ones((4, 5)).astype(np.int16)
+
+    commands = [('CREATE', 1, 'VertexBuffer'),
+                ('UNIFORM', 2, 'u_scale', 'vec3', (1, 2, 3)),
+                ('DATA', 3, 0, arr),
+                ('UNIFORM', 4, 'u_pan', 'vec2', np.array([1, 2, 3])),
+                ('DATA', 5, 20, arr2)]
+    msg = serialization.create_glir_message(commands,
+                                            array_serialization='base64')
+    assert_equal(msg['msg_type'], 'glir_commands')
+
+    commands_serialized = msg['commands']
+    assert_equal(commands_serialized,
+                 [['CREATE', 1, 'VertexBuffer'],
+                  ['UNIFORM', 2, 'u_scale', 'vec3', [1, 2, 3]],
+                  ['DATA', 3, 0, {'buffer_index': 0}],
+                  ['UNIFORM', 4, 'u_pan', 'vec2', [1, 2, 3]],
+                  ['DATA', 5, 20, {'buffer_index': 1}]])
+
+    buffers_serialized = msg['buffers']
+    buf0 = buffers_serialized[0]
+    assert_equal(buf0['storage_type'], 'base64')
+    assert_equal(buf0['buffer'], 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+
+    buf1 = buffers_serialized[1]
+    assert_equal(buf0['storage_type'], 'base64')
+    assert_equal(buf1['buffer'],
+                 'AQABAAEAAQABAAEAAQABAAEAAQABAAEAAQABAAEAAQABAAEAAQABAA==')
+
+
 run_tests_if_main()
