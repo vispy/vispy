3ff38c36ea6b79e23ea02216bfe4063ef8a0e37d
Alex J. Champandard <alexjc@aigamedev.com>
2014-12-04 11:35:09 +0100

    First pass integration with iPython 3.x.  A new InputHook is defined for VisPy, and it can be enabled from a magic command or by calling the enable_gui() function.  A demo is also included.

---
+++ examples/tutorial/app/interactive.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+"""
+This example shows how to run from iPython in interactive mode, while 
+also updating the VisPy event loop.
+"""
+
+import math
+from vispy import app, gloo
+
+
+class Canvas(app.Canvas):
+
+    def __init__(self, *args, **kwargs):
+        app.Canvas.__init__(self, *args, **kwargs)
+        self._timer = app.Timer('auto', connect=self.on_timer, start=True)
+        self.tick = 0
+        self.color = (1.0, 1.0, 1.0)
+
+    def on_draw(self, event):
+        gloo.clear(color=True)
+
+    def on_timer(self, event):
+        # Speed at which the colors animate.
+        self.tick += 0.097
+        s = abs(0.5 + 0.5 * math.sin(self.tick))
+        c = self.color
+        gloo.set_clear_color((c[0] * s, c[1] * s, c[2] * s, 1))
+        self.update()
+
+
+# You should only run this demo as main with ipython -i <file>
+assert __name__ == '__main__'
+
+
+from vispy.ext import inputhook
+inputhook.enable()
+
+
+# All variables listed in this scope are accessible via the console.
+canvas = Canvas(keys='interactive')
+canvas.show()
+
+
+# In iPython, try typing the following:
+#   > canvas.color = (1.0, 0.0, 0.0)
