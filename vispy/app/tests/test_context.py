ad36d9e6fb0a4d824238db91671790ad9c4bfe36
Eric89GXL <larson.eric.d@gmail.com>
2014-12-06 23:10:56 -0800

    FIX: Skip bad mesa test

--- vispy/app/tests/test_context.py
+++ vispy/app/tests/test_context.py
@@ -28,8 +28,9 @@ def test_context_properties():
     for context in contexts:
         n_items = len(context)
         with Canvas(context=context):
-            if os.getenv('TRAVIS', 'false') == 'true':
-                # Travis cannot handle obtaining these values
+            if os.getenv('TRAVIS', 'false') == 'true' or \
+                    os.getenv('APPVEYOR', 'false') == 'true':
+                # Travis and Appveyor cannot handle obtaining these values
                 props = context
             else:
                 props = get_gl_configuration()
