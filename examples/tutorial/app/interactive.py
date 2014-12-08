c43fbeb2160f94ac967b5b858ba3936669e236e3
Alex J. Champandard <alexjc@aigamedev.com>
2014-12-08 20:01:05 +0100

    Variety of minor fixes from the latest review, mostly comments and documentation.  Polished the example.

--- examples/tutorial/app/interactive.py
+++ examples/tutorial/app/interactive.py
@@ -2,12 +2,17 @@
 # Copyright (c) 2014, Vispy Development Team.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 """
-This example shows how to run from iPython in interactive mode, while 
-also updating the VisPy event loop.
+This example shows how to configure VisPy to run from IPython (or Python) in
+interactive mode, while simultaneously updating the VisPy event loop.  This
+behavior is supported by default in all code that calls vispy.app.run(), but
+here it's setup manually.
+
+Run this file with `ipython -i interactive.py` to get a console and a window.
 """
 
 import math
 from vispy import app, gloo
+from vispy.color import Color
 
 
 class Canvas(app.Canvas):
@@ -15,25 +20,28 @@ class Canvas(app.Canvas):
     def __init__(self, *args, **kwargs):
         app.Canvas.__init__(self, *args, **kwargs)
         self._timer = app.Timer('auto', connect=self.on_timer, start=True)
-        self.tick = 0
-        self.color = (1.0, 1.0, 1.0)
+        self.color = 'white'
 
     def on_draw(self, event):
         gloo.clear(color=True)
 
     def on_timer(self, event):
-        # Speed at which the colors animate.
-        self.tick += 0.097
-        s = abs(0.5 + 0.5 * math.sin(self.tick))
-        c = self.color
+        # Animation speed based on global time.
+        t = event.elapsed
+        c = Color(self.color).rgb
+        # Simple sinusoid wave animation.
+        s = abs(0.5 + 0.5 * math.sin(t))
         gloo.set_clear_color((c[0] * s, c[1] * s, c[2] * s, 1))
         self.update()
 
 
-# You should run this demo as main with ipython -i <file>
+# You should run this demo as main with ipython -i <file>.  If interactive
+# mode is not specified, this demo will exit immediately because this demo
+# doesn't call run and relies on the input hook being setup.
 if __name__ == '__main__':
-    from vispy.app import set_interactive
-    set_interactive()
+    from vispy import app
+    # app.use_app('glfw')  # for testing specific backends
+    app.set_interactive()
 
 
 # All variables listed in this scope are accessible via the console.
@@ -41,5 +49,6 @@ canvas = Canvas(keys='interactive')
 canvas.show()
 
 
-# In iPython, try typing the following:
-#   > canvas.color = (1.0, 0.0, 0.0)
+# In IPython, try typing any of the following:
+#   >>> canvas.color = (1.0, 0.0, 0.0)
+#   >>> canvas.color = 'red'
--- vispy/app/inputhook.py
+++ vispy/app/inputhook.py
@@ -3,9 +3,12 @@
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 """
 Support for interactive mode to allow VisPy's event loop to be run alongside
-an console or notebook, without using threads.  This code relies on
-inputhooks built-in to the Python interpreter, and supports IPython too. The
-underlying inputhook implementation is from IPython 3.x.
+a console terminal, without using threads.  This code relies on inputhooks
+built-in to the Python interpreter, and supports IPython too. The underlying
+inputhook implementation is from IPython 3.x.
+
+Note that IPython notebook integration is not supported, as the browser does
+not use Python's PyOS_InputHook functionality.
 """
 
 from ..ext.ipy_inputhook import inputhook_manager, InputHookBase, stdin_ready
@@ -15,7 +18,7 @@ from ..util.ptime import time
 
 
 def set_interactive(enabled=True, app=None):
-    """Activate the iPython hook for VisPy.  If the app is not specified, the
+    """Activate the IPython hook for VisPy.  If the app is not specified, the
     default is used.
     """
     if enabled:
@@ -26,23 +29,27 @@ def set_interactive(enabled=True, app=None):
 
 @inputhook_manager.register('vispy')
 class VisPyInputHook(InputHookBase):
-    """Implementation of an iPython 3.x InputHook for VisPy.  This is registered
-    with the manager, so once imported can be used from the notebook with
-        >> %enable_gui vispy
+    """Implementation of an IPython 3.x InputHook for VisPy.  This is loaded
+    by default when you call vispy.app.run() in a console-based interactive
+    session, but you can also trigger it manually by importing this module
+    then typing:
+        >>> %enable_gui vispy
     """
 
     def enable(self, app=None):
-        """Enable event loop integration with vispy.
+        """Activate event loop integration with this VisPy application.
+
         Parameters
         ----------
-        app :
-           The vispy application that's being used.  If None, then the
+        app : instance of Application
+           The VisPy application that's being used.  If None, then the
            default application is retrieved.
+
         Notes
         -----
-        This methods sets the ``PyOS_InputHook`` for vispy, which allows
-        vispy to integrate with terminal based applications like
-        IPython.
+        This methods sets the ``PyOS_InputHook`` to this implementation,
+        which allows Vispy to integrate with terminal-based applications
+        running in interactive mode (Python or IPython).
         """
 
         from .. import app as _app
