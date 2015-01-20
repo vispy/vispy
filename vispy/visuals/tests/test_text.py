3ca53584d7627ade6aa54599c6ee9db2bb5e1e68
Eric89GXL <larson.eric.d@gmail.com>
2015-01-19 17:55:58 -0800

    FIX: Remove comment [ci skip]

--- vispy/visuals/tests/test_text.py
+++ vispy/visuals/tests/test_text.py
@@ -9,14 +9,10 @@ def test_text():
     """Test basic text support"""
     
     with TestingCanvas(bgcolor='w', size=(92, 92), dpi=92) as c:
-        #c.context.glir.clear()  # Just to be safe
         pos = [92 // 2] * 2
         text = Text('testing', font_size=20, color='k',
                     pos=pos, anchor_x='center', anchor_y='baseline')
         c.draw_visual(text)
-        # This limit seems large, but the images actually match quite well...
-        # TODO: we should probably make more "modes" for assert_image_equal
-        # at some point
         # Test image created in Illustrator CS5, 1"x1" output @ 92 DPI
         assert_image_equal("screenshot", 'visuals/text1.png')
 
