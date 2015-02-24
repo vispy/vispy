0e45c7b3aad855ba44252db8b253280e61b0d6d5
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-02-24 21:35:52 +0000

    Moved tube example canvas out of __name__ check

--- examples/basics/visuals/tube.py
+++ examples/basics/visuals/tube.py
@@ -24,59 +24,59 @@ def torus_knot(p=3, q=4, num=100):
     data[:, 2] = -1*np.sin(q*ts)
     return data
 
+canvas = scene.SceneCanvas(keys='interactive')
+canvas.view = canvas.central_widget.add_view()
+
+points1 = torus_knot(5, 3)[:-1] * 5.
+points1[:, 0] -= 20.
+points1[:, 2] -= 15.
+
+points2 = points1.copy()
+points2[:, 2] += 30.
+
+points3 = points1.copy()
+points3[:, 0] += 41.
+points3[:, 2] += 30
+
+points4 = points1.copy()
+points4[:, 0] += 41.
+
+colors = np.linspace(0, 1, len(points1))
+colors = np.array([hsv_to_rgb(c, 1, 1) for c in colors])
+
+vertex_colors = np.random.random(8 * len(points1))
+vertex_colors = np.array([hsv_to_rgb(c, 1, 1) for c in vertex_colors])
+
+l1 = scene.visuals.Tube(points1, 
+                        shading='flat',
+                        vertex_colors=vertex_colors,
+                        tube_points=8)
+
+l2 = scene.visuals.Tube(points2,
+                        colors=colors,
+                        shading='smooth',
+                        tube_points=8)
+
+l3 = scene.visuals.Tube(points3, colors=colors,
+                        shading='flat',
+                        tube_points=8,
+                        closed=True)
+
+l4 = scene.visuals.Tube(points4, colors=colors,
+                        shading='flat',
+                        tube_points=8,
+                        mode='lines')
+
+canvas.view.add(l1)
+canvas.view.add(l2)
+canvas.view.add(l3)
+canvas.view.add(l4)
+canvas.view.set_camera('turntable', mode='perspective',
+                       up='z', distance=2.9*1.5*np.max(np.max(
+                           points1, axis=0)))
+canvas.show()
+
 if __name__ == '__main__':
-    canvas = scene.SceneCanvas(keys='interactive')
-    canvas.view = canvas.central_widget.add_view()
-
-    points1 = torus_knot(5, 3)[:-1] * 5.
-    points1[:, 0] -= 20.
-    points1[:, 2] -= 15.
-
-    points2 = points1.copy()
-    points2[:, 2] += 30.
-
-    points3 = points1.copy()
-    points3[:, 0] += 41.
-    points3[:, 2] += 30
-
-    points4 = points1.copy()
-    points4[:, 0] += 41.
-
-    colors = np.linspace(0, 1, len(points1))
-    colors = np.array([hsv_to_rgb(c, 1, 1) for c in colors])
-
-    vertex_colors = np.random.random(8 * len(points1))
-    vertex_colors = np.array([hsv_to_rgb(c, 1, 1) for c in vertex_colors])
-
-    l1 = scene.visuals.Tube(points1, 
-                            shading='flat',
-                            vertex_colors=vertex_colors,
-                            tube_points=8)
-
-    l2 = scene.visuals.Tube(points2,
-                            colors=colors,
-                            shading='smooth',
-                            tube_points=8)
-
-    l3 = scene.visuals.Tube(points3, colors=colors,
-                            shading='flat',
-                            tube_points=8,
-                            closed=True)
-
-    l4 = scene.visuals.Tube(points4, colors=colors,
-                            shading='flat',
-                            tube_points=8,
-                            mode='lines')
-
-    canvas.view.add(l1)
-    canvas.view.add(l2)
-    canvas.view.add(l3)
-    canvas.view.add(l4)
-    canvas.view.set_camera('turntable', mode='perspective',
-                           up='z', distance=2.9*1.5*np.max(np.max(
-                               points1, axis=0)))
-    canvas.show()
-
     if sys.flags.interactive != 1:
         canvas.app.run()
 
