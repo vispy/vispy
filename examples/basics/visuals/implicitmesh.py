81ecb696adb25ba7e54bc7ef07ffb8a83753aad9
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-03-22 01:38:33 +0000

    Improved implicit mesh example

--- examples/basics/visuals/implicitmesh.py
+++ examples/basics/visuals/implicitmesh.py
@@ -14,31 +14,39 @@ canvas.view = canvas.central_widget.add_view()
 
 thetas, phis = np.meshgrid(np.linspace(0, np.pi, 100),
                            np.linspace(0, 2*np.pi, 150))
+shape = thetas.shape
+linear_shape = thetas.shape[0] * thetas.shape[1]
 
-harmonic_values = sph_harm(2, 5, phis, thetas).real
-rs = 1. + 0.4*harmonic_values
+for l in range(5):
+    for m in range(l+1):
+        harmonic_values = sph_harm(m, l, phis, thetas).real
+        rs = 1. + 0.4*harmonic_values
 
-xs = rs * np.sin(thetas) * np.cos(phis)
-ys = rs * np.sin(thetas) * np.sin(phis)
-zs = rs * np.cos(thetas)
+        xs = rs * np.sin(thetas) * np.cos(phis)
+        ys = rs * np.sin(thetas) * np.sin(phis)
+        zs = rs * np.cos(thetas)
 
-v_min = np.min(harmonic_values)
-v_max = np.max(harmonic_values)
-scale = 1. / np.max(np.abs([v_min, v_max]))
+        xs -= 4.25
+        zs -= 4.25
+        xs += 2.5 * l
+        zs += 2.5 * m
 
-length = xs.shape[0] * xs.shape[1]
-cm = get_colormap('hot')
-col_vals = 0.5 * harmonic_values * scale + 0.5
-colors = cm[col_vals.reshape(length)].rgb.reshape((xs.shape[0],
-                                                   xs.shape[1],
-                                                   3))
+        v_min = np.min(harmonic_values)
+        v_max = np.max(harmonic_values)
+        scale = 1. / np.max(np.abs([v_min, v_max]))
 
-mesh = scene.visuals.ImplicitMesh(xs, ys, zs, color=colors)
+        cm = get_colormap('hot')
+        harmonic_values *= 0.5*scale
+        harmonic_values += 0.5
+        colors = cm[harmonic_values.reshape(linear_shape)].rgb.reshape(
+            (shape[0], shape[1], 3))
 
-canvas.view.add(mesh)
+        mesh = scene.visuals.ImplicitMesh(xs, ys, zs, color=colors)
+
+        canvas.view.add(mesh)
 
 canvas.view.set_camera('turntable', mode='perspective',
-                       up='z', distance=3.0)
+                       up='z', distance=12)
 canvas.show()
 
 if __name__ == '__main__':
