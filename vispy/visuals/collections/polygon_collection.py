70b8804886f0caeb6a593ad1391054105d90c909
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-03-25 10:05:13 +0100

    Fix import bug

--- vispy/visuals/collections/polygon_collection.py
+++ vispy/visuals/collections/polygon_collection.py
@@ -4,9 +4,9 @@
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
 """ """
-from raw_polygon_collection import RawPolygonCollection
-#from agg_polygon_collection import AggPolygonCollection
-#from agg_fast_polygon_collection import AggPolygonCollection
+from . raw_polygon_collection import RawPolygonCollection
+#from . agg_polygon_collection import AggPolygonCollection
+#from . agg_fast_polygon_collection import AggPolygonCollection
 
 
 def PolygonCollection(mode="raw", *args, **kwargs):
