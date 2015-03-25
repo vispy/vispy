dc460eb1dd4eb833712cc6c9410fffe41f0bb3fc
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-03-25 10:05:13 +0100

    Fix import bug

--- vispy/util/svg/svg.py
+++ vispy/util/svg/svg.py
@@ -3,9 +3,9 @@
 # Copyright (c) 2014, Nicolas P. Rougier. All rights reserved.
 # Distributed under the terms of the new BSD License.
 # -----------------------------------------------------------------------------
-from group import Group
-from base import namespace
-from viewport import Viewport
+from . group import Group
+from . base import namespace
+from . viewport import Viewport
 
 class SVG(Group):
 
