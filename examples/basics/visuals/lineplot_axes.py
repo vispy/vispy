c79251261883d564d1c930f308b0ab7541742eda
Max Hunter <nippoo@nippoo.net>
2015-03-27 17:46:16 +0100

    x-axis ticks prototype

--- examples/basics/visuals/lineplot_axes.py
+++ examples/basics/visuals/lineplot_axes.py
@@ -16,10 +16,10 @@ from vispy import gloo, app, visuals
 
 # size of canvas and graph
 canvas_size = (1000, 800)
-graph_size = (800, 600)
+graph_size = (900, 700)
 
 # vertex positions of data to draw
-N = 20
+N = 30
 
 margin_x = (canvas_size[0]-graph_size[0]) / 2.
 margin_y = (canvas_size[1]-graph_size[1]) / 2.
@@ -32,7 +32,7 @@ extents_y = np.array([[margin_x, canvas_size[1]-margin_y],
 
 pos = np.zeros((N, 2), dtype=np.float32)
 pos[:, 0] = np.linspace(margin_x, canvas_size[0]-margin_x, N)
-pos[:, 1] = np.random.normal(size=N, scale=100, loc=canvas_size[1] / 2.)
+pos[:, 1] = np.clip(np.random.normal(size=N, scale=100, loc=canvas_size[1] / 2.), margin_y, canvas_size[0]-margin_y)
 
 
 class Canvas(app.Canvas):
@@ -43,8 +43,8 @@ class Canvas(app.Canvas):
         self.line = visuals.LinePlotVisual(pos, color='w', edge_color='w',
                                            face_color=(0.2, 0.2, 1))
 
-        self.axis_x = visuals.AxisVisual(extents=extents_x)
-        self.axis_y = visuals.AxisVisual(extents=extents_y)
+        self.axis_x = visuals.AxisVisual(extents=extents_x, domain=(0, 100))
+        # self.axis_y = visuals.AxisVisual(extents=extents_y, domain=(5, 7.5))
 
         self.tr_sys = visuals.transforms.TransformSystem(self)
 
@@ -55,7 +55,9 @@ class Canvas(app.Canvas):
         gloo.set_viewport(0, 0, *self.physical_size)
 
         self.axis_x.draw(self.tr_sys)
-        self.axis_y.draw(self.tr_sys)
+        # self.axis_y.draw(self.tr_sys)
+
+        self.axis_x.draw_ticks(self.tr_sys)
 
         self.line.draw(self.tr_sys)
 
--- vispy/visuals/axis.py
+++ vispy/visuals/axis.py
@@ -8,16 +8,32 @@ class AxisVisual(LineVisual):
     """
     Single axis.
     """
-    def __init__(self, extents, domain=(0., 1.), range=(0., 1.)):
-        color = np.array([[1, 1, 1, 1],
-                          [1, 1, 1, 1]])
+    def __init__(self, extents, domain=(0., 1.)):
+        color = (1, 1, 1, 1)
+
+        self.extents = extents
 
         LineVisual.__init__(self, pos=extents, color=color, mode='gl',
-                            width=4.0)
+                            width=3.0)
+
+    def draw_ticks(self, transforms):
+        t = TicksVisual(extents=self.extents)
+        t.draw(transforms)
 
 class TicksVisual(LineVisual):
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, extents, domain=(0., 1.)):
+        color = (0.7, 0.7, 0.7, 1)
+
+        # Generate 10 evenly-spaced ticks
+        tick_spaces = np.linspace(extents[0][0], extents[1][0], num=10)
+
+        x = np.repeat(tick_spaces, 2)
+        y = np.tile([extents[0][1], extents[0][1] + 10], len(tick_spaces))
+
+        tickpos=np.c_[x,y]
+
+        LineVisual.__init__(self, pos=tickpos, color=color, mode='gl',
+                            width=2.0, connect='segments')
 
 class TickTextVisual(TextVisual):
     def __init__(self, **kwargs):
