01c10ad75c9b5857f5d0f3d425bd22038fd75495
Eric Larson <larson.eric.d@gmail.com>
2015-04-01 11:00:50 +0200

    FIX: Fix examples

---
+++ examples/basics/scene/cube.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# vispy: gallery 30
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014, Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
+"""
+Simple use of SceneCanvas to display a cube with an arcball camera.
+"""
+import sys
+
+from vispy import scene
+
+canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
+
+# Set up a viewbox to display the cube with interactive arcball
+view = canvas.central_widget.add_view()
+cube = scene.visuals.Cube(edge_color='k', parent=view.scene)
+view.camera = 'arcball'
+
+if __name__ == '__main__' and sys.flags.interactive == 0:
+    canvas.app.run()
