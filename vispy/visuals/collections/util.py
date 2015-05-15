180da9c35beccfa4db3d3924a9cd8f051538ec89
Eric89GXL <larson.eric.d@gmail.com>
2015-05-15 08:36:18 -0700

    FIX: Fix for Py26

--- vispy/visuals/collections/util.py
+++ vispy/visuals/collections/util.py
@@ -122,7 +122,10 @@ attribute float     collection_index;
     int i = index_x;
     vec4 _uniform;\n"""
 
-    _utype = {name: count for name, count, _ in _utype}
+    orig = _utype
+    _utype = dict()
+    for name, count, _ in orig:
+        _utype[name] = count
     store = 0
     # Be very careful with utype name order (_utype.keys is wrong)
     for name in utype.names:
