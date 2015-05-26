234b97bcd5fd249c80ebe52e1c4a5c809d46df91
Felix Schill <felix.schill@hydromea.com>
2015-05-26 10:21:49 +0200

    fixed over-indentation

--- examples/basics/visuals/line_draw.py
+++ examples/basics/visuals/line_draw.py
@@ -128,9 +128,9 @@ class EditLineVisual(scene.visuals.Line):
                 pos_scene = event.pos
                 # update selected point to new position given by mouse
                 self.selected_point[0] = round(pos_scene[0] / self.gridsize) \
-                                         * self.gridsize
+                    * self.gridsize
                 self.selected_point[1] = round(pos_scene[1] / self.gridsize) \
-                                         * self.gridsize
+                    * self.gridsize
                 self.set_data(pos=self.pos)
                 self.update_markers(self.selected_index)
 
