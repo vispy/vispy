8090f78fef26166f66742c73eb5e98a052a8f09d
Luke Campagnola <luke.campagnola@gmail.com>
2015-05-26 21:47:03 -0400

    Broke cameras into individual modules

--- vispy/scene/cameras/magnify.py
+++ vispy/scene/cameras/magnify.py
@@ -5,7 +5,7 @@ from __future__ import division
 
 import numpy as np
 
-from .cameras import PanZoomCamera
+from .panzoom import PanZoomCamera
 from ...visuals.transforms.nonlinear import (MagnifyTransform, 
                                              Magnify1DTransform)
 from ...app import Timer
