2c2ca7fb8d41e7b1e80327f60953c2df8a9c0fe7
Lucas van Dijk <info@return1.net>
2015-05-28 14:23:38 +0200

    Copy arrow shaders to the arrowheads folder.
    These will contain modified shaders to be used with the ArrowVisual. In
    contrast to the shaders in the vispy/glsl/arrows folder, do the shaders
    in the vispy/glsl/arrowheads folder not draw the arrow body.

---
+++ vispy/glsl/arrowheads/arrowheads.glsl
@@ -0,0 +1,17 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2014, Nicolas P. Rougier. All Rights Reserved.
+// Distributed under the (new) BSD License.
+// ----------------------------------------------------------------------------
+
+#include "arrowheads/util.glsl"
+
+#include "arrowheads/curved.glsl"
+#include "arrowheads/stealth.glsl"
+
+#include "arrowheads/angle-30.glsl"
+#include "arrowheads/angle-60.glsl"
+#include "arrowheads/angle-90.glsl"
+
+#include "arrowheads/triangle-30.glsl"
+#include "arrowheads/triangle-60.glsl"
+#include "arrowheads/triangle-90.glsl"
