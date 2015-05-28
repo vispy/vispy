c0cb904065c916b0502e9f40bf09cf9e91192720
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2015-05-28 18:56:52 -0400

    Move ordereddict backport to _bundled directory.

--- vispy/ext/ordereddict.py
+++ vispy/ext/ordereddict.py
@@ -4,4 +4,4 @@ from sys import version_info
 if version_info >= (2, 7):
     from collections import OrderedDict
 else:
-    from .py24_ordereddict import OrderedDict  # noqa
+    from ._bundled.ordereddict import OrderedDict  # noqa
--- vispy/testing/_runners.py
+++ vispy/testing/_runners.py
@@ -121,7 +121,7 @@ def _flake():
     else:
         sys.argv[1:] = [op.basename(import_dir)]
     sys.argv.append('--ignore=E226,E241,E265,E266,W291,W293,W503')
-    sys.argv.append('--exclude=six.py,py24_ordereddict.py,glfw.py,'
+    sys.argv.append('--exclude=six.py,ordereddict.py,glfw.py,'
                     '_proxy.py,_es2.py,_gl2.py,_pyopengl2.py,'
                     '_constants.py,png.py,decorator.py,ipy_inputhook.py,'
                     'experimental,wiki,_old,mplexporter.py,cubehelix.py')
