cdb0f893767c01860d6626d3529f6544c5b4d955
Eric Larson <larson.eric.d@gmail.com>
2015-06-03 09:52:31 -0700

    FIX: More font tests

--- vispy/util/fonts/tests/test_font.py
+++ vispy/util/fonts/tests/test_font.py
@@ -27,17 +27,15 @@ def test_font_glyph():
     # try both a vispy and system font
     sys_fonts = set(list_fonts()) - set(_vispy_fonts)
     assert_true(len(sys_fonts) > 0)
-    for face in ('OpenSans', list(sys_fonts)[0]):
+    for face in ['OpenSans'] + list(sys_fonts):
         font_dict = dict(face=face, size=12, bold=False, italic=False)
         glyphs_dict = dict()
         chars = 'foobar^C&#'
-        if face != 'OpenSans' and os.getenv('APPVEYOR', '').lower() == 'true':
-            continue  # strange system font failure
-        if 'true' in os.getenv('TRAVIS', '') and sys.version[0] == '3':
-            continue  # as of April 2015 strange FontConfig error on Travis
         for char in chars:
             # Warning that Arial might not exist
-            _load_glyph(font_dict, char, glyphs_dict)
+            with warnings.catch_warnings(record=True):
+                warnings.simplefilter('always')
+                _load_glyph(font_dict, char, glyphs_dict)
         assert_equal(len(glyphs_dict), np.unique([c for c in chars]).size)
 
 
