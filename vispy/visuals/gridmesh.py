4c606a50a6ec0eee101fafd4d5aae551f822bad3
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-06-03 23:34:20 +0100

    Changed GridMesh to use set_data

--- vispy/visuals/gridmesh.py
+++ vispy/visuals/gridmesh.py
@@ -1,6 +1,6 @@
 
 from .mesh import MeshVisual
-from ..geometry import create_grid_mesh
+from ..geometry import create_grid_mesh, MeshData
 from ..color import ColorArray
 
 import numpy as np
@@ -40,23 +40,63 @@ class GridMeshVisual(MeshVisual):
 
     def __init__(self, xs, ys, zs, colors=None,
                  shading='smooth',
-                 vertex_colors=None,
-                 face_colors=None):
+                 **kwargs):
 
-        vertices, indices = create_grid_mesh(xs, ys, zs)
+        if xs is None or ys is None or zs is None:
+            raise ValueError('All of xs, ys and zs must be initialised '
+                             'with arrays.')
+        self._xs = None
+        self._ys = None
+        self._zs = None
 
-        shape = xs.shape
-        if isinstance(colors, np.ndarray) and colors.ndim == 3:
-            colors = colors.reshape((shape[0] * shape[1], colors.shape[2]))
-        colors = ColorArray(colors).rgba
-        if vertex_colors is None:
-            vertex_colors = np.resize(colors, (shape[0] * shape[1], 4))
+        self.__colors = None
+        self.__vertices = None
+        self.__meshdata = MeshData()
 
-        MeshVisual.__init__(self, vertices, indices,
-                            vertex_colors=vertex_colors,
-                            face_colors=face_colors,
-                            color='purple',
-                            shading=shading)
+        MeshVisual.__init__(self, shading=shading, **kwargs)
+        self.set_data(xs, ys, zs, colors)
+
+    def set_data(self, xs=None, ys=None, zs=None, colors=None):
+
+        if xs is not None:
+            if self._xs is not None:
+                self.__vertices = None
+            self._xs = xs
+
+        if ys is not None:
+            if self._ys is not None:
+                self.__vertices = None
+            self._ys = ys
+
+        if zs is not None:
+            if self._zs is not None:
+                self.__vertices = None
+            self._zs = zs
+
+        if self.__vertices is None:
+            vertices, indices = create_grid_mesh(self._xs, self._ys, self._zs)
+            self.__meshdata.set_vertices(vertices)
+            self.__meshdata.set_faces(indices)
+
+        if colors is not None:
+            self.__colors = colors
+            self.__meshdata.set_vertex_colors(colors.reshape(colors.shape[0] * colors.shape[1], colors.shape[2]))
+            
+        MeshVisual.set_data(self, meshdata=self.__meshdata)
+
+        # shape = xs.shape
+        # if isinstance(colors, np.ndarray) and colors.ndim == 3:
+        #     colors = colors.reshape((shape[0] * shape[1], colors.shape[2]))
+        
+        # colors = ColorArray(colors).rgba
+        # if vertex_colors is None:
+        #     vertex_colors = np.resize(colors, (shape[0] * shape[1], 4))
+
+        # MeshVisual.__init__(self, vertices, indices,
+        #                     vertex_colors=vertex_colors,
+        #                     face_colors=face_colors,
+        #                     color='purple',
+        #                     shading=shading)
 
     def draw(self, transforms):
         MeshVisual.draw(self, transforms)
