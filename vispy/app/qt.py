4d5e4fce403fb106aff2f294a89b16a94cee1f82
Hadrien Mary <hadrien.mary@gmail.com>
2015-06-03 13:53:04 +0200

    Fix compatibility with pyqt5 and pyside for QtCanvas and QtSceneCanvas

--- vispy/app/qt.py
+++ vispy/app/qt.py
@@ -13,14 +13,30 @@ except AttributeError:
     raise RuntimeError("Cannot import Qt library; non-Qt backend is already "
                        "in use.")
 
+from .backends import qt_lib
 
-class QtCanvas(QtGui.QWidget):
-    """ Qt widget containing a vispy Canvas. 
-    
+if qt_lib == 'pyqt4':
+    from PyQt4 import QtGui, QtCore
+    QWidget, QGridLayout = QtGui.QWidget, QtGui.QGridLayout  # Compat
+elif qt_lib == 'pyqt5':
+    from PyQt5 import QtGui, QtCore, QtWidgets
+    QWidget, QGridLayout = QtWidgets.QWidget, QtWidgets.QGridLayout  # Compat
+elif qt_lib == 'pyside':
+    from PySide import QtGui, QtCore
+    QWidget, QGridLayout = QtGui.QWidget, QtGui.QGridLayout  # Compat
+elif qt_lib:
+    raise RuntimeError("Invalid value for qt_lib %r." % qt_lib)
+else:
+    raise RuntimeError("Module backends._qt should not be imported directly.")
+
+
+class QtCanvas(QWidget):
+    """ Qt widget containing a vispy Canvas.
+
     This is a convenience class that allows a vispy canvas to be embedded
     directly into a Qt application.
     All methods and properties of the Canvas are wrapped by this class.
-    
+
     Parameters
     ----------
     parent : QWidget or None
@@ -29,7 +45,7 @@ class QtCanvas(QtGui.QWidget):
         The vispy Canvas to display inside this widget, or a Canvas subclass
         to instantiate using any remaining keyword arguments.
     """
-    
+
     def __init__(self, parent=None, canvas=None, **kwargs):
         from .canvas import Canvas
         if canvas is None:
@@ -37,20 +53,20 @@ class QtCanvas(QtGui.QWidget):
         if issubclass(canvas, Canvas):
             canvas = canvas(**kwargs)
         elif len(**kwargs) > 0:
-            raise TypeError('Invalid keyword arguments: %s' % 
+            raise TypeError('Invalid keyword arguments: %s' %
                             list(kwargs.keys()))
         if not isinstance(canvas, Canvas):
             raise TypeError('canvas argument must be an instance or subclass '
                             'of Canvas.')
-            
-        QtGui.QWidget.__init__(self, parent)
-        self.layout = QtGui.QGridLayout()
+
+        QWidget.__init__(self, parent)
+        self.layout = QGridLayout()
         self.setLayout(self.layout)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self._canvas = canvas
         self.layout.addWidget(canvas.native)
         self.setSizePolicy(canvas.native.sizePolicy())
-        
+
     def __getattr__(self, attr):
         if hasattr(self._canvas, attr):
             return getattr(self._canvas, attr)
@@ -60,13 +76,13 @@ class QtCanvas(QtGui.QWidget):
     def update(self):
         """Call update() on both this widget and the internal canvas.
         """
-        QtGui.QWidget.update(self)
+        QWidget.update(self)
         self._canvas.update()
 
 
 class QtSceneCanvas(QtCanvas):
     """ Convenience class embedding a vispy SceneCanvas inside a QWidget.
-    
+
     See QtCanvas.
     """
     def __init__(self, parent=None, **kwargs):
