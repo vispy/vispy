8ee3a1d74f9d0678fefd1f9478aff7c67e974d6a
Siddharth Bhat <siddu.druid@gmail.com>
2015-06-09 10:28:17 +0530

    make a separate test branch for ipython <= 3.0, so we can test failure of webGL loading as well

--- vispy/app/tests/test_ipython.py
+++ vispy/app/tests/test_ipython.py
@@ -8,16 +8,29 @@ from numpy.testing import assert_equal
 from vispy.testing import requires_ipython
 
 
-@requires_ipython()
+@requires_ipython(2.0)
 def test_webgl_loading():
-    """Test if the vispy.ipython extension loads the webGL backend"""
+    """Test if the vispy.ipython extension loads the webGL backend
+    on IPython 3.0 and greater.
 
+    Test if it fails to load the webGL backend for IPython versions
+    less that 3.0"""
+
+    import IPython
+    from distutils.version import LooseVersion
     from IPython.testing.globalipapp import get_ipython
 
     ipy = get_ipython()
     ipy.run_cell("from vispy import app")
-    ipy.run_cell("%load_ext vispy.ipython")
-    ipy.run_cell("backend_name = app.use_app().backend_name")
 
-    # make sure that the webgl backend got loaded
-    assert_equal(ipy.user_ns["backend_name"], "ipynb_webgl")
+    if LooseVersion(IPython.__version__) >= LooseVersion("3.0.0"):
+        ipy.run_cell("%load_ext vispy.ipython")
+        ipy.run_cell("backend_name = app.use_app().backend_name")
+        # make sure that the webgl backend got loaded
+        assert_equal(ipy.user_ns["backend_name"], "ipynb_webgl")
+    else:
+        ipy.run_cell("%load_ext vispy.ipython")
+        # HACK: duplicate code branches for now to see what happens. 
+        ipy.run_cell("backend_name = app.use_app().backend_name")
+        # make sure that the webgl backend got loaded
+        assert_equal(ipy.user_ns["backend_name"], "ipynb_webgl")
