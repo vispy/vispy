c53537a659184163ba1182b836883119758e9676
Lucas van Dijk <info@return1.net>
2015-06-10 21:45:02 +0200

    Some progress on the curved arrow heads

--- vispy/glsl/arrowheads/curved.glsl
+++ vispy/glsl/arrowheads/curved.glsl
@@ -28,21 +28,21 @@
 float arrow_curved(vec2 texcoord, float size,
                    float linewidth, float antialias)
 {
-    vec2 start = -vec2(size/2.0, 0.0);
-    vec2 end   = +vec2(size/2.0, 0.0);
-    float height = 0.5;
+    vec2 start = -vec2(size/4.0, 0.0);
+    vec2 end   = +vec2(size/4.0, 0.0);
+    float height = 0.2;
 
-    vec2 p1 = end - size*vec2(+1.0,+height);
-    vec2 p2 = end - size*vec2(+1.0,-height);
+    vec2 p1 = start + size*vec2(0.1,-height);
+    vec2 p2 = start + size*vec2(0.1,+height);
     vec2 p3 = end;
 
     // Head : 3 circles
-    vec2 c1  = circle_from_2_points(p1, p3, 6.0*size).zw;
-    float d1 = length(texcoord - c1) - 6*size;
-    vec2 c2  = circle_from_2_points(p2, p3, 6.0*size).xy;
-    float d2 = length(texcoord - c2) - 6*size;
-    vec2 c3  = circle_from_2_points(p1, p2, 6.0*size).xy;
-    float d3 = length(texcoord - c3) - 6*size;
+    vec2 c1  = circle_from_2_points(p1, p3, 4.0*size).zw;
+    float d1 = length(texcoord - c1) - 4*size;
+    vec2 c2  = circle_from_2_points(p2, p3, 4.0*size).xy;
+    float d2 = length(texcoord - c2) - 4*size;
+    vec2 c3  = circle_from_2_points(p1, p2, 3.0*size).xy;
+    float d3 = length(texcoord - c3) - 3*size;
 
     // Outside (because of circles)
     if( texcoord.y > +(2.0*size + antialias) )
