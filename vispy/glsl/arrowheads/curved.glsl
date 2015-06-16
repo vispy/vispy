bd2f323b5d4d1833c0cfbbb62fd709584253a336
Lucas van Dijk <info@return1.net>
2015-06-16 16:28:02 +0200

    Clean the GLSL shader code for arrow heads.
    Things changed:
    
    1. Use proper scaling when tranforming the coordinates in the fragment shader
    2. Use correct coordinates when drawing the curved arrow heads
    3. Add proper comment headings
    4. Several other cleanups

--- vispy/glsl/arrowheads/curved.glsl
+++ vispy/glsl/arrowheads/curved.glsl
@@ -1,56 +1,42 @@
-// ----------------------------------------------------------------------------
-// Copyright (c) 2014, Nicolas P. Rougier. All Rights Reserved.
-// Distributed under the (new) BSD License.
-// ----------------------------------------------------------------------------
+/**
+ * Copyright (c) Vispy Development Team
+ * Distributed under the (new) BSD License. See LICENSE.txt for more info.
+ *
+ * This files contains the code for drawing curved arrow heads.
+ */
 
 #include "arrowheads/util.glsl"
 
-/* ---------------------------------------------------------
-
-   Computes the signed distance to an curved arrow
-
-   Parameters:
-   -----------
-
-   texcoord : Point to compute distance to
-   body :     Total length of the arrow (pixels, body+head)
-   head :     Length of the head (pixels)
-   height :   Height of the head (pixel)
-   linewidth: Stroke line width (in pixels)
-   antialias: Stroke antialiased area (in pixels)
-
-   Return:
-   -------
-   Signed distance to the arrow
-
-   --------------------------------------------------------- */
-
-float arrow_curved(vec2 texcoord, float size,
-                   float linewidth, float antialias)
+/**
+ * Computes the signed distance to an curved arrow
+ *
+ * Parameters:
+ * -----------
+ * texcoord : Point to compute distance to
+ * size : size of the arrow head in pixels
+ *
+ * Return:
+ * -------
+ * Signed distance to the arrow
+ *
+ */
+float arrow_curved(vec2 texcoord, float size)
 {
-    vec2 start = -vec2(size/4.0, 0.0);
-    vec2 end   = +vec2(size/4.0, 0.0);
-    float height = 0.2;
+    vec2 start = -vec2(size/2, 0.0);
+    vec2 end   = +vec2(size/2, 0.0);
+    float height = 0.5;
 
-    vec2 p1 = start + size*vec2(0.1,-height);
-    vec2 p2 = start + size*vec2(0.1,+height);
+    vec2 p1 = start + size*vec2(0, -height);
+    vec2 p2 = start + size*vec2(0, +height);
     vec2 p3 = end;
 
     // Head : 3 circles
-    vec2 c1  = circle_from_2_points(p1, p3, 4.0*size).zw;
-    float d1 = length(texcoord - c1) - 4*size;
-    vec2 c2  = circle_from_2_points(p2, p3, 4.0*size).xy;
-    float d2 = length(texcoord - c2) - 4*size;
+    vec2 c1  = circle_from_2_points(p1, p3, 6.0*size).zw;
+    float d1 = length(texcoord - c1) - 6*size;
+    vec2 c2  = circle_from_2_points(p2, p3, 6.0*size).xy;
+    float d2 = length(texcoord - c2) - 6*size;
     vec2 c3  = circle_from_2_points(p1, p2, 3.0*size).xy;
     float d3 = length(texcoord - c3) - 3*size;
-
-    // Outside (because of circles)
-    if( texcoord.y > +(2.0*size + antialias) )
-         return 1000.0;
-    if( texcoord.y < -(2.0*size + antialias) )
-         return 1000.0;
-    if( texcoord.x > c1.x ) //(size + antialias) )
-         return 1000.0;
 
     return -min(d3, min(d1,d2));
 }
