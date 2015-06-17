bc2ef4e73546ff4f9fdd216cdd29d9cb1d1a5870
Lucas van Dijk <info@return1.net>
2015-06-17 14:43:09 +0200

    Update arrows example so it shows more arrow types

--- examples/basics/visuals/arrows.py
+++ examples/basics/visuals/arrows.py
@@ -8,33 +8,30 @@ This example demonstrates how to draw lines with one or more arrow heads.
 """
 
 import sys
-import math
-
-import numpy as np
 
 from vispy import app, gloo, visuals
 from vispy.geometry import curves
-from vispy.visuals.transforms import STTransform, NullTransform
+from vispy.visuals.transforms import STTransform
 
 
 class Canvas(app.Canvas):
     def __init__(self):
         app.Canvas.__init__(self, title='Arrows example',
-                            keys='interactive', size=(450, 750))
+                            keys='interactive', size=(1050, 650))
 
         line1 = curves.curve4_bezier(
-            (10, 0),
+            (10.0, 0.0),
             (50, -190),
             (350, 190),
-            (390, 0)
+            (390, 0.0)
         )
-        arrows1 = ([line1[-3], line1[-2]],)
+        arrows1 = (line1[-2:],)
 
         line2 = curves.curve4_bezier(
-            (10, 0),
+            (10.0, 0.0),
             (190, -190),
             (210, 190),
-            (390, 0)
+            (390, 0.0)
         )
         arrows2 = (
             line2[1::-1],
@@ -42,60 +39,43 @@ class Canvas(app.Canvas):
         )
 
         line3 = curves.curve3_bezier(
-            (10, 0),
-            (30, 200),
-            (390, 0)
+            (10.0, 0.0),
+            (50, 190),
+            (390, 0.0)
         )
 
-        print(arrows1)
-        print(arrows2)
+        arrows3 = (
+            line3[-2:],
+        )
 
-        print("Direction vectors")
-        for v1, v2 in arrows1:
-            body = v2 - v1
-            length = np.linalg.norm(body)
-            corner = (math.acos(body[0]/length) * 360) / (2*math.pi)
-            print(length, body/length, corner)
+        arrow_types = ["curved", "stealth", "triangle_60", "angle_60"]
+        self.lines = []
 
-        for v1, v2 in arrows2:
-            body = v2 - v1
-            length = np.linalg.norm(body)
-            corner = (math.acos(body[0]/length) * 360) / (2*math.pi)
-            print(length, body/length, corner)
+        for i, arrow_type in enumerate(arrow_types):
+            arrows = [
+                visuals.ArrowVisual(line1, color='w', width=6, method='agg',
+                                    arrows=arrows1, arrow_type=arrow_type,
+                                    arrow_size=30.0),
+                visuals.ArrowVisual(line2, color='w', width=2, method='agg',
+                                    arrows=arrows2, arrow_type=arrow_type,
+                                    arrow_size=5.0),
+                visuals.ArrowVisual(line3, color='w', width=4, method='agg',
+                                    arrows=arrows3, arrow_type=arrow_type,
+                                    arrow_size=10.0)
+            ]
 
-        body = arrows1[0][1] - arrows1[0][0]
-        print(np.linalg.norm(body), body/np.linalg.norm(body))
+            # Translate each line visual downwards
+            for j, visual in enumerate(arrows):
+                x = 50 + (i * 250)
+                y = 100 + (200 * j)
 
-        self.lines = [
-            visuals.ArrowVisual(line1, color='w', width=2, method='agg',
-                                arrows=arrows1, arrow_type='curved',
-                                arrow_size=30.0),
-            visuals.ArrowVisual(line2, color='w', width=2, method='agg',
-                                arrows=arrows2, arrow_type='curved',
-                                arrow_size=10.0),
-            visuals.LineVisual(line3, color='w', width=2, method='agg')
-        ]
+                visual.transform = STTransform(translate=[x, y],
+                                               scale=(0.5, 1.0))
 
-        # Translate each line visual downwards
-        for i, line in enumerate(self.lines):
-            x = 0
-            y = 200 * (i + 1)
-
-            line.transform = STTransform(translate=[x, y])
-
-        self.texts = [
-            visuals.TextVisual('4 point Bezier curve', bold=True, color='w',
-                               font_size=24, pos=(200, 75)),
-            visuals.TextVisual('3 point Bezier curve', bold=True, color='w',
-                               font_size=24, pos=(200, 525)),
-        ]
-
-        for text in self.texts:
-            text.transform = NullTransform()
+            self.lines.extend(arrows)
 
         # Initialize transform systems for each visual
-        self.visuals = self.lines + self.texts
-        for visual in self.visuals:
+        for visual in self.lines:
             visual.tr_sys = visuals.transforms.TransformSystem(self)
             visual.tr_sys.visual_to_document = visual.transform
 
@@ -105,7 +85,7 @@ class Canvas(app.Canvas):
         gloo.clear('black')
         gloo.set_viewport(0, 0, *self.physical_size)
 
-        for visual in self.visuals:
+        for visual in self.lines:
             visual.draw(visual.tr_sys)
 
 if __name__ == '__main__':
