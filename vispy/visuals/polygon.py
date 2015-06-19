4169ddd85da7b3c549bea45a1b8c79a588fefb3c
Siddharth <siddu.druid@gmail.com>
2015-06-20 01:44:05 +0530

    replaced old PolygonVisual with new version. Example in polygon_prototype.py

--- vispy/visuals/polygon.py
+++ vispy/visuals/polygon.py
@@ -11,18 +11,16 @@ from __future__ import division
 
 import numpy as np
 
-from .. import gloo
-from .visual import Visual
+from .visual import CompoundVisual
 from .mesh import MeshVisual
 from .line import LineVisual
 from ..color import Color
 from ..geometry import PolygonData
 
 
-class PolygonVisual(Visual):
+class PolygonVisual(CompoundVisual):
     """
     Displays a 2D polygon
-
     Parameters
     ----------
     pos : array
@@ -38,8 +36,6 @@ class PolygonVisual(Visual):
     """
     def __init__(self, pos=None, color='black',
                  border_color=None, border_width=1, **kwargs):
-        super(PolygonVisual, self).__init__(**kwargs)
-
         self.mesh = MeshVisual()
         self.border = LineVisual()
         self._pos = pos
@@ -47,8 +43,27 @@ class PolygonVisual(Visual):
         self._border_width = border_width
         self._border_color = Color(border_color)
         self._update()
-        #glopts = kwargs.pop('gl_options', 'translucent')
-        #self.set_gl_options(glopts)
+
+        CompoundVisual.__init__(self, [self.mesh, self.border])
+
+    def _update(self):
+        self.data = PolygonData(vertices=np.array(self._pos, dtype=np.float32))
+        if self._pos is None:
+            return
+        if not self._color.is_blank:
+            pts, tris = self.data.triangulate()
+            self.mesh.set_data(vertices=pts, faces=tris.astype(np.uint32),
+                               color=self._color.rgba)
+        if not self._border_color.is_blank:
+            # Close border if it is not already.
+            border_pos = self._pos
+            if np.any(border_pos[0] != border_pos[1]):
+                border_pos = np.concatenate([border_pos, border_pos[:1]],
+                                            axis=0)
+            self.border.set_data(pos=border_pos,
+                                 color=self._border_color.rgba,
+                                 width=self._border_width,
+                                 connect='strip')
 
     @property
     def pos(self):
@@ -82,47 +97,3 @@ class PolygonVisual(Visual):
     def border_color(self, border_color):
         self._border_color = Color(border_color)
         self._update()
-
-    def _update(self):
-        self.data = PolygonData(vertices=np.array(self._pos, dtype=np.float32))
-        if self._pos is None:
-            return
-        if not self._color.is_blank:
-            pts, tris = self.data.triangulate()
-            self.mesh.set_data(vertices=pts, faces=tris.astype(np.uint32),
-                               color=self._color.rgba)
-        if not self._border_color.is_blank:
-            # Close border if it is not already.
-            border_pos = self._pos
-            if np.any(border_pos[0] != border_pos[1]):
-                border_pos = np.concatenate([border_pos, border_pos[:1]], 
-                                            axis=0)
-            self.border.set_data(pos=border_pos,
-                                 color=self._border_color.rgba, 
-                                 width=self._border_width,
-                                 connect='strip')
-        self.update()
-
-    def set_gl_options(self, *args, **kwargs):
-        self.mesh.set_gl_options(*args, **kwargs)
-
-    def update_gl_options(self, *args, **kwargs):
-        self.mesh.update_gl_options(*args, **kwargs)
-
-    def draw(self, transforms):
-        """Draw the visual
-
-        Parameters
-        ----------
-        transforms : instance of TransformSystem
-            The transforms to use.
-        """
-        if self._pos is None:
-            return
-        if not self._color.is_blank:
-            gloo.set_state(polygon_offset_fill=True, 
-                           cull_face=False)
-            gloo.set_polygon_offset(1, 1)
-            self.mesh.draw(transforms)
-        if not self._border_color.is_blank:
-            self.border.draw(transforms)
