d915b81bd4ac750acdf2ee1fa4e5394984282081
Siddharth <siddu.druid@gmail.com>
2015-06-20 06:03:36 +0530

    update example, move polygons around

--- examples/basics/visuals/polygon_prototype.py
+++ examples/basics/visuals/polygon_prototype.py
@@ -1,13 +1,14 @@
 from vispy import app
 import sys
 from vispy.scene import SceneCanvas
-from vispy.scene.visuals import Polygon, Ellipse
+from vispy.scene.visuals import Polygon, Ellipse, Rectangle
 from vispy.color import Color
 
 white = Color("#ecf0f1")
 gray = Color("#121212")
 red = Color("#e74c3c")
 blue = Color("#2980b9")
+orange = Color("#e88834")
 
 if __name__ == '__main__':
     canvas = SceneCanvas(keys='interactive', title='Polygon Example',
@@ -27,11 +28,18 @@ if __name__ == '__main__':
     p = Polygon(rect_coords, color=red, border_color=white,
                 border_width=3,  parent=v.scene)
 
-    e = Ellipse(pos=(0.5, 0.2), radius=(0.1, 0.05),
+    e = Ellipse(pos=(0.4, 0.2), radius=(0.1, 0.05),
                 color=blue,
-                parent=v.scene,
                 border_width=2,
-                border_color=white)
+                border_color=white,
+                parent=v.scene)
+
+    r = Rectangle(pos=(0.6, 0.2), width=0.1, height=0.2,
+                  color=orange,
+                  border_width=2,
+                  border_color=white,
+                  radius=0.02,
+                  parent=v.scene)
 
     e.num_segments = 50
     e.start_angle = 0
