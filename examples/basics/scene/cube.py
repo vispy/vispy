056a4ed2c62e9ec8b4562a42aecb41230ac83bcd
Siddharth Bhat <siddu.druid@gmail.com>
2015-06-23 03:34:41 +0530

    got cube example working

--- examples/basics/scene/cube.py
+++ examples/basics/scene/cube.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*-coding: utf-8 -*-
 # vispy: gallery 30
 # -----------------------------------------------------------------------------
 # Copyright (c) 2015, Vispy Development Team. All Rights Reserved.
@@ -10,14 +10,23 @@ Simple use of SceneCanvas to display a cube with an arcball camera.
 import sys
 
 from vispy import scene
-
+from vispy.visuals.transforms import AffineTransform
+from vispy.color import Color
 canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
 
 # Set up a viewbox to display the cube with interactive arcball
 view = canvas.central_widget.add_view()
-cube = scene.visuals.Cube(edge_color='k', parent=view.scene)
-view.camera = 'arcball'
-view.camera.fov = 30.
+view.bgcolor = '#efefef'
+view.camera = 'turntable'
+
+color = Color("#3f51b5")
+
+cube = scene.visuals.Cube(size=0.5, color=color, edge_color="black",
+                          parent=view.scene)
+cube_transform = AffineTransform()
+# cube_transform.rotate(30, (1, 0, 1))
+cube_transform.translate([0.5, 0.5, 0.5])
+cube.transform = cube_transform
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
     canvas.app.run()
