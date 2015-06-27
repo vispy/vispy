9b238b80c1e76038cc7a4e5aca529fe0efaa6f08
Luke Campagnola <luke.campagnola@gmail.com>
2015-06-27 16:47:26 -0400

    minor fixes

--- examples/basics/plotting/scatter_histogram.py
+++ examples/basics/plotting/scatter_histogram.py
@@ -16,8 +16,8 @@ color = (0.8, 0.25, 0.)
 n_bins = 100
 
 fig = vp.Fig(show=False)
-fig[0:4, 0:4].plot(data, width=0, face_color=color + (0.05,), edge_color=None,
-                   marker_size=10.)
+fig[0:4, 0:4].plot(data, symbol='o', width=0, face_color=color + (0.05,),
+                   edge_color=None, marker_size=10.)
 fig[4, 0:4].histogram(data[:, 0], bins=n_bins, color=color, orientation='h')
 fig[0:4, 4].histogram(data[:, 1], bins=n_bins, color=color, orientation='v')
 
