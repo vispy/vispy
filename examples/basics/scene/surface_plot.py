601507d171284a2fd0f7c4daaa5bfd6dc01a4bc2
Luke Campagnola <luke.campagnola@gmail.com>
2015-07-03 14:14:27 -0400

    update surface plot example and corrections to axisvisual

--- examples/basics/scene/surface_plot.py
+++ examples/basics/scene/surface_plot.py
@@ -17,17 +17,28 @@ from vispy.util.filter import gaussian_filter
 
 canvas = scene.SceneCanvas(keys='interactive')
 view = canvas.central_widget.add_view()
-view.camera = scene.TurntableCamera(up='z')
+view.camera = scene.TurntableCamera(up='z', fov=60)
 
 # Simple surface plot example
 # x, y values are not specified, so assumed to be 0:50
-z = gaussian_filter(np.random.normal(size=(50, 50)), (1, 1)) * 10
+z = np.random.normal(size=(250, 250), scale=200)
+z[100, 100] += 50000
+z = gaussian_filter(z, (10, 10))
 p1 = scene.visuals.SurfacePlot(z=z, color=(0.5, 0.5, 1, 1), shading='smooth')
 p1.transform = scene.transforms.MatrixTransform()
-p1.transform.scale([1/49., 1/49., 0.02])
+p1.transform.scale([1/249., 1/249., 1/249.])
 p1.transform.translate([-0.5, -0.5, 0])
 
 view.add(p1)
+
+
+xax = scene.Axis(pos=[[-0.5, -0.5], [0.5, -0.5]], tick_direction=(0, -1),
+                 font_size=16, parent=view.scene)
+xax.transform = scene.STTransform(translate=(0, 0, -0.3))
+
+yax = scene.Axis(pos=[[-0.5, -0.5], [-0.5, 0.5]], tick_direction=(-1, 0),
+                 font_size=16, parent=view.scene)
+yax.transform = scene.STTransform(translate=(0, 0, -0.3))
 
 # Add a 3D axis to keep us oriented
 axis = scene.visuals.XYZAxis(parent=view.scene)
