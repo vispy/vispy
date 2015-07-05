09062ffe815d5afcb8f2bb710fa02571135b9c4c
Eric Larson <larson.eric.d@gmail.com>
2015-07-05 16:47:15 -0400

    FIX: Only load scalable fonts

--- vispy/ext/freetype.py
+++ vispy/ext/freetype.py
@@ -30,6 +30,7 @@ FT_LOAD_NO_AUTOHINT = 32768
 FT_LOAD_TARGET_LCD = 196608
 FT_LOAD_TARGET_LIGHT = 65536
 FT_LOAD_NO_SCALE = 1
+FT_FACE_FLAG_SCALABLE = 1
 
 _64_bit = (8 * struct.calcsize("P")) == 64
 ##############################################################################
