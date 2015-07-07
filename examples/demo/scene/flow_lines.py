9d0f2bb29798267e1d7ef9c1f06cffd982ec2f06
Luke Campagnola <luke.campagnola@gmail.com>
2015-07-07 11:18:23 -0500

    more more pretty

--- examples/demo/scene/flow_lines.py
+++ examples/demo/scene/flow_lines.py
@@ -12,11 +12,11 @@ class VectorFieldVisual(visuals.Visual):
     uniform vec2 shape;
     uniform vec2 field_shape;
     uniform float spacing;
-    varying float dist;
+    varying float dist;  // distance along path for this vertex
     varying vec2 ij;
     uniform sampler2D offset;
-    varying float flow_rate;
     uniform float seg_len;
+    uniform int n_iter;  // iterations to integrate along field per vertex
     
     void main() {
         // distance along one line
@@ -29,10 +29,12 @@ class VectorFieldVisual(visuals.Visual):
         vec2 off = texture2D(offset, ij / shape).xy - 0.5;
         local = spacing * (ij + off);
         for( int i=0; i<index.y; i+=1 ) {
-            vec2 uv = local / field_shape;
-            vec2 dir = texture2D(field, uv).xy;
-            flow_rate = log(length(dir));
-            local += seg_len * normalize(dir);
+            for ( int j=0; j<n_iter; j += 1 ) {
+                vec2 uv = local / field_shape;
+                vec2 dir = texture2D(field, uv).xy;
+                // maybe pick a more accurate integration method?
+                local += seg_len * dir / n_iter;
+            }
         }
         gl_Position = $transform(vec4(local, 0, 1));
     }
@@ -46,7 +48,6 @@ class VectorFieldVisual(visuals.Visual):
     uniform sampler2D offset;
     uniform sampler2D color;
     uniform vec2 shape;
-    varying float flow_rate;
     uniform float nseg;
     uniform float seg_len;
     
@@ -54,14 +55,21 @@ class VectorFieldVisual(visuals.Visual):
         float totlen = nseg * seg_len;
         float phase = texture2D(offset, ij / shape).b;
         float alpha;
-        alpha = mod((dist / totlen) + phase - time, 1);
+        
+        // vary alpha along the length of the line to give the appearance of
+        // motion
+        alpha = mod((dist / totlen) + phase - time * speed, 1);
+        
+        // add a cosine envelope to fade in and out smoothly at the ends
         alpha *= (1 - cos(2 * 3.141592 * dist / totlen)) * 0.5;
+        
         vec4 base_color = texture2D(color, ij / shape);
         gl_FragColor = vec4(base_color.rgb, base_color.a * alpha);
     }
     """
     
-    def __init__(self, field, spacing=10, segments=3, seg_len=0.5):
+    def __init__(self, field, spacing=10, segments=3, seg_len=0.5,
+                 color=(1, 1, 1, 0.3)):
         self._time = 0.0
         self._last_time = ptime.time()
         rows = field.shape[0] / spacing
@@ -74,7 +82,8 @@ class VectorFieldVisual(visuals.Visual):
         index[:, ::2, 1] = np.arange(segments)[np.newaxis, :]
         index[:, 1::2, 1] = np.arange(segments)[np.newaxis, :] + 1
         self._index = gloo.VertexBuffer(index)
-        color = np.array([[[1, 1, 1, 0.3]]], dtype='float32')
+        if not isinstance(color, np.ndarray):
+            color = np.array([[list(color)]], dtype='float32')
         self._color = gloo.Texture2D(color)
         
         offset = np.random.uniform(256, size=(rows, cols, 3)).astype(np.ubyte)
@@ -92,10 +101,11 @@ class VectorFieldVisual(visuals.Visual):
         self.shared_program['spacing'] = spacing
         self.shared_program['t'] = self._time
         self.shared_program['offset'] = self._offset
-        self.shared_program['speed'] = 10
+        self.shared_program['speed'] = 1
         self.shared_program['color'] = self._color
         self.shared_program['seg_len'] = seg_len
         self.shared_program['nseg'] = segments
+        self.shared_program['n_iter'] = 1
         
         self._draw_mode = 'lines'
         self.set_gl_state('translucent', depth_test=False)
@@ -126,15 +136,28 @@ VectorField = scene.visuals.create_visual_node(VectorFieldVisual)
 
 
 
-field = np.zeros((100, 100, 3), dtype='float32')
-field[..., 0] = np.fromfunction(lambda x,y: x-50, (100, 100))
-field[..., 1] = np.fromfunction(lambda x,y: y-50, (100, 100))
+#field = np.zeros((100, 100, 3), dtype='float32')
+#field[..., 0] = np.fromfunction(lambda x,y: x-50, (100, 100))
+#field[..., 1] = np.fromfunction(lambda x,y: y-50, (100, 100))
+#field *= 0.01
+
+def fn(y, x):
+    dx = x-50
+    dy = y-30
+    l = (dx**2 + dy**2)**0.5
+    return np.array([100*dy/l**1.7, -100*dx/l**1.8])
+field = np.fromfunction(fn, (100, 100)).transpose(1, 2, 0).astype('float32')
+field[..., 0] += 10 * np.cos(np.linspace(0, 2*3.1415, 100))
+
+color = np.zeros((100, 100, 3), dtype='float32')
+color[...,:2] = (field + 5) / 10.
+color[...,2] = 0.5
 
 win = scene.SceneCanvas(keys='interactive', show=True)
 view = win.central_widget.add_view(camera='panzoom')
 #img = scene.Image(field, parent=view.scene)
 
-vfield = VectorField(field[..., :2], spacing=0.5, segments=30, seg_len=0.5, parent=view.scene)
+vfield = VectorField(field[..., :2], spacing=0.5, segments=30, seg_len=0.05, parent=view.scene, color=color)
 view.camera.set_range()
 
 
