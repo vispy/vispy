cd7f138e2050d6a7041d0b59adb5743e37cc258e
Lucas van Dijk <info@return1.net>
2015-07-16 16:02:23 +0200

    Add shebang to example

--- examples/basics/visuals/arrows.py
+++ examples/basics/visuals/arrows.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
 # Copyright (c) 2015, Vispy Development Team. All Rights Reserved.
