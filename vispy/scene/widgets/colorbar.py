b6a548c76a80f05d1725bfc42a355937ab11f4da
Siddharth <siddu.druid@gmail.com>
2015-07-30 14:56:52 +0530

    added colorbar widget file

---
+++ vispy/scene/widgets/colorbar.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2015, Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
+import numpy as np
+
+from .widget import Widget
+from ...visuals import ColorBarVisual
