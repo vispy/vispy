60145866525b3eba443a425558af255a75644ae7
Siddharth <siddu.druid@gmail.com>
2015-07-30 20:02:17 +0530

    create ColorBarWidget, the widget which wraps ColorBar.

--- vispy/scene/widgets/colorbar.py
+++ vispy/scene/widgets/colorbar.py
@@ -7,3 +7,63 @@ import numpy as np
 
 from .widget import Widget
 from ...visuals import ColorBarVisual
+
+
+class ColorBarWidget(Widget):
+    """Widget containing a ColorBar
+
+    Parameters
+    ----------
+    **kwargs : dict
+        Keyword arguments to pass to ColorBarVisual.
+    """
+    def __init__(self, **kwargs):
+        self._colorbar = ColorBarVisual(**kwargs)
+        Widget.__init__(self)
+        self.add_subvisual(self._colorbar)
+        self._set_pos()
+
+    def on_resize(self, event):
+        """Resize event handler
+
+        Parameters
+        ----------
+        event : instance of Event
+            The event.
+        """
+        self._set_pos()
+
+    def _set_pos(self):
+        self._colorbar.center_pos = self.rect.center
+
+    @property
+    def cmap(self):
+        return self._colorbar.cmap
+
+    @cmap.setter
+    def cmap(self, cmap):
+        self._colorbar.cmap = cmap
+
+    @property
+    def halfdim(self):
+        return self._colorbar.halfdim
+
+    @halfdim.setter
+    def halfdim(self, halfdim):
+        self._colorbar.halfdim = halfdim
+
+    @property
+    def label(self):
+        return self._colorbar.label
+
+    @label.setter
+    def label(self, label):
+        self._colorbar.label = label
+
+    @property
+    def clim(self):
+        return self._colorbar.clim
+
+    @clim.setter
+    def clim(self, clim):
+        self._colorbar.clim = clim
