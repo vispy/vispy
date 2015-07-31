cdbfc6c6a393bdbf976a371053da16fec4853bbf
Lucas van Dijk <info@return1.net>
2015-07-31 20:36:57 +0200

    Create testing canvas inside the loop
    Hopefully this stops some warnings when setting arrow.parent to
    None.

--- vispy/visuals/tests/test_arrows.py
+++ vispy/visuals/tests/test_arrows.py
@@ -18,7 +18,7 @@ vertices = np.array([
     [50, 75],
     [75, 25],
     [75, 75]
-]).astype('f32')
+], dtype=np.float32)
 
 vertices += 0.33
 
@@ -34,8 +34,8 @@ arrows = np.array([
 def test_arrow_draw():
     """Test drawing arrows without transforms"""
 
-    with TestingCanvas() as c:
-        for arrow_type in ARROW_TYPES:
+    for arrow_type in ARROW_TYPES:
+        with TestingCanvas() as c:
             arrow = visuals.Arrow(pos=vertices, arrow_type=arrow_type,
                                   arrows=arrows, arrow_size=10, color='red',
                                   connect="segments", parent=c.scene)
@@ -43,15 +43,13 @@ def test_arrow_draw():
             assert_image_approved(c.render(), 'visuals/arrow_type_%s.png' %
                                   arrow_type)
 
-            arrow.parent = None
-
 
 @requires_application()
 def test_arrow_transform_draw():
     """Tests the ArrowVisual when a transform is applied"""
 
-    with TestingCanvas() as c:
-        for arrow_type in ARROW_TYPES:
+    for arrow_type in ARROW_TYPES:
+        with TestingCanvas() as c:
             arrow = visuals.Arrow(pos=vertices, arrow_type=arrow_type,
                                   arrows=arrows, arrow_size=10, color='red',
                                   connect="segments", parent=c.scene)
@@ -61,8 +59,6 @@ def test_arrow_transform_draw():
             assert_image_approved(c.render(),
                                   'visuals/arrow_transform_type_%s.png' %
                                   arrow_type)
-
-            arrow.parent = None
 
 
 @requires_application()
