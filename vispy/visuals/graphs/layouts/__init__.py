1dcec12c8696036d48d1fc23fbe6604cbaf9bc04
Lucas van Dijk <info@return1.net>
2015-08-04 15:05:57 +0200

    Add new way of retreiving graph layouts

--- vispy/visuals/graphs/layouts/__init__.py
+++ vispy/visuals/graphs/layouts/__init__.py
@@ -1,2 +1,24 @@
-from .random import random  # noqa
-from .circular import circular  # noqa
+import inspect
+
+from .random import random
+from .circular import circular
+from .force_directed import fruchterman_reingold
+
+
+_layout_map = {
+    'random': random,
+    'circular': circular,
+    'force_directed': fruchterman_reingold
+}
+
+
+def get(name, *args, **kwargs):
+    if name not in _layout_map:
+        raise KeyError("Graph layout '{}' not found.".format(name))
+
+    layout = _layout_map[name]
+
+    if inspect.isclass(layout):
+        layout = layout(*args, **kwargs)
+
+    return layout
