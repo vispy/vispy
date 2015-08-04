e743f8003e5c876b0d9c71c201c1058b5bc02340
bollu <siddu.druid@gmail.com>
2015-08-04 23:40:02 +0530

    update plotting example for new colorbar API

--- examples/basics/plotting/colorbar.py
+++ examples/basics/plotting/colorbar.py
@@ -13,16 +13,16 @@ plot = fig[0, 0]
 
 # note: "bottom" could also be used, but this would
 # conflict with top.
-orientations = ["top", "left", "right"]
+positions = ["top", "bottom", "left", "right"]
 
 
-for orientation in orientations:
-    cbar = plot.colorbar(orientation=orientation,
-                         label=orientation,
-                         clim=(0, 100),
-                         cmap="winter",
-                         border_width=1,
-                         border_color="#212121")
+for position in positions:
+    plot.colorbar(position=position,
+                  label=position,
+                  clim=(0, 100),
+                  cmap="winter",
+                  border_width=1,
+                  border_color="#212121")
 
 if __name__ == '__main__':
     fig.show(run=True)
