b551e2b9aed8af2e1b6396709ae202f23d28bcf3
bollu <siddu.druid@gmail.com>
2015-08-05 03:24:20 +0530

    change tests to use size

--- vispy/visuals/tests/test_colorbar.py
+++ vispy/visuals/tests/test_colorbar.py
@@ -11,9 +11,9 @@ from vispy.testing import (requires_application, TestingCanvas,
 from vispy.testing.image_tester import assert_image_approved
 
 
-def create_colorbar(pos, halfdim, orientation):
+def create_colorbar(pos, size, orientation):
     colorbar = visuals.ColorBar(pos=pos,
-                                halfdim=halfdim,
+                                size=size,
                                 orientation=orientation,
                                 label_str='label string here',
                                 cmap='autumn',
@@ -37,7 +37,7 @@ def test_colorbar_draw():
     """Test drawing Colorbar without transform using ColorbarVisual"""
     with TestingCanvas() as c:
         colorbar_top = create_colorbar(pos=(50, 50),
-                                       halfdim=(30, 2),
+                                       size=(60, 4),
                                        orientation='top')
 
         c.draw_visual(colorbar_top)
@@ -45,7 +45,7 @@ def test_colorbar_draw():
         colorbar_top.parent = None
 
         colorbar_bottom = create_colorbar(pos=(50, 50),
-                                          halfdim=(30, 2),
+                                          size=(60, 4),
                                           orientation='bottom')
 
         c.draw_visual(colorbar_bottom)
@@ -53,7 +53,7 @@ def test_colorbar_draw():
         colorbar_bottom.parent = None
 
         colorbar_left = create_colorbar(pos=(50, 50),
-                                        halfdim=(2, 30),
+                                        size=(60, 4),
                                         orientation='left')
 
         c.draw_visual(colorbar_left)
@@ -61,7 +61,7 @@ def test_colorbar_draw():
         colorbar_left.parent = None
 
         colorbar_right = create_colorbar(pos=(50, 50),
-                                         halfdim=(2, 30),
+                                         size=(60, 4),
                                          orientation='right')
 
         c.draw_visual(colorbar_right)
@@ -73,7 +73,7 @@ def test_reactive_draw():
     """Test reactive RectPolygon attributes"""
     with TestingCanvas() as c:
         colorbar = create_colorbar(pos=(50, 50),
-                                   halfdim=(30, 2),
+                                   size=(60, 4),
                                    orientation='top')
         c.draw_visual(colorbar)
 
@@ -109,43 +109,40 @@ def test_attributes():
     """Test if attribute checks are in place"""
     with TestingCanvas():
 
-        # initialize with wrong dimensions for orientations
+        # initialize with major axis < minor axis
         with raises(ValueError):
             create_colorbar(pos=(50, 50),
-                            halfdim=(1, 30),
+                            size=(1, 30),
                             orientation='top')
 
+        # set major axis to 0
         with raises(ValueError):
             create_colorbar(pos=(50, 50),
-                            halfdim=(1, 30),
-                            orientation='bottom')
-
-        with raises(ValueError):
-            create_colorbar(pos=(50, 50),
-                            halfdim=(30, 1),
-                            orientation='left')
-
-        with raises(ValueError):
-            create_colorbar(pos=(50, 50),
-                            halfdim=(30, 1),
+                            size=(0, 1),
                             orientation='right')
 
-        # set width to 0
+        # set negative major axis
         with raises(ValueError):
             create_colorbar(pos=(50, 50),
-                            halfdim=(0, 1),
+                            size=(-10, 1),
                             orientation='right')
 
-        # set height to 0
+        # set negative minor axis
         with raises(ValueError):
             create_colorbar(pos=(50, 50),
-                            halfdim=(1, 0),
+                            size=(1, -10),
+                            orientation='right')
+
+        # set minor axis to 0
+        with raises(ValueError):
+            create_colorbar(pos=(50, 50),
+                            size=(1, 0),
                             orientation='right')
 
         # set invalid orientation
         with raises(ValueError):
             create_colorbar(pos=(50, 50),
-                            halfdim=(30, 2),
+                            size=(60, 4),
                             orientation='top-invalid')
 
 run_tests_if_main()
