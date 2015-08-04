1bcbf167ab9bd5184fd95aab5e973f94a73201e7
bollu <siddu.druid@gmail.com>
2015-08-05 00:40:13 +0530

    added image to colorbar plot

--- examples/basics/plotting/colorbar.py
+++ examples/basics/plotting/colorbar.py
@@ -7,22 +7,41 @@ Plot different styles of ColorBar using vispy.plot
 """
 
 from vispy import plot as vp
+import numpy as np
 
-fig = vp.Fig(size=(800, 400), show=False)
+
+# arg( e^(1/z) )
+def exp_z_inv(x, y):
+    z = np.complex(x, y)
+    f = np.exp(1.0 / z)
+    return np.angle(f, deg=True)
+
+
+# create a 2d grid whose elements are of exp_z_inv
+def gen_image(width, height):
+    x_vals = np.linspace(-0.5, 0.5, width)
+    y_vals = np.linspace(-0.5, 0.5, height)
+
+    grid = np.meshgrid(x_vals, y_vals)
+    v_fn = np.vectorize(exp_z_inv)
+
+    return v_fn(*grid).astype(np.float)
+
+fig = vp.Fig(size=(800, 600), show=False)
 plot = fig[0, 0]
+plot.bgcolor = "#efefef"
 
-# note: "bottom" could also be used, but this would
-# conflict with top.
+img = gen_image(500, 500)
+plot.image(img, cmap="hsl")
+
 positions = ["top", "bottom", "left", "right"]
-
 
 for position in positions:
     plot.colorbar(position=position,
-                  label=position,
-                  clim=(0, 100),
-                  cmap="winter",
+                  label="argument of e^(1/z)",
+                  clim=("0°", "180°"),
+                  cmap="hsl",
                   border_width=1,
-                  border_color="#212121")
-
+                  border_color="#aeaeae")
 if __name__ == '__main__':
     fig.show(run=True)
