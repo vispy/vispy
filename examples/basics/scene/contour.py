c3f1d0fe5012cc20893a5eeaef47dfe11ea6db52
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2015-08-19 13:25:56 +0200

    WIP: isoline-filter implementation

---
+++ examples/basics/scene/contour.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# vispy: gallery 30
+# -----------------------------------------------------------------------------
+# Copyright (c) 2015, Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
+"""
+Simple use of SceneCanvas to display an Image.
+"""
+import sys
+from vispy import scene
+from vispy import app
+from vispy import visuals
+from vispy.util.filter import gaussian_filter
+from vispy.visuals.filters import Isoline
+import numpy as np
+from vispy.io import load_data_file, read_png
+
+canvas = scene.SceneCanvas(keys='interactive')
+canvas.size = 800, 600
+canvas.show()
+
+# Set up a viewbox to display the image with interactive pan/zoom
+view = canvas.central_widget.add_view()
+
+# Create the image
+interpolation = 'bicubic'
+np.random.seed(1000)
+img_data = np.random.normal(size=(100, 100), loc=50, scale=150)
+img_data = gaussian_filter(img_data, (4, 4, 0)).astype(np.float32)
+
+img_data = np.zeros(25).reshape((5, 5)).astype(np.float32)
+img_data[1:4, 1::2] = 0.5
+img_data[1::2, 2] = 0.5
+img_data[2, 2] = 1.0
+
+#img_data = read_png(load_data_file('mona_lisa/mona_lisa_sm.png'))
+#img_data = np.dot(img_data[...,:3], [0.299, 0.587, 0.144])
+#img_data = img_data[...,0] * img_data[...,1] * img_data[...,2]
+#img_data /= 3.
+#image = scene.visuals.Image(img_data, interpolation=interpolation,
+#                            parent=view.scene, method='subdivide')
+
+img_data1 = np.zeros((100, 100))
+img_data1[0,0] = -0.5
+img_data1[99,99] = +0.5
+image = scene.visuals.Image(img_data, interpolation=interpolation,
+                            parent=view.scene, method='subdivide')
+
+#image = scene.visuals.Contour(img_data, interpolation='bicubic1',
+#                            parent=view.scene, method='subdivide',
+#                            levels=2, width=1.0, color='yellow')
+
+image.transform = visuals.transforms.STTransform(translate=(0, 0, 0.5))
+
+levels = np.linspace(img_data.min(), img_data.max(), num=13, endpoint=True)[1:-1]
+color_lev = 'white'
+# Create isocurve, make a child of the image to ensure the two are always
+# aligned.
+#curve = scene.visuals.Isocurve(img_data, levels=levels, color_lev=color_lev,
+#                              parent=view.scene, method='subdivide')
+
+iso = Isoline(level=10, width=1., color='black')
+
+canvas.title = 'Spatial Filtering using %s Filter' % interpolation
+
+# Set 2D camera (the camera will scale to the contents in the scene)
+view.camera = scene.PanZoomCamera(aspect=1)
+# flip y-axis to have correct aligment
+view.camera.flip = (0, 1, 0)
+view.camera.set_range()
+
+# get interpolation functions from Image
+names = image.interpolation_functions
+names = list(names)
+names.sort()
+print(names)
+act = 17
+
+act = -1
+
+level = 2
+first = True
+
+
+# Implement key presses
+@canvas.events.key_press.connect
+def on_key_press(event):
+    global act, level, first, interpolation
+    if event.key in ['Left', 'Right']:
+        if event.key == 'Right':
+            step = 1
+        else:
+            step = -1
+        act = (act + step) % len(names)
+        interpolation = names[act]
+        image.interpolation = interpolation
+
+    if event.key in ['Up', 'Down']:
+        if first:
+            image.attach(iso)
+            first = False
+        if event.key == 'Up':
+            level += 1
+        else:
+            if level > 1:
+                level -= 1
+        #levels = np.linspace(img_data.min(), img_data.max(), num=level+1, endpoint=True)[1:-1]
+        #curve.levels = levels
+        iso.level = level
+    canvas.title = 'Spatial Filtering using %s Filter - Isoline %d level' % (interpolation, level)
+    canvas.update()
+
+
+if __name__ == '__main__' and sys.flags.interactive == 0:
+    app.run()
--- vispy/visuals/filters/color.py
+++ vispy/visuals/filters/color.py
@@ -5,7 +5,139 @@
 import weakref
 
 from ..shaders import Function, Varying
-from ...color import colormap
+from ...color import colormap, Color
+
+class Isoline(object):
+    def __init__(self, level=10., width=1.0, color='black', antialias=1.0):
+        self.vshader = Function("""
+            void position_support()
+            {
+                $coords = gl_Position.xyz;
+            }
+        """)
+
+        self.fshader = Function("""
+            void isoline() {
+                // function taken from glumpy/examples/isocurves.py
+                // and extended to have level, width and color as parameters
+
+                // Extract data value
+                vec3 val3 = gl_FragColor.rgb;
+                //vec3 val3 = $coords.rgb;
+
+
+                const vec3 w = vec3(0.299, 0.587, 0.114);
+                //const vec3 w = vec3(0.2126, 0.7152, 0.0722);
+                //float value = dot(gl_FragColor.rgb, w);
+
+                float value = dot(val3, w);
+
+
+                // setup lw, aa
+                float linewidth = $isowidth + $antialias;
+
+                // "middle" contour(s) dividing upper and lower half
+                // but only if isolevel is even
+                if( mod($isolevel,2.0) == 0.0 ) {
+                    if( length(value - 0.5) < 0.5 / $isolevel)
+                        linewidth = linewidth * 2;
+                }
+
+                // Trace contouriso
+                float v  = $isolevel * value - 0.5;
+                vec3 v3  = $isolevel * val3 - 0.5;
+
+                //float dv = linewidth/2.0 * fwidth(v);
+                vec3 dv3 = linewidth/2.0 * fwidth(v3);
+
+                //float f = abs(fract(v) - 0.5);
+                vec3 f3 = abs(fract(v3) - 0.5);
+
+                //float d = smoothstep(-dv, +dv, f);
+                vec3 d3 = smoothstep(-dv3, +dv3, f3);
+
+                float t = linewidth/2.0 - $antialias;
+
+                //d = abs(d)*linewidth/2.0 - t;
+                float d = d3.x * d3.y *d3.z *linewidth/2.0 - t;
+
+                if( d < - linewidth ) {
+                    d = 1.0;
+                } else  {
+                     d /= $antialias;
+                }
+
+                vec4 bg = $color_transform1(gl_FragColor);
+
+                vec4 fc = vec4($isocolor.rgb, 0);
+
+                if (d < 1.) {
+                    fc.a = 1-d;
+                }
+
+                gl_FragColor = mix(bg, fc, fc.a);
+
+            }
+        """)
+        self.level = level
+        self.width = width
+        self.color = color
+        self.antialias = antialias
+        #self.vshader['coords'] = Varying('coords', dtype='vec3')
+        #self.fshader['coords'] = self.vshader['coords']
+
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, l):
+        self._level = l
+        self.fshader['isolevel'] = l
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, w):
+        self._width = w
+        self.fshader['isowidth'] = w
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, c):
+        self._color = c
+        self.fshader['isocolor'] = Color(c).rgba
+
+    @property
+    def antialias(self):
+        return self._antialias
+
+    @antialias.setter
+    def antialias(self, a):
+        self._antialias = a
+        self.fshader['antialias'] = a
+
+    def _attach(self, visual):
+        visual._get_hook('frag', 'post').add(self.fshader())
+        #visual._get_hook('vert', 'post').add(self.vshader())
+
+        try:
+            # this works for single and multi channel data
+            self.fshader['color_transform1'] = \
+                visual.shared_program.frag['color_transform']
+            visual.shared_program.frag['color_transform'] = \
+                Function('vec4 pass(vec4 color) { return color; }')
+        except:
+            self.fshader['color_transform1'] = \
+                Function('vec4 pass(vec4 color) { return color; }')
+
+        #self.vshader['position'] = visual.shared_program.vert['position']
+
 
 
 class Alpha(object):
