eb45e206255441090525e1c1bc26fc6dcf7c19fa
Julien Rebetez <julien.rebetez@heig-vd.ch>
2015-08-27 18:14:20 +0200

    OSMesa test program

---
+++ examples/offscreen/simple_osmesa.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# vispy: testskip
+# Copyright (c) 2015, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+"""
+This is a simple osmesa example that produce an image of a cube
+
+Execute with something like :
+    VISPY_GL_LIB=/opt/osmesa_llvmpipe/lib/libglapi.so \
+        LD_LIBRARY_PATH=/opt/osmesa_llvmpipe/lib/ \
+        OSMESA_LIBRARY=/opt/osmesa_llvmpipe/lib/libOSMesa.so \
+        python examples/offscreen/simple_osmesa.py
+"""
+##
+import vispy.app
+vispy.app.use_app(backend_name='osmesa')
+
+import numpy as np
+import vispy.plot as vp
+import vispy.io as io
+##
+data = np.load(io.load_data_file('electrophys/iv_curve.npz'))['arr_0']
+time = np.arange(0, data.shape[1], 1e-4)
+
+fig = vp.Fig(size=(800, 800), show=False)
+
+x = np.linspace(0, 10, 20)
+y = np.cos(x)
+line = fig[0, 0].plot((x, y), symbol='o', width=3, title='I/V Curve',
+                      xlabel='Current (pA)', ylabel='Membrane Potential (mV)')
+grid = vp.visuals.GridLines(color=(0, 0, 0, 0.5))
+grid.set_gl_state('translucent')
+fig[0, 0].view.add(grid)
+
+fig.show(run=True)
+
+## -- Save to PNG
+img = fig.render()
+io.write_png("osmesa.png", img)
+
+##
+if __name__ == '__main__':
+    fig.show(run=True)
+##
