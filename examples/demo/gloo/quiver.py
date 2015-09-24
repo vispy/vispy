ec388cab88d211263dd3802b593cc00b29e2cfb5
Dzhelil Rufat <drufat@caltech.edu>
2015-09-24 09:33:14 -0700

    Use set_state instead.

--- examples/demo/gloo/quiver.py
+++ examples/demo/gloo/quiver.py
@@ -261,13 +261,9 @@ void main()
 
 
 canvas = app.Canvas(size=(2*512, 2*512), keys='interactive')
-
-
-@canvas.connect
-def on_initialize(event):
-    gl.glEnable(gl.GL_BLEND)
-    gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-    gl.glBlendEquation(gl.GL_FUNC_ADD)
+canvas.context.set_state(blend=True, 
+                         blend_func=('src_alpha', 'one_minus_src_alpha'),
+                         blend_equation='func_add')
 
 
 @canvas.connect
