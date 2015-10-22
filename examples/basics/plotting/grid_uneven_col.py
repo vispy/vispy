22b26ee2749603490faffee9ffe97942732461da
bollu <siddu.druid@gmail.com>
2015-10-22 18:57:27 +0530

    renamed file, now renders correctly as expected

--- examples/basics/plotting/grid_uneven_col.py
+++ examples/basics/plotting/grid_uneven_col.py
@@ -5,11 +5,11 @@
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
 """
-+-----+-----------+
-| 1   |     2     |
 +-----+----+------+
-|   3      |   4  |
-+----------+------+
+| 1   |  2 |  2   |
++-----+----+------+
+|   3 |    |   4  |
++-----+----+------+
 """
 
 import sys
@@ -25,15 +25,15 @@ grid = canvas.central_widget.add_grid()
 widget_top_left = grid.add_widget(row=0, col=0)
 widget_top_left.bgcolor = "#999999"
 
-widget_top_right = grid.add_widget(row=0, col=1)
+widget_top_right = grid.add_widget(row=0, col=1, col_span=2)
 widget_top_right.bgcolor = "#dd0000"
 widget_top_right.stretch = (2, 1)
 
-widget_bottom_left = grid.add_widget(row=1, col=0)
+widget_bottom_left = grid.add_widget(row=1, col=0, col_span=2)
 widget_bottom_left.bgcolor = "#0000dd"
 widget_bottom_left.stretch = (2, 1)
 
-widget_bottom_right = grid.add_widget(row=1, col=1)
+widget_bottom_right = grid.add_widget(row=1, col=2)
 widget_bottom_right.bgcolor = "#00dd00"
 
 
