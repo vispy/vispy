a908b673e83f6df45266da69a6f49e42cbfbc447
bollu <siddu.druid@gmail.com>
2016-02-15 14:16:30 +0530

    added a file that can be run with ipython -i to setup a basic plotting env

---
+++ examples/basics/plotting/ipython-base.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Vispy Development Team.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+"""
+Bare bones plotting region that can be used with the python
+interpreter as a playground.
+
+Run with
+python -i ipython-base.py
+"""
+
+
+import vispy
+from vispy.plot import *
+
+
+fig = Fig()
+plotwidget = fig[0]
+
+
+grid = plotwidget.plot([1, 2, 3])
