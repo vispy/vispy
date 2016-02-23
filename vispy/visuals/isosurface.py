90642ff97100b4aec2a12f643a80817cab5f2b70
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-02-23 17:42:51 +0000

    Fix bug that caused settable Isosurface.color property to not work. Colors can now be specified in Isosurface.set_data.

--- vispy/visuals/isosurface.py
+++ vispy/visuals/isosurface.py
@@ -6,6 +6,7 @@ from __future__ import division
 
 from .mesh import MeshVisual
 from ..geometry.isosurface import isosurface
+from ..color import Color
 
 
 class IsosurfaceVisual(MeshVisual):
@@ -21,9 +22,13 @@ class IsosurfaceVisual(MeshVisual):
     Notes
     -----
     """
-    def __init__(self, data=None, level=None, **kwargs):
+    def __init__(self, data=None, level=None, vertex_colors=None,
+                 face_colors=None, color=(0.5, 0.5, 1, 1), **kwargs):
         self._data = None
         self._level = level
+        self._vertex_colors = vertex_colors
+        self._face_colors = face_colors
+        self._color = Color(color)
         self._recompute = True
         MeshVisual.__init__(self, **kwargs)
         if data is not None:
@@ -42,7 +47,7 @@ class IsosurfaceVisual(MeshVisual):
         self._recompute = True
         self.update()
 
-    def set_data(self, data):
+    def set_data(self, data=None, vertex_colors=None, face_colors=None, color=None):
         """ Set the scalar array data
 
         Parameters
@@ -50,8 +55,22 @@ class IsosurfaceVisual(MeshVisual):
         data : ndarray
             A 3D array of scalar values. The isosurface is constructed to show
             all locations in the scalar field equal to ``self.level``.
+        vertex_colors : array-like | None
+            Colors to use for each vertex.
+        face_colors : array-like | None
+            Colors to use for each face.
+        color : instance of Color
+            The color to use.
         """
-        self._data = data
+        # We only change the internal variables if they are provided by the user
+        if data is not None:
+            self._data = data
+        if vertex_colors is not None:
+            self._vertex_colors = vertex_colors
+        if face_colors is not None:
+            self._face_colors = face_colors
+        if color is not None:
+            self._color = Color(color)
         self._recompute = True
         self.update()
 
@@ -61,7 +80,8 @@ class IsosurfaceVisual(MeshVisual):
 
         if self._recompute:
             verts, faces = isosurface(self._data, self._level)
-            MeshVisual.set_data(self, vertices=verts, faces=faces)
+            MeshVisual.set_data(self, vertices=verts, faces=faces,
+                                color=self._color)
             self._recompute = False
 
         return MeshVisual._prepare_draw(self, view)
