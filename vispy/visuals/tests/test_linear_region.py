acdd5b5bae4fb8154657e01d472e1416e30a3a51
Alessandro Bacchini <allebacco@gmail.com>
2016-05-23 20:30:13 +0200

    Update tests for LinearRegionVisual, both code and documentation.

--- vispy/visuals/tests/test_linear_region.py
+++ vispy/visuals/tests/test_linear_region.py
@@ -17,10 +17,12 @@ from vispy.testing import raises
 
 
 @requires_application()
-def test_set_data_pos():
-    """Test the pos argument of LinearRegionVisual.set_data() method"""
+def test_linear_region_vertical_horizontal():
+    """Test vertical and horizontal LinearRegionVisual with a single color"""
 
+    # Definition of the region
     pos = np.array([5, 15, 24, 36, 40, 42], dtype=np.float32)
+    # Expected internal pos buffer for vertical region
     expected_pos_v = np.array([[5.0, -1.],
                                [5.0, 1.],
                                [15.0, -1.],
@@ -33,10 +35,13 @@ def test_set_data_pos():
                                [40.0, 1.],
                                [42.0, -1.],
                                [42.0, 1.]], dtype=np.float32)
+    # Expected internal pos buffer for horizontal region
     expected_pos_h = np.array([expected_pos_v[:, 1] * -1,
                                expected_pos_v[:, 0]], dtype=np.float32).T
 
-    for is_vertical in [True, False]:
+    # Test both horizontal and vertical region
+    for is_vertical, reference_image in [(True, 'linear_region1.png'),
+                                         (False, 'linear_region1_h.png')]:
 
         expected_pos = expected_pos_v if is_vertical else expected_pos_h
 
@@ -50,10 +55,14 @@ def test_set_data_pos():
             assert np.all(region.pos == pos)
             assert region.is_vertical == is_vertical
 
+            # Check set_data is working as expected when passing a list as
+            # pos argument
             region.set_data(pos=list(pos))
             assert np.all(region._pos == expected_pos)
             assert np.all(region.pos == pos)
 
+            # Check set_data is working as expected when passing a tuple as
+            # pos argument
             region.set_data(pos=tuple(pos))
             assert np.all(region._pos == expected_pos)
             assert np.all(region.pos == pos)
@@ -64,24 +73,23 @@ def test_set_data_pos():
                 assert np.all(region._pos == expected_pos)
                 assert np.all(region.pos == pos)
 
-            if is_vertical:
-                assert_image_approved(c.render(), 'visuals/linear_region1.png')
-            else:
-                assert_image_approved(c.render(),
-                                      'visuals/linear_region1_h.png')
+            assert_image_approved(c.render(), 'visuals/%s' % reference_image)
 
-            # Only 1D array are accepted
+            # Check ValueError is raised when pos is not 1D
             with raises(ValueError):
                 region.set_data(pos=[[1, 2], [3, 4]])
 
 
 @requires_application()
-def test_set_data_color_singlecolor():
+def test_linear_region_color():
     """Test the color argument of LinearRegionVisual.set_data() method
     using a single color"""
 
+    # Definition of the region
     pos1 = [5, 42]
+    # Definition of the color of the region
     color1 = np.array([0.0, 1.0, 0.0, 0.5], dtype=np.float32)
+    # Expected internal color buffer
     color1_expected = np.array([color1, color1, color1, color1],
                                dtype=np.float32)
 
@@ -91,10 +99,14 @@ def test_set_data_color_singlecolor():
         assert np.all(region._color == color1_expected)
         assert np.all(region.color == color1)
 
+        # Check set_data is working as expected when passing a list as
+        # color argument
         region.set_data(color=list(color1))
         assert np.all(region._color == color1_expected)
         assert np.all(region.color == color1)
 
+        # Check set_data is working as expected when passing a tuple as
+        # color argument
         region.set_data(color=tuple(color1))
         assert np.all(region._color == color1_expected)
         assert np.all(region.color == color1)
@@ -106,24 +118,27 @@ def test_set_data_color_singlecolor():
 
         assert_image_approved(c.render(), 'visuals/linear_region1.png')
 
+        # Check a ValueError is raised when the length of color argument
+        # is not 4.
         with raises(ValueError):
             region.set_data(color=[1.0, 0.5, 0.5])
 
+        # Check a ValueError is raised when too many colors are provided
         with raises(ValueError):
             region.set_data(color=[color1, color1, color1])
 
-        region.parent = None
-
 
 @requires_application()
-def test_set_data_color_multicolor():
-    """Test the color argument of LinearRegionVisual.set_data() method
-    using multiple colors"""
+def test_linear_region_gradient():
+    """Test LinearRegionVisual with a gradient as color"""
 
+    # Definition of the region
     pos2 = [5, 42, 80]
+    # Definition of the color of the region
     color2 = np.array([[0.0, 1.0, 0.0, 0.5],
                        [1.0, 0.0, 0.0, 0.75],
                        [0.0, 0.0, 1.0, 1.0]], dtype=np.float32)
+    # Expected internal color buffer
     color2_expected = np.array([color2[0], color2[0],
                                 color2[1], color2[1],
                                 color2[2], color2[2]],
@@ -135,22 +150,7 @@ def test_set_data_color_multicolor():
         assert np.all(region._color == color2_expected)
         assert np.all(region.color == color2)
 
-        region.set_data(color=list(color2))
-        assert np.all(region._color == color2_expected)
-        assert np.all(region.color == color2)
-
-        region.set_data(color=tuple(color2))
-        assert np.all(region._color == color2_expected)
-        assert np.all(region.color == color2)
-
-        # Test with different dtypes that must be converted to float32
-        region.set_data(color=color2.astype(np.float64))
-        assert np.all(region._color == color2_expected)
-        assert np.all(region.color == color2)
-
         assert_image_approved(c.render(), 'visuals/linear_region2.png')
-
-        region.parent = None
 
 
 run_tests_if_main()
