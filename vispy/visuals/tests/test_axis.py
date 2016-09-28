c32e29084064ca58ef0d505f4a2e359572af085e
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-28 11:35:24 +0100

    Add alternative test for AxisVisual

--- vispy/visuals/tests/test_axis.py
+++ vispy/visuals/tests/test_axis.py
@@ -4,6 +4,7 @@
 Tests for AxisVisual
 """
 
+from vispy import scene
 from vispy.scene import visuals
 from vispy.testing import (requires_application, TestingCanvas,
                            run_tests_if_main)
@@ -14,6 +15,17 @@ def test_axis():
     with TestingCanvas() as c:
         axis = visuals.Axis(pos=[[-1.0, 0], [1.0, 0]])
         c.draw_visual(axis)
+
+@requires_application()
+def test_axis_alt():
+
+    canvas = scene.SceneCanvas(keys=None, size=(800, 600), show=True)
+    view = canvas.central_widget.add_view()
+    scene_transform = scene.STTransform()
+    view.camera = scene.cameras.TurntableCamera(parent=view.scene,
+                                                fov=0., distance=4.0)
+    axes = visuals.Axis(pos=[[-1.0, 0], [1.0, 0]], parent=view.scene)
+    canvas.render()
 
 
 run_tests_if_main()
