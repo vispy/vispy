fae3a3f6bc1e47df9de8f1a3f46bc8f1084e3937
ktritz <ktritz@gmail.com>
2016-11-22 22:36:30 -0500

    Various fixes
    Changed default shading to auto (smooth unless face_colors is set)

--- vispy/plot/plotwidget.py
+++ vispy/plot/plotwidget.py
@@ -243,9 +243,10 @@ class PlotWidget(scene.Widget):
         meshdata : MeshData | None
             Meshdata to use. If not None, then vertices, faces, and fname
             must be None.
-        shading : str | 'auto'
-            Shading to use. Default shading is set to 'smooth'. If face_colors
-            is set, shading is set to None.
+        shading : str
+            Shading to use, can be None, 'smooth', 'flat', or 'auto'.
+            Default ('auto') will use None if face_colors is set, and
+            'smooth' otherwise.
 
         Returns
         -------
@@ -253,9 +254,10 @@ class PlotWidget(scene.Widget):
             The mesh.
         """
         self._configure_3d()
-        shading = 'smooth'
-        if face_colors is not None:
-            shading = None
+        if shading == 'auto':
+            shading = 'smooth'
+            if face_colors is not None:
+                shading = None
         if fname is not None:
             if not all(x is None for x in (vertices, faces, meshdata)):
                 raise ValueError('vertices, faces, and meshdata must be None '
@@ -266,8 +268,8 @@ class PlotWidget(scene.Widget):
                 raise ValueError('vertices, faces, and fname must be None if '
                                  'fname is not None')
         else:
-            meshdata = MeshData(vertices, faces, face_colors=face_colors,
-                                vertex_colors=vertex_colors)
+            meshdata = MeshData(vertices, faces, vertex_colors=vertex_colors,
+                          face_colors=face_colors)
         mesh = scene.Mesh(meshdata=meshdata, vertex_colors=vertex_colors,
                           face_colors=face_colors, color=color,
                           shading=shading)
