910559e8ed6e5835d0fc766f51f937cf49a17af4
davidh-ssec <david.hoese@ssec.wisc.edu>
2017-10-02 12:44:31 +0200

    Fix collections for python 3

--- vispy/visuals/collections/agg_path_collection.py
+++ vispy/visuals/collections/agg_path_collection.py
@@ -136,8 +136,8 @@ class AggPathCollection(Collection):
            Path antialias area
         """
 
-        itemsize = itemsize or len(P)
-        itemcount = len(P) / itemsize
+        itemsize = int(itemsize or len(P))
+        itemcount = len(P) // itemsize
 
         # Computes the adjacency information
         n, p = len(P), P.shape[-1]
@@ -155,7 +155,7 @@ class AggPathCollection(Collection):
                 V[name] = kwargs.get(name, self._defaults[name])
 
         # Extract relevant segments only
-        V = (V.reshape(n / itemsize, itemsize)[:, :-1])
+        V = (V.reshape(n // itemsize, itemsize)[:, :-1])
         if closed:
             V['p0'][:, 0] = V['p2'][:, -1]
             V['p3'][:, -1] = V['p1'][:, 0]
