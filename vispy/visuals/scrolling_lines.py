27c26ce5641747863b8a0166aafb73585aa30942
rwarren <russ@perspexis.com>
2017-10-19 14:30:29 -0400

    fix demo/test generation (per instructions from @larsoner)

--- vispy/visuals/scrolling_lines.py
+++ vispy/visuals/scrolling_lines.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# vispy: gallery 30
 # -----------------------------------------------------------------------------
 # Copyright (c) Vispy Development Team. All Rights Reserved.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
