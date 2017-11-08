a4dbf051ae6d4b005f28f0cec53daab38e3358cb
macrocosme <danyvohl@hotmail.com>
2017-11-08 12:49:08 +0200

    Adding ._colourbar. to access label_str
    Acting on @kmuehlbauer request

--- vispy/scene/widgets/colorbar.py
+++ vispy/scene/widgets/colorbar.py
@@ -134,7 +134,7 @@ class ColorBarWidget(Widget):
 
     @property
     def label_str(self):
-        return self._label_str
+        return self._colorbar._label_str
 
     @label_str.setter
     def label_str(self, label_str):
