fb04e9873f044d910b26dd964fc228527101f2dd
davidh-ssec <david.hoese@ssec.wisc.edu>
2018-03-06 14:54:00 -0600

    Hack to see if not running npm on appveyor will fix pytest collection

--- setup.py
+++ setup.py
@@ -141,6 +141,9 @@ class NPM(Command):
 
     def has_npm(self):
         npmName = self.get_npm_name();
+        if platform.system() == 'Windows':
+            # HACK: See if we can get appveyor installation to work
+            return False
         try:
             check_call([npmName, '--version'])
             return True
