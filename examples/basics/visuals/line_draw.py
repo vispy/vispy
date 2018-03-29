9aa886722a208cdbfba4687d21aa470d692af2e8
Felix Schill <felix.schill@hydromea.com>
2018-03-29 22:48:11 +0200

    fixed indentation to pass travis checks

--- examples/basics/visuals/line_draw.py
+++ examples/basics/visuals/line_draw.py
@@ -121,9 +121,9 @@ class EditLineVisual(scene.visuals.Line):
         if self.selected_point is not None:
             # update selected point to new position given by mouse
             self.selected_point[0] = round(pos_scene[0] / self.gridsize) \
-                                     * self.gridsize
+                * self.gridsize
             self.selected_point[1] = round(pos_scene[1] / self.gridsize) \
-                                     * self.gridsize
+                * self.gridsize
             self.set_data(pos=self.pos)
             self.update_markers(self.selected_index)
 
