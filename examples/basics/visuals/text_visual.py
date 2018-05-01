d06fb25a2ae5ef1f0bf2f6a3ab7a15518ac3a023
Kristian <mkkb4987@hotmail.com>
2018-05-01 11:25:35 +0200

    Revision

--- examples/basics/visuals/text_visual.py
+++ examples/basics/visuals/text_visual.py
@@ -1,32 +1,48 @@
+# -*- coding: utf-8 -*-
+# vispy: gallery 30
+# -----------------------------------------------------------------------------
+# Copyright (c) Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
+
+
 from vispy import app, gloo, visuals
 import numpy as np
 
 
 class Canvas(app.Canvas):
+    """
+    This is an interactive example of how to display text on app.Canvas. The text
+    rendering will respond to standard C escape sequences.
+
+    - Scroll for changing text font size
+    - Press left and right arrows for changing text anchor positions
+    - Press up and down arrows for changing text content
+
+    Standard C escape sequences:
+        -   \a  ->  alert (beep, bell)
+        -   \b  ->  Backspace
+        -   \f  ->  Formfeed
+        -   \n  ->  Newline
+        -   \r  ->  Carriage Return
+        -   \t  ->  Horizontal Tab
+        -   \v  ->  Vertical Tab
+        -   \\  ->  Backslash
+        -   \'  ->  Single quotation mark
+        -   \"  ->  Double quotation mark
+    """
+
     def __init__(self):
         app.Canvas.__init__(self, title='Glyphs', keys='interactive')
 
-        self.font_size = 48.
+        self.font_size = 9.
 
         # Create a cross eye of lines for easy see if text anchor positions are good
         l_pos = np.array([[-1.0,0.0], [1.0,0.0], [0.0,0.0], [0.0,1.0], [0.0,-1.0]])
         self.cross_eye_line = visuals.LineVisual(pos=l_pos, color=(1.0, 0.0, 0.0, 1), method='gl')
 
-        """
-        Standard C escape sequences:
-            -   \a  ->  alert (beep, bell)
-            -   \b  ->  Backspace
-            -   \f  ->  Formfeed
-            -   \n  ->  Newline
-            -   \r  ->  Carriage Return
-            -   \t  ->  Horizontal Tab
-            -   \v  ->  Vertical Tab
-            -   \\  ->  Backslash
-            -   \'  ->  Single quotation mark
-            -   \"  ->  Double quotation mark
-        """
         the_big_test_string = 'This is the big test string!\n'
-        the_big_test_string += 'It includes all of the esqape sequences known\n'
+        the_big_test_string += 'It includes all of the escape sequences known\n'
         the_big_test_string += 'to man:\n\n'
         the_big_test_string += '\t-\t\\n\n'
         the_big_test_string += '\t-\t\\v\n'
@@ -36,20 +52,21 @@ class Canvas(app.Canvas):
         the_big_test_string += 'decent job and deserves a beer and a digital salute\a! ;)'
         the_big_test_string += '\vThe end!'
         self.string_alternatives = [
-            '%s pt, Hello (scroll/press arrows to change text properties)|\a|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\b|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\f|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\n|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\r|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\t|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\v|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\\|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\'|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\"|how are u' % round(self.font_size, 1),
-            '%s pt, Hello (scroll/press arrows to change text properties)|\?|how are u' % round(self.font_size, 1),
+            '',
+            'Hello (scroll/arrows to change text properties)|\a|how are u',
+            'Hello (scroll/arrows to change text properties)|\b|how are u',
+            'Hello (scroll/arrows to change text properties)|\f|how are u',
+            'Hello (scroll/arrows to change text properties)|\n|how are u',
+            'Hello (scroll/arrows to change text properties)|\r|how are u',
+            'Hello (scroll/arrows to change text properties)|\t|how are u',
+            'Hello (scroll/arrows to change text properties)|\v|how are u',
+            'Hello (scroll/arrows to change text properties)|\\|how are u',
+            'Hello (scroll/arrows to change text properties)|\'|how are u',
+            'Hello (scroll/arrows to change text properties)|\"|how are u',
+            'Hello (scroll/arrows to change text properties)|\?|how are u',
             the_big_test_string,
             ]
-        self.str_ind = 10
+        self.str_ind = 0
 
         # anchor_x , anchor_y
         self.anchor_variants = [
@@ -65,8 +82,8 @@ class Canvas(app.Canvas):
         ]
         self.anchor_ind = 0
 
-        self.text = visuals.TextVisual('', bold=True)
-        self.apply_zoom()
+        self.text = visuals.TextVisual('', bold=True, pos=(0.,0.))
+        self.update_text()
 
     def on_draw(self, event):
         gloo.clear(color='white')
@@ -78,14 +95,14 @@ class Canvas(app.Canvas):
         """Use the mouse wheel to zoom."""
         self.font_size *= 1.25 if event.delta[1] > 0 else 0.8
         self.font_size = max(min(self.font_size, 160.), 6.)
-        self.apply_zoom()
+        self.update_text()
 
     def on_resize(self, event):
         # Set canvas viewport and reconfigure visual transforms to match.
         vp = (0, 0, self.physical_size[0], self.physical_size[1])
         self.context.set_viewport(*vp)
         self.text.transforms.configure(canvas=self, viewport=vp)
-        self.apply_zoom()
+        self.update_text()
 
     def on_key_release(self, event):
         if 'Down' in str(event.key):
@@ -108,12 +125,13 @@ class Canvas(app.Canvas):
                 self.anchor_ind = 0
             else:
                 self.anchor_ind += 1
-        extra_txt = "\n(anchor_x = " + self.anchor_variants[self.anchor_ind][1]
-        extra_txt += ", anchor_y = " + self.anchor_variants[self.anchor_ind][0] + ")"
-        self.apply_zoom(extra_txt)
+        self.update_text()
 
-    def apply_zoom(self, extra_txt=""):
-        self.text.text = self.string_alternatives[self.str_ind] + extra_txt
+    def update_text(self):
+        pre_text = '%s pt, '% round(self.font_size, 1)
+        post_text = "\n(anchor_x = " + self.anchor_variants[self.anchor_ind][1]
+        post_text += ", anchor_y = " + self.anchor_variants[self.anchor_ind][0] + ")"
+        self.text.text = pre_text + self.string_alternatives[self.str_ind] + post_text
 
         anchor_x = self.anchor_variants[self.anchor_ind][1]
         anchor_y = self.anchor_variants[self.anchor_ind][0]
