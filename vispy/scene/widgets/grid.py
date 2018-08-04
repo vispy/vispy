e5eff3fefbad95497a74745cd805a3048809b888
Mike Kubanski <mkubanski.dev@gmail.com>
2018-08-04 14:20:52 -0700

    Fix grid size check on solver to handle float values

--- vispy/scene/widgets/grid.py
+++ vispy/scene/widgets/grid.py
@@ -460,7 +460,7 @@ class Grid(Widget):
 
         # we only need to remove and add the height and width constraints of
         # the solver if they are not the same as the current value
-        if rect.height != self._var_h.value:
+        if abs(rect.height - self._var_h.value) > 1e-4:
             if self._height_stay:
                 self._solver.remove_constraint(self._height_stay)
 
@@ -468,7 +468,7 @@ class Grid(Widget):
             self._height_stay = self._solver.add_stay(self._var_h,
                                                       strength=STRONG)
 
-        if rect.width != self._var_w.value:
+        if abs(rect.width - self._var_w.value) > 1e-4:
             if self._width_stay:
                 self._solver.remove_constraint(self._width_stay)
 
