288e248bdacf7cd3de777627afcd50d54fc31ce7
Kira Evans <contact@kne42.me>
2018-09-25 21:56:06 -0700

    re-implement most filters to subclass base filter

--- vispy/visuals/filters/picking.py
+++ vispy/visuals/filters/picking.py
@@ -4,32 +4,38 @@
 import struct
 import weakref
 
+from .base_filter import Filter
 from ..shaders import Function
 
 
-class PickingFilter(object):
-    """Filter used to color visuals by a picking ID. 
-    
+class PickingFilter(Filter):
+    """Filter used to color visuals by a picking ID.
+
     Note that the ID color uses the alpha channel, so this may not be used
     with blending enabled.
     """
+    __slots__ = ('_id', '_id_color', '_enabled')
+
+    FRAG_SHADER = """
+        void picking_filter() {
+            if( $enabled == 0 )
+                return;
+            if( gl_FragColor.a == 0.0 )
+                discard;
+            gl_FragColor = $id_color;
+        }
+    """
+    FRAG_HOOK = 'post'
+    FRAG_POSITION = 10
+
     def __init__(self, id_=None):
-        self.shader = Function("""
-            void picking_filter() {
-                if( $enabled == 0 )
-                    return;
-                if( gl_FragColor.a == 0.0 )
-                    discard;
-                gl_FragColor = $id_color;
-            }
-        """)
         self.id = id_
         self.enabled = False
 
     @property
     def id(self):
         return self._id
-        
+
     @id.setter
     def id(self, id):
         if id < 1:
@@ -42,20 +48,15 @@ class PickingFilter(object):
     @property
     def enabled(self):
         return self._enabled
-    
+
     @enabled.setter
     def enabled(self, e):
         self._enabled = e
-        self.shader['enabled'] = 1 if e is True else 0
-        
+        self.fshader['enabled'] = 1 if e is True else 0
+
     @property
     def color(self):
         """ The RGBA color that will be drawn to the framebuffer for visuals
         that use this filter.
         """
         return self._id_color
-        
-    def _attach(self, visual):
-        self._visual = weakref.ref(visual)
-        hook = self._visual()._get_hook('frag', 'post')
-        hook.add(self.shader(), position=10)
