11544d531c52507fe3f88b0f3936ee25d460eff0
GuillaumeFavelier <guillaume.favelier@gmail.com>
2019-02-21 17:20:25 +0100

    Disable textures, check geometry and add terrain-type interaction

--- examples/demo/gloo/skybox.py
+++ examples/demo/gloo/skybox.py
@@ -1,67 +1,169 @@
-#Copyright (c) 2009-2016 Nicolas P. Rougier. All rights reserved.
-# Distributed under the (new) BSD License.
-# -----------------------------------------------------------------------------
 import numpy as np
-from glumpy import app, gloo, gl, data
-from glumpy.transforms import Trackball, Position
+from vispy import app, gloo
+from vispy.gloo import gl
+from vispy.io import read_png
+from vispy.util.transforms import perspective, translate, rotate
 
-vertex = """
-    attribute vec3 position;
-    attribute vec3 texcoord;
-    varying vec3 v_texcoord;
-    void main()
-    {
-        gl_Position = <transform(position)> * vec4(-1,-1,1,1);
-        v_texcoord = texcoord;
-    }
+# DEPRECATED
+# from glumpy import app, gloo, gl, data
+# from glumpy.transforms import Trackball, Position
+
+vertex_shader = """
+attribute vec3 a_position;
+attribute vec3 a_texcoord;
+varying vec3 v_texcoord;
+uniform   mat4 u_model;
+uniform   mat4 u_view;
+uniform   mat4 u_projection;
+
+void main()
+{
+    v_texcoord = a_texcoord;
+    gl_Position = u_projection * u_view * u_model * vec4(a_position, 1.0);
+}
 """
 
-fragment = """
-    uniform samplerCube texture;
-    varying vec3 v_texcoord;
-    void main()
-    {
-        gl_FragColor = textureCube(texture, v_texcoord);
-    }
+fragment_shader = """
+//uniform sampler3D u_tex1;
+//uniform sampler3D u_tex2;
+//uniform sampler3D u_tex3;
+//uniform sampler3D u_tex4;
+//uniform sampler3D u_tex5;
+//uniform sampler3D u_tex6;
+varying vec3 v_texcoord;
+
+void main()
+{
+    //vec3 clr1 = texture3D(u_tex1, v_texcoord).rgb;
+    //vec3 clr2 = texture3D(u_tex2, v_texcoord).rgb;
+    //vec3 clr3 = texture3D(u_tex3, v_texcoord).rgb;
+    //vec3 clr4 = texture3D(u_tex4, v_texcoord).rgb;
+    //vec3 clr5 = texture3D(u_tex5, v_texcoord).rgb;
+    //vec3 clr6 = texture3D(u_tex6, v_texcoord).rgb;
+    //gl_FragColor.rgb = clr1 + clr2 + clr3 + clr4 + clr5 + clr6;
+    //gl_FragColor.a = 1.0;
+    gl_FragColor = vec4(v_texcoord, 1.0);
+}
 """
-
-
-window = app.Window(width=1024, height=1024)
-
-
-@window.event
-def on_draw(dt):
-    window.clear()
-    program.draw(gl.GL_TRIANGLES, indices)
-
-
-@window.event
-def on_init():
-    gl.glEnable(gl.GL_DEPTH_TEST)
-
 
 vertices = np.array([[+1, +1, +1], [-1, +1, +1], [-1, -1, +1], [+1, -1, +1],
-                     [+1, -1, -1], [+1, +1, -1], [-1, +1, -1], [-1, -1, -1]])
+                     [+1, -1, -1], [+1, +1, -1], [-1, +1, -1], [-1, -1, -1]]).astype(np.float32)
 texcoords = np.array([[+1, +1, +1], [-1, +1, +1], [-1, -1, +1], [+1, -1, +1],
-                     [+1, -1, -1], [+1, +1, -1], [-1, +1, -1], [-1, -1, -1]])
+                     [+1, -1, -1], [+1, +1, -1], [-1, +1, -1], [-1, -1, -1]]).astype(np.float32)
 faces = np.array([vertices[i] for i in [0, 1, 2, 3, 0, 3, 4, 5, 0, 5, 6, 1,
                                         6, 7, 2, 1, 7, 4, 3, 2, 4, 7, 6, 5]])
 indices = np.resize(np.array([0, 1, 2, 0, 2, 3], dtype=np.uint32), 36)
 indices += np.repeat(4 * np.arange(6, dtype=np.uint32), 6)
-indices = indices.view(gloo.IndexBuffer)
-texture = np.zeros((6, 1024, 1024, 3), dtype=np.float32).view(gloo.TextureCube)
-texture.interpolation = gl.GL_LINEAR
-program = gloo.Program(vertex, fragment, count=24)
-program['position'] = faces*10
-program['texcoord'] = faces
-program['texture'] = texture
-program['transform'] = Trackball(Position(), distance=0)
+# indices = indices.view(gloo.IndexBuffer)
 
-texture[2] = data.get("sky-left.png")/255.
-texture[3] = data.get("sky-right.png")/255.
-texture[0] = data.get("sky-front.png")/255.
-texture[1] = data.get("sky-back.png")/255.
-texture[4] = data.get("sky-up.png")/255.
-texture[5] = data.get("sky-down.png")/255.
-window.attach(program["transform"])
-app.run()
+texture = np.zeros((6, 1024, 1024, 3), dtype=np.float32)
+texture[2] = read_png("sky-left.png")/255.
+texture[3] = read_png("sky-right.png")/255.
+texture[0] = read_png("sky-front.png")/255.
+texture[1] = read_png("sky-back.png")/255.
+texture[4] = read_png("sky-up.png")/255.
+texture[5] = read_png("sky-down.png")/255.
+# window.attach(program["transform"])
+print("read is finished.")
+
+
+class Canvas(app.Canvas):
+
+    def __init__(self):
+        app.Canvas.__init__(self, size=(1024, 1024), keys='interactive')
+        self.program = gloo.Program(vertex_shader, fragment_shader, count=24)
+        self.program['a_position'] = faces*10
+        self.program['a_texcoord'] = faces
+        #self.program['u_tex1'] = gloo.Texture3D(texture[2], interpolation='linear')
+        #self.program['u_tex2'] = gloo.Texture3D(texture[3], interpolation='linear')
+        #self.program['u_tex3'] = gloo.Texture3D(texture[0], interpolation='linear')
+        #self.program['u_tex4'] = gloo.Texture3D(texture[1], interpolation='linear')
+        #self.program['u_tex5'] = gloo.Texture3D(texture[4], interpolation='linear')
+        #self.program['u_tex6'] = gloo.Texture3D(texture[5], interpolation='linear')
+        self.program.bind(gloo.VertexBuffer(faces))
+        #self.program['texture'] = texture
+        #self.program['transform'] = Trackball(Position(), distance=0)
+
+        self.default_view = np.array([[ 8.00000012e-01, 2.00000003e-01, -4.79999989e-01, 0.00000000e+00],
+            [-5.00000000e-01, 3.00000012e-01, -7.79999971e-01, 0.00000000e+00],
+            [-9.99999978e-03, 8.99999976e-01, -3.00000012e-01, 0.00000000e+00],
+            [-3.00000000e-01, -2.15000000e+01, -4.76000001e+01, 1.00000000e+00]],dtype=np.float32)
+        self.view = self.default_view
+        self.model = np.eye(4, dtype=np.float32)
+        self.projection = np.eye(4, dtype=np.float32)
+
+        self.translate = [0, 0, 0]
+        self.rotate = [0, 0, 0]
+
+        self.program['u_model'] = self.model
+        self.program['u_view'] = self.view
+
+        gl.glEnable(gl.GL_DEPTH_TEST)
+        gloo.set_clear_color('black')
+        self.show()
+        print("canvas.init()")
+
+    def on_draw(self, event):
+        gloo.clear(color=True, depth=True)
+        self.program.draw(gl.GL_TRIANGLES, gloo.IndexBuffer(indices))
+
+    def on_key_press(self, event):
+        """Controls -
+        q(Q) - move left
+        d(D) - move right
+        z(Z) - move up
+        s(S) - move down
+        j/J - rotate about x-axis cw/anti-cw
+        k/K - rotate about y-axis cw/anti-cw
+        l/L - rotate about z-axis cw/anti-cw
+        space - reset view
+        p(P) - print current view
+        """
+        self.translate = [0, 0, 0]
+        self.rotate = [0, 0, 0]
+
+        if(event.text == 'p' or event.text == 'P'):
+            print(self.view)
+        elif(event.text == 'd' or event.text == 'D'):
+            self.translate[0] = 0.3
+        elif(event.text == 'q' or event.text == 'Q'):
+            self.translate[0] = -0.3
+        elif(event.text == 'z' or event.text == 'Z'):
+            self.translate[1] = 0.3
+        elif(event.text == 's' or event.text == 'S'):
+            self.translate[1] = -0.3
+        elif(event.text == 'j'):
+            self.rotate = [1, 0, 0]
+        elif(event.text == 'J'):
+            self.rotate = [-1, 0, 0]
+        elif(event.text == 'k'):
+            self.rotate = [0, 1, 0]
+        elif(event.text == 'K'):
+            self.rotate = [0, -1, 0]
+        elif(event.text == 'l'):
+            self.rotate = [0, 0, 1]
+        elif(event.text == 'L'):
+            self.rotate = [0, 0, -1]
+        elif(event.text == ' '):
+            self.view = self.default_view
+
+        self.view = self.view.dot(
+            translate(-np.array(self.translate)).dot(
+                rotate(self.rotate[0], (1, 0, 0)).dot(
+                    rotate(self.rotate[1], (0, 1, 0)).dot(
+                        rotate(self.rotate[2], (0, 0, 1))))))
+        self.program['u_view'] = self.view
+        self.update()
+
+    def on_resize(self, event):
+        self.activate_zoom()
+
+    def activate_zoom(self):
+        gloo.set_viewport(0, 0, *self.physical_size)
+        self.projection = perspective(60.0, self.size[0] /
+                                      float(self.size[1]), 1.0, 100.0)
+        self.program['u_projection'] = self.projection
+
+if __name__ == '__main__':
+    c = Canvas()
+    app.run()
