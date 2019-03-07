0ce8b7f9506c1a4b72ac0c71aeda72588ecbeee2
GuillaumeFavelier <guillaume.favelier@gmail.com>
2019-03-07 16:28:39 +0100

    Avoid multiple values for default keyword arguments

--- vispy/visuals/xyz_axis.py
+++ vispy/visuals/xyz_axis.py
@@ -10,17 +10,23 @@ class XYZAxisVisual(LineVisual):
     x=red, y=green, z=blue.
     """
     def __init__(self, **kwargs):
-        verts = np.array([[0, 0, 0],
-                          [1, 0, 0],
-                          [0, 0, 0],
-                          [0, 1, 0],
-                          [0, 0, 0],
-                          [0, 0, 1]])
-        color = np.array([[1, 0, 0, 1],
-                          [1, 0, 0, 1],
-                          [0, 1, 0, 1],
-                          [0, 1, 0, 1],
-                          [0, 0, 1, 1],
-                          [0, 0, 1, 1]])
-        LineVisual.__init__(self, pos=verts, color=color, connect='segments',
-                            method='gl', **kwargs)
+        if 'pos' not in kwargs:
+            kwargs['pos'] = np.array([[0, 0, 0],
+                                      [1, 0, 0],
+                                      [0, 0, 0],
+                                      [0, 1, 0],
+                                      [0, 0, 0],
+                                      [0, 0, 1]])
+        if 'color' not in kwargs:
+            kwargs['color'] = np.array([[1, 0, 0, 1],
+                                        [1, 0, 0, 1],
+                                        [0, 1, 0, 1],
+                                        [0, 1, 0, 1],
+                                        [0, 0, 1, 1],
+                                        [0, 0, 1, 1]])
+        if 'connect' not in kwargs:
+            kwargs['connect'] = 'segments'
+        if 'method' not in kwargs:
+            kwargs['method'] = 'gl'
+
+        LineVisual.__init__(self, **kwargs)
