603476604e16ba322e094000961460c5080b60b6
David Hoese <david.hoese@ssec.wisc.edu>
2019-03-08 15:23:44 -0600

    Fix shared context test xfail [skip size]

--- vispy/app/tests/test_context.py
+++ vispy/app/tests/test_context.py
@@ -70,15 +70,16 @@ def test_context_sharing():
         # Check while c1 is active
         check()
 
+        # pyqt5 does not currently support context sharing
+        if 'pyqt5' in c1.app.backend_name.lower():
+            pytest.xfail("Context sharing is not supported in PyQt5 at this time.")
+
         # Check while c2 is active (with different context)
         with Canvas() as c2:
             # pyglet always shares
             if 'pyglet' not in c2.app.backend_name.lower():
                 assert_raises(Exception, check)
 
-        # pyqt5 does not currently support context sharing (
-        if 'pyqt5' in c2.app.backend_name.lower():
-            pytest.xfail("Context sharing is not supported in PyQt5 at this time.")
         # Check while c2 is active (with *same* context)
         with Canvas(shared=c1.context) as c2:
             assert c1.context.shared is c2.context.shared  # same object
