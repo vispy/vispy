53ee0c6b7e2e586fa5c890ebe2cb7e461a766f29
GuillaumeFavelier <guillaume.favelier@gmail.com>
2019-03-11 11:21:44 +0100

    Use kwargs.setdefault() function

--- vispy/visuals/xyz_axis.py
+++ vispy/visuals/xyz_axis.py
@@ -10,23 +10,24 @@ class XYZAxisVisual(LineVisual):
     x=red, y=green, z=blue.
     """
     def __init__(self, **kwargs):
-        if 'pos' not in kwargs:
-            kwargs['pos'] = np.array([[0, 0, 0],
-                                      [1, 0, 0],
-                                      [0, 0, 0],
-                                      [0, 1, 0],
-                                      [0, 0, 0],
-                                      [0, 0, 1]])
-        if 'color' not in kwargs:
-            kwargs['color'] = np.array([[1, 0, 0, 1],
-                                        [1, 0, 0, 1],
-                                        [0, 1, 0, 1],
-                                        [0, 1, 0, 1],
-                                        [0, 0, 1, 1],
-                                        [0, 0, 1, 1]])
-        if 'connect' not in kwargs:
-            kwargs['connect'] = 'segments'
-        if 'method' not in kwargs:
-            kwargs['method'] = 'gl'
+        pos = np.array([[0, 0, 0],
+                        [1, 0, 0],
+                        [0, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, 0],
+                        [0, 0, 1]])
+        color = np.array([[1, 0, 0, 1],
+                          [1, 0, 0, 1],
+                          [0, 1, 0, 1],
+                          [0, 1, 0, 1],
+                          [0, 0, 1, 1],
+                          [0, 0, 1, 1]])
+        connect = 'segments'
+        method = 'gl'
+
+        kwargs.setdefault('pos', pos)
+        kwargs.setdefault('color', color)
+        kwargs.setdefault('connect', connect)
+        kwargs.setdefault('method', method)
 
         LineVisual.__init__(self, **kwargs)
