68c381ad1a05201efe41ada49162bcd44d3d20e2
David Hoese <david.hoese@ssec.wisc.edu>
2019-04-02 15:05:44 -0500

    Remove unnecessary __webpack magic in index.js

--- js/lib/index.js
+++ js/lib/index.js
@@ -5,7 +5,6 @@
 // Some static assets may be required by the custom widget javascript. The base
 // url for the notebook is not known at build time and is therefore computed
 // dynamically.
-__webpack_public_path__ = document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/vispy/';
 
 // Export widget models and views, and the npm package version number.
 // module.exports.VispyView = require('./webgl-backend.js').VispyView;
