7bca86bc04a5133c943f6bf815a73bca13cb374e
Eric Larson <larson.eric.d@gmail.com>
2019-07-10 21:21:28 -0400

    WIP: cibuildwheel config

---
+++ vispy/conftest.py
@@ -0,0 +1,11 @@
+def pytest_configure(config):
+    config.addinivalue_line(
+        'markers',
+        'vispy_app_test: Tests that require a valid GUI application.')
+    warning_lines = """
+    ignore:.*imp module.*:
+    """  # noqa: E501
+    for warning_line in warning_lines.split('\n'):
+        warning_line = warning_line.strip()
+        if warning_line and not warning_line.startswith('#'):
+            config.addinivalue_line('filterwarnings', warning_line)
