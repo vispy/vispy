23d6b4f218b79bbd1d9d7618f86bcafd77cc1004
Eric Larson <larson.eric.d@gmail.com>
2019-07-11 14:22:46 -0400

    FIX: One more

--- setup.py
+++ setup.py
@@ -297,10 +297,9 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: IPython'
     ],
 )
