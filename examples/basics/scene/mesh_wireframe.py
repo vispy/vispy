a09e182d99eb2a1f9c5fc435247cc1c2d5a0aff0
Alexandre Saint <830589+asnt@users.noreply.github.com>
2019-08-04 11:56:03 +0200

    Mix shading and wireframe in the example

--- examples/basics/scene/mesh_wireframe.py
+++ examples/basics/scene/mesh_wireframe.py
@@ -3,12 +3,13 @@ import argparse
 from vispy import app, scene
 from vispy.io import read_mesh, load_data_file
 from vispy.scene.visuals import Mesh
-from vispy.visuals.filters import WireframeFilter
+from vispy.visuals.filters import ShadingFilter, WireframeFilter
 
 
 parser = argparse.ArgumentParser()
 default_mesh = load_data_file('orig/triceratops.obj.gz')
 parser.add_argument('--mesh', default=default_mesh)
+parser.add_argument('--shininess', default=None)
 args = parser.parse_args()
 
 vertices, faces, normals, texcoords = read_mesh(args.mesh)
@@ -23,8 +24,45 @@ mesh = Mesh(vertices, faces, color=(.5, .7, .5, 1))
 mesh.set_gl_state('translucent', depth_test=True, cull_face=True)
 view.add(mesh)
 
-wireframe_filter = WireframeFilter()
-mesh.attach(wireframe_filter)
+w = WireframeFilter()
+mesh.attach(w)
+
+shading_filter = ShadingFilter()
+if args.shininess is not None:
+    shading_filter.shininess = args.shininess
+mesh.attach(shading_filter)
+
+shading_filter.light_dir = (0, -1, 0)
+initial_light_dir = view.camera.transform.imap(shading_filter.light_dir)[:3]
+mesh.update()
+
+
+# Make the mesh light follow the direction of the camera.
+@canvas.events.mouse_move.connect
+def on_mouse_move(event):
+    transform = view.camera.transform
+    shading_filter.light_dir = transform.map(initial_light_dir)[:3]
+    mesh.update()
+
+
+shadings = [None, 'flat', 'smooth']
+shading_index = shadings.index(shading_filter.shading)
+
+
+@canvas.events.key_press.connect
+def on_key_press(event):
+    global shading_index
+    if event.key == 's':
+        shading_index = (shading_index + 1) % len(shadings)
+        shading = shadings[shading_index]
+        shading_filter.shading = shading
+        shading_filter.enabled = shading is not None
+        mesh.update()
+    elif event.key == 'w':
+        if w._attached:
+            mesh.detach(w)
+        else:
+            mesh.attach(w)
 
 
 canvas.show()
