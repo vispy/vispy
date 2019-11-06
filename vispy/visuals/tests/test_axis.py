8c791c53c58e16a9a69ecbd588b6d8972c6fe69f
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-11-06 14:31:47 +0000

    Added regression test for dmin == dmax

---
+++ vispy/visuals/tests/test_axis.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for AxisVisual
+"""
+
+from vispy import scene
+from vispy.scene import visuals
+from vispy.testing import (requires_application, TestingCanvas,
+                           run_tests_if_main)
+
+
+@requires_application()
+def test_axis():
+    with TestingCanvas() as c:
+        axis = visuals.Axis(pos=[[-1.0, 0], [1.0, 0]], parent=c.scene)
+        c.draw_visual(axis)
+
+
+@requires_application()
+def test_axis_zero_domain():
+    # Regression test for a bug that caused an overflow error when the domain
+    # min was same as max
+    with TestingCanvas() as c:
+        axis = visuals.Axis(pos=[[-1.0, 0], [1.0, 0]], domain=(0.5, 0.5), parent=c.scene)
+        c.draw_visual(axis)
+
+
+run_tests_if_main()
