eccb2b3931e6b6e0a7b413ab6d568aa4c8a8f58d
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-11-07 10:35:04 +0000

    Added full header to test_axis.py

--- vispy/visuals/tests/test_axis.py
+++ vispy/visuals/tests/test_axis.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
 
 """
 Tests for AxisVisual
