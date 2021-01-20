92dbef7d26440b86eded8690e73813fdf7c0fca3
R.K.Garcia <r.keoni@gmail.com>
2021-01-20 11:17:53 -0600

    Dandruff removal with trusty wire brush

--- vispy/ext/cocoapy.py
+++ vispy/ext/cocoapy.py
@@ -17,28 +17,25 @@ if sys.version_info[0] >= 3:
 else:
     string_types = basestring,  # noqa
 
+
 # handle dlopen cache changes in macOS 11 (Big Sur)
 # ref https://stackoverflow.com/questions/63475461/unable-to-import-opengl-gl-in-python-on-macos
 try:
-    import OpenGL as ogl
-    try:
-        import OpenGL.GL   # this fails in <=2020 versions of Python on OS X 11.x
-    except ImportError:
-        # print('Drat, patching for Big Sur')
-        from ctypes import util
-        orig_util_find_library = util.find_library
-        def new_util_find_library( name ):
-            res = orig_util_find_library( name )
-            if res: 
-                return res
-            lut = {
-                'objc': 'libobjc.dylib',
-                'quartz': 'Quartz.framework/Quartz'
-            }
-            return lut.get(name, name+'.framework/'+name)
-        util.find_library = new_util_find_library
+    import OpenGL.GL   # noqa
 except ImportError:
-    pass
+    # print('Drat, patching for Big Sur')
+    orig_util_find_library = util.find_library
+    
+    def new_util_find_library(name):
+        res = orig_util_find_library(name)
+        if res:
+            return res
+        lut = {
+            'objc': 'libobjc.dylib',
+            'quartz': 'Quartz.framework/Quartz'
+        }
+        return lut.get(name, name+'.framework/'+name)
+    util.find_library = new_util_find_library
 
 
 # Based on Pyglet code
