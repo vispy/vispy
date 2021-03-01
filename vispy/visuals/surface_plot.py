850be96760ee4101a84a6f95e910c78c06fad34f
David Hoese <david.hoese@ssec.wisc.edu>
2021-03-01 10:11:30 -0600

    Optimize SurfacePlotVisual when only color is updated

--- vispy/visuals/surface_plot.py
+++ vispy/visuals/surface_plot.py
@@ -54,6 +54,70 @@ class SurfacePlotVisual(MeshVisual):
         MeshVisual.__init__(self, **kwargs)
         self.set_data(x, y, z, colors)
 
+    def _update_x_data(self, x):
+        if x is not None:
+            if self._x is None or len(x) != len(self._x):
+                self.__vertices = None
+            self._x = x
+
+    def _update_y_data(self, y):
+        if y is not None:
+            if self._y is None or len(y) != len(self._y):
+                self.__vertices = None
+            self._y = y
+
+    def _update_z_data(self, z):
+        if z is not None:
+            if self._x is not None and z.shape[0] != len(self._x):
+                raise TypeError('Z values must have shape (len(x), len(y))')
+            if self._y is not None and z.shape[1] != len(self._y):
+                raise TypeError('Z values must have shape (len(x), len(y))')
+            self._z = z
+            if (self.__vertices is not None and
+                    self._z.shape != self.__vertices.shape[:2]):
+                self.__vertices = None
+
+    def _update_mesh_vertices(self, x_is_new, y_is_new, z_is_new):
+        new_vertices = False
+        update_vertices = False
+        update_faces = False
+
+        # Generate vertex and face array
+        if self.__vertices is None:
+            self.__vertices = np.empty((self._z.shape[0], self._z.shape[1], 3),
+                                       dtype=np.float32)
+            self.generate_faces()
+            new_vertices = True
+            update_faces = True
+
+        # Copy x, y, z data into vertex array
+        if new_vertices or x_is_new:
+            if not x_is_new and self._x is None:
+                x = np.arange(self._z.shape[0])
+            else:
+                x = self._x
+            if x.ndim == 1:
+                x = x.reshape(len(x), 1)
+            # Copy the 2D data into the appropriate slice
+            self.__vertices[:, :, 0] = x
+            update_vertices = True
+
+        if new_vertices or y_is_new:
+            if not y_is_new and self._y is None:
+                y = np.arange(self._z.shape[1])
+            else:
+                y = self._y
+            if y.ndim == 1:
+                y = y.reshape(1, len(y))
+            # Copy the 2D data into the appropriate slice
+            self.__vertices[:, :, 1] = y
+            update_vertices = True
+
+        if new_vertices or z_is_new:
+            self.__vertices[..., 2] = self._z
+            update_vertices = True
+        return update_faces, update_vertices
+
     def set_data(self, x=None, y=None, z=None, colors=None):
         """Update the data in this surface plot.
 
@@ -74,81 +138,30 @@ class SurfacePlotVisual(MeshVisual):
         colors : ndarray
             (width, height, 4) array of vertex colors.
         """
-        if x is not None:
-            if self._x is None or len(x) != len(self._x):
-                self.__vertices = None
-            self._x = x
-
-        if y is not None:
-            if self._y is None or len(y) != len(self._y):
-                self.__vertices = None
-            self._y = y
-
-        if z is not None:
-            if self._x is not None and z.shape[0] != len(self._x):
-                raise TypeError('Z values must have shape (len(x), len(y))')
-            if self._y is not None and z.shape[1] != len(self._y):
-                raise TypeError('Z values must have shape (len(x), len(y))')
-            self._z = z
-            if (self.__vertices is not None and
-                    self._z.shape != self.__vertices.shape[:2]):
-                self.__vertices = None
+        self._update_x_data(x)
+        self._update_y_data(y)
+        self._update_z_data(z)
 
         if self._z is None:
+            # no mesh data to plot so no need to update
             return
 
-        update_mesh = False
-        new_vertices = False
+        update_faces, update_vertices = self._update_mesh_vertices(
+            x is not None,
+            y is not None,
+            z is not None
+        )
 
-        # Generate vertex and face array
-        if self.__vertices is None:
-            new_vertices = True
-            self.__vertices = np.empty((self._z.shape[0], self._z.shape[1], 3),
-                                       dtype=np.float32)
-            self.generate_faces()
-            self.__meshdata.set_faces(self.__faces)
-            update_mesh = True
-
-        # Copy x, y, z data into vertex array
-        if new_vertices or x is not None:
-            if x is None:
-                if self._x is None:
-                    x = np.arange(self._z.shape[0])
-                else:
-                    x = self._x
-            if x.ndim == 1:
-                x = x.reshape(len(x), 1)
-            # Copy the 2D data into the appropriate slice
-            self.__vertices[:, :, 0] = x
-            update_mesh = True
-
-        if new_vertices or y is not None:
-            if y is None:
-                if self._y is None:
-                    y = np.arange(self._z.shape[1])
-                else:
-                    y = self._y
-
-            if y.ndim == 1:
-                y = y.reshape(1, len(y))
-
-            # Copy the 2D data into the appropriate slice
-            self.__vertices[:, :, 1] = y
-            update_mesh = True
-
-        if new_vertices or z is not None:
-            self.__vertices[..., 2] = self._z
-            update_mesh = True
-
-        if colors is not None:
+        update_colors = colors is not None
+        if update_colors:
             self.__meshdata.set_vertex_colors(colors)
-            update_mesh = True
-
-        # Update MeshData
-        if update_mesh:
+        if update_faces:
+            self.__meshdata.set_faces(self.__faces)
+        if update_vertices:
             self.__meshdata.set_vertices(
                 self.__vertices.reshape(self.__vertices.shape[0] *
                                         self.__vertices.shape[1], 3))
+        if update_faces or update_vertices or update_colors:
             MeshVisual.set_data(self, meshdata=self.__meshdata)
 
     def generate_faces(self):
