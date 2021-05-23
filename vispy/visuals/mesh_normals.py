67a0de60552e604c2f592a9c625865db3e0fbc1d
Alexandre Saint <830589+asnt@users.noreply.github.com>
2021-05-23 23:18:14 +0200

    Format

--- vispy/visuals/mesh_normals.py
+++ vispy/visuals/mesh_normals.py
@@ -3,7 +3,6 @@
 # Copyright (c) Vispy Development Team. All Rights Reserved.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
-
 """A visual for displaying mesh normals as lines."""
 
 import numpy as np
