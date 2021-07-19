75dd668a6435284540655bde3f2ec33cadf1a174
David Hoese <david.hoese@ssec.wisc.edu>
2021-07-19 11:43:23 -0500

    Add sphinx-gallery scraper and scraper hook

--- vispy/__init__.py
+++ vispy/__init__.py
@@ -26,6 +26,7 @@ except ImportError:
 
 from .util import config, set_log_level, keys, sys_info  # noqa
 from .util.wrappers import use, test  # noqa
+from .util.gallery_scraper import _get_sg_image_scraper  # noqa
 # load the two functions that IPython uses to instantiate an extension
 # that way, the user only needs to run %load_ext vispy.ipython rather that
 # %load_ext vispy.ipython.ipython
