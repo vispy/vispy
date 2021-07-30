8101fe6f4356c31d81e9d1fdb982be3a4c51d119
Almar Klein <almar@almarklein.org>
2021-07-30 14:33:05 +0200

    fix case where cmap can be None

--- vispy/visuals/line/line.py
+++ vispy/visuals/line/line.py
@@ -338,7 +338,7 @@ class _GLLineVisual(Visual):
                     self._color_vbo.set_data(color)
                     self._program.vert['color'] = self._color_vbo
 
-            self.shared_program['texture2D_LUT'] = cmap.texture_lut()
+            self.shared_program['texture2D_LUT'] = cmap and cmap.texture_lut()
 
         if "webgl" not in default_app_module.default_app.backend_name:
             self.update_gl_state(line_smooth=bool(self._parent._antialias))
