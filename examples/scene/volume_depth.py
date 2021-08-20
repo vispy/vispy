eef5ea3b77a120db303b344a783712def6a2e2a5
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2021-08-20 08:49:11 +0200

    update depth example with mesh

--- examples/scene/volume_depth.py
+++ examples/scene/volume_depth.py
@@ -26,70 +26,47 @@ canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
 view = canvas.central_widget.add_view()
 
 # Create the volume visual
-im_data = np.zeros((50, 50, 50))
-im_data[30:40, 25:35, 25:35] = 1
-green_cmap = get_colormap('greens')
-volume = scene.visuals.Volume(
-    im_data,
-    cmap=green_cmap,
-    parent=view.scene,
-    threshold=0.5,
-    method='iso'
-)
-
+im_data = np.zeros((30, 30, 30))
+im_data[5:25, 5:25, 5:25] = 1
 blue_cmap = get_colormap('blues')
-volume_2 = scene.visuals.Volume(
+volume_1 = scene.visuals.Volume(
     im_data,
     cmap=blue_cmap,
     parent=view.scene,
     threshold=0.5,
     method='iso'
 )
-volume_2.transform = scene.STTransform(translate=(30, 0, 0))
+volume_1.transform = scene.STTransform(translate=(0, -10, -10))
 
-markers_data = np.array([
-    [20, 30, 35],
-    [45, 30, 30],
-    [70, 30, 35]
-])
-markers = scene.visuals.Markers(pos=markers_data, size=20, parent=view.scene)
+vertices = np.array(
+    [
+        [0, 0, 0],
+        [0, 10, 0],
+        [0, 10, 10],
+        [0, 0, 10],
+        [50, 5, 5]
+    ]
+)
+faces = np.array(
+    [
+        [0, 1, 2],
+        [0, 2, 3],
+        [0, 1, 4],
+        [1, 2, 4],
+        [2, 3, 4],
+        [3, 0, 4]
+    ]
+)
+mesh = scene.visuals.Mesh(vertices, faces, color=(.5, .7, .5, 1), parent=view.scene)
 
 # Create and set the camera
-fov = 60.
-cam = scene.cameras.TurntableCamera(
+fov = 60
+cam = scene.cameras.ArcballCamera(
     parent=view.scene,
     fov=fov,
-    name='Turntable'
+    name='Arcball'
 )
 view.camera = cam
-
-# Create an XYZAxis visual
-axis = scene.visuals.XYZAxis(parent=view)
-s = STTransform(translate=(50, 50), scale=(50, 50, 50, 1))
-affine = s.as_matrix()
-axis.transform = affine
-
-
-def update_axis_visual():
-    """Sync XYZAxis visual with camera angles"""
-    axis.transform.reset()
-
-    axis.transform.rotate(cam.roll, (0, 0, 1))
-    axis.transform.rotate(cam.elevation, (1, 0, 0))
-    axis.transform.rotate(cam.azimuth, (0, 1, 0))
-    axis.transform.scale((50, 50, 0.001))
-    axis.transform.translate((50., 50.))
-
-    axis.update()
-
-
-update_axis_visual()
-
-
-@canvas.events.mouse_move.connect
-def on_mouse_move(event):
-    if event.button == 1 and event.is_dragging:
-        update_axis_visual()
 
 
 if __name__ == '__main__':
