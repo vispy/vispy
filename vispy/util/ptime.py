1c85d407eb617305c3689bdd0b0dfc31ad7e3186
Llewyllen <61664783+Llewyllen@users.noreply.github.com>
2021-08-24 18:59:16 +0200

    Fix python version check for python > 3.9 (#2185)
    * fix python version check for python > 3.9
    
    * return precision time on all platforms
    
    also remove support for python < 3.3
    
    * Update ptime.py

--- vispy/util/ptime.py
+++ vispy/util/ptime.py
@@ -4,38 +4,13 @@
 
 """
 ptime.py -  Precision time function made os-independent
-(should have been taken care of by python)
 """
 
-from __future__ import division
-
-import sys
 import time as systime
-START_TIME = None
-time = None
+# get a reference starting time - initial performance counter
+START_TIME = systime.time() - systime.perf_counter()
 
 
-def winTime():
-    """Return the current time in seconds with high precision
-
-    (Windows version, use Manager.time() to stay platform independent.)
-    """
-    return systime.clock() + START_TIME
-    # return systime.time()
-
-
-def unixTime():
-    """Return the current time in seconds with high precision
-
-    (Unix version, use Manager.time() to stay platform independent.)
-    """
-    return systime.time()
-
-
-if sys.platform.startswith('win') and sys.version < '3.3':
-    cstart = systime.clock()  # Required to start the clock in windows
-    START_TIME = systime.time() - cstart
-
-    time = winTime
-else:
-    time = unixTime
+def time():
+    # return reference starting time + delta of performance counters
+    return START_TIME + systime.perf_counter()
