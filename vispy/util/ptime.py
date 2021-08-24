30716c71c8ed3860042d03776b8971c892e3fedc
Llewyllen <61664783+Llewyllen@users.noreply.github.com>
2021-08-24 18:11:18 +0200

    Update ptime.py

--- vispy/util/ptime.py
+++ vispy/util/ptime.py
@@ -10,6 +10,7 @@ import time as systime
 # get a reference starting time - initial performance counter
 START_TIME = systime.time() - systime.perf_counter()
 
+
 def time():
     # return reference starting time + delta of performance counters
     return START_TIME + systime.perf_counter()
