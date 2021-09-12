8c30765e8bd3716a3f0790f099550776159e00a2
Lorenzo Gaifas <brisvag@gmail.com>
2021-09-12 21:13:41 +0200

    clipping planes example updated

--- examples/scene/clipping_planes.py
+++ examples/scene/clipping_planes.py
@@ -5,8 +5,8 @@
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
 """
-Volume rendering with clipping planes
-=====================================
+Clipping planes with volume and markers
+=======================================
 Controls:
 - x/y/z/o - add new clipping plane with normal along x/y/z or [1,1,1] oblique axis
 - r - remove a clipping plane
@@ -16,9 +16,7 @@ import numpy as np
 
 from vispy import app, scene, io
 from vispy.visuals.transforms import STTransform
-
-# Read volume
-vol = np.load(io.load_data_file('volume/stent.npz'))['arr_0']
+from vispy.visuals.filters.clipping_planes import PlanesClipper
 
 # Prepare canvas
 canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
@@ -26,9 +24,20 @@ canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
 # Set up a viewbox to display the image with interactive pan/zoom
 view = canvas.central_widget.add_view()
 
-# Create the volume visual
-volume = scene.visuals.Volume(vol, parent=view.scene, threshold=0.225, clipping_planes_coord_system='visual')
-volume.transform = scene.STTransform(translate=(64, 64, 0))
+# Create the visuals
+vol = np.load(io.load_data_file('volume/stent.npz'))['arr_0']
+volume = scene.visuals.Volume(vol, parent=view.scene, threshold=0.225)
+
+np.random.seed(1)
+points = np.random.rand(100, 3) * (128, 128, 128)
+markers = scene.visuals.Markers(pos=points, parent=view.scene)
+# add a transform to markers, to show clipping is in scene coordinates
+markers.transform = scene.STTransform(translate=(0, 0, 128))
+
+# Create the clipping planes filter for the markers (Volume has its own clipping logic)
+clipper = PlanesClipper()
+# and attach it to the markers
+markers.attach(clipper)
 
 # Create and set the camera
 fov = 60.
@@ -60,8 +69,10 @@ def on_mouse_move(event):
         axis.transform.translate((50., 50.))
         axis.update()
 
+
 volume_center = (np.array(vol.shape) / 2)
 
+# clipping planes around the origin
 clip_modes = {
     'x': np.array([[volume_center, [0, 0, 1]]]),
     'y': np.array([[volume_center, [0, 1, 0]]]),
@@ -70,34 +81,32 @@ clip_modes = {
 }
 
 
-def add_clip(vol, mode):
+def add_clip(mode):
     if mode not in clip_modes:
         return
     new_plane = clip_modes[mode]
-    if vol.clipping_planes is None:
-        vol.clipping_planes = new_plane
+    if volume.clipping_planes is None:
+        clipping_planes = new_plane
     else:
-        vol.clipping_planes = np.concatenate([vol.clipping_planes, new_plane])
+        clipping_planes = np.concatenate([volume.clipping_planes, new_plane])
+    volume.clipping_planes = clipping_planes
+    clipper.clipping_planes = clipping_planes
 
 
-def remove_clip(vol):
-    if vol.clipping_planes is not None:
-        vol.clipping_planes = vol.clipping_planes[:-1]
+def remove_clip():
+    if volume.clipping_planes is not None:
+        volume.clipping_planes = volume.clipping_planes[:-1]
+        clipper.clipping_planes = clipper.clipping_planes[:-1]
 
 
 # Implement key presses
 @canvas.events.key_press.connect
 def on_key_press(event):
     if event.text in 'xyzo':
-        add_clip(volume, event.text)
+        add_clip(event.text)
     elif event.text == 'r':
-        remove_clip(volume)
+        remove_clip()
 
-
-# for testing performance
-# @canvas.connect
-# def on_draw(ev):
-# canvas.update()
 
 if __name__ == '__main__':
     print(__doc__)
