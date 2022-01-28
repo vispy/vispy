d6a6107d3eaa4dd6c0d3872f8cb45614951e591d
joelsner <joel_oelsner@fastmail.fm>
2022-01-28 01:08:42 +0100

    added bar graph plotting functionality

--- vispy/plot/tests/test_plot.py
+++ vispy/plot/tests/test_plot.py
@@ -7,7 +7,8 @@ from vispy.testing import (assert_raises, requires_application,
                            run_tests_if_main)
 from vispy.visuals.axis import AxisVisual
 from unittest import mock
-
+from vispy import scene, app
+import numpy as np
 
 @requires_application()
 def test_figure_creation():
@@ -41,6 +42,49 @@ def test_plot_widget_axes():
         expectation = [point[0] - buffer, point[1] + buffer]
         for call in domain_setter.call_args_list:
             assert [round(x, 2) for x in call[0][1]] == expectation
+
+@requires_application()
+def test_bar_creation():
+    """Test creating a figure"""
+
+    canvas = scene.SceneCanvas(keys='interactive', vsync=False)
+    canvas.size = 800, 600
+    canvas.show()
+
+    grid = canvas.central_widget.add_grid()
+    grid.padding = 10
+
+    # Create two ViewBoxes, place side-by-side
+    vb1 = grid.add_view(row=0, col=1, camera='panzoom')
+
+    x_axis1 = scene.AxisWidget(orientation='bottom')
+    x_axis1.stretch = (1, 0.1)
+    grid.add_widget(x_axis1, row=1, col=1)
+    x_axis1.link_view(vb1)
+    y_axis1 = scene.AxisWidget(orientation='left')
+    y_axis1.stretch = (0.1, 1)
+    grid.add_widget(y_axis1, row=0, col=0)
+    y_axis1.link_view(vb1)
+
+    length = 100
+
+    h = 10
+
+    bottom = np.random.randint(h, size=length)
+
+    height = bottom + np.random.randint(h, size=length)
+
+    index = np.arange(length, dtype=int)
+
+    bar1 = scene.Bar(index=index, bottom=bottom,
+                     height=height, width=0.8, color=(0.25, 0.8, 0.), orientation='v',
+                     parent=vb1.scene)
+
+    vb1.camera.set_range()
+
+    # canvas.measure_fps()
+    # app.run()
+
 
 
 run_tests_if_main()
---
+++ vispy/visuals/bar.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
+
+import time
+import numpy as np
+from datetime import date
+
+from .mesh import MeshVisual
+
+
+class BarVisual(MeshVisual):
+    """Visual that calculates and displays a bar graph
+
+    Parameters
+    ----------
+    index : array-like
+        Index of bar
+    bottom : int | float | array-like
+        Bottom of the bars
+    height : int | float | array-like
+        Height of the bars
+    color : instance of Color
+        Color of the histogram.
+    orientation : {'h', 'v'}
+        Orientation of the histogram.
+    """
+
+    def __init__(self, color='w', orientation='h', index=None, bottom=None, height=None, width=0.8):
+        X, Y = (0, 1)
+
+        # construct our vertices
+        rr = np.zeros((6 * len(index), 3), np.float32)
+        rr[:, X] = np.repeat(index, 6)
+        rr[0::6, X] += (1 - width) / 2
+        rr[1::6, X] += (1 - width) / 2
+        rr[2::6, X] += 1 - (1 - width) / 2
+        rr[3::6, X] += (1 - width) / 2
+        rr[4::6, X] += 1 - (1 - width) / 2
+        rr[5::6, X] += 1 - (1 - width) / 2
+        rr[:, Y] = np.repeat(bottom, 6)
+        rr[::2, Y] += np.repeat(height, 3)
+        index.astype(np.float32)
+
+        if not isinstance(orientation, str) or \
+                orientation not in ('h', 'v'):
+            raise ValueError('orientation must be "h" or "v", not %s'
+                             % (orientation,))
+
+        MeshVisual.__init__(self, rr, color=color)
+
+    def update_data(self, index=None, bottom=None, height=None, width=None):
+        X, Y = (0, 1)
+        rr = np.zeros((6 * len(index), 3), np.float32)
+        rr[:, X] = np.repeat(index, 6)
+        rr[0::6, X] += (1 - width) / 2
+        rr[1::6, X] += (1 - width) / 2
+        rr[2::6, X] += 1 - (1 - width) / 2
+        rr[3::6, X] += (1 - width) / 2
+        rr[4::6, X] += 1 - (1 - width) / 2
+        rr[5::6, X] += 1 - (1 - width) / 2
+        rr[:, Y] = np.repeat(bottom, 6)
+        rr[::2, Y] += np.repeat(height, 3)
+        index.astype(np.float32)
+        MeshVisual.set_data(self, vertices=rr)
