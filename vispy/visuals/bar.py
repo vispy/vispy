1f647d65c598986c121c319dfb24d371fee4c174
joelsner <joel_oelsner@fastmail.fm>
2022-01-31 02:26:36 +0100

    Numerous improvements to Bar Plotting

--- vispy/visuals/bar.py
+++ vispy/visuals/bar.py
@@ -14,54 +14,110 @@ class BarVisual(MeshVisual):
     Parameters
     ----------
     data : array-like
-        1st column is the index, 2nd column is the height, optional 3rd column is bottom
-    index : array-like
-        Index of bar
-    bottom : int | float | array-like
-        Bottom of the bars
-    height : int | float | array-like
+        1st column is the height, optional 2nd column is bottom
+    height : array-like
         Height of the bars
+    bottom : array-like
+        Bottom of the bars
     width : int | float
         Width of all bars
     color : instance of Color
         Color of the bar.
     """
 
-    def __init__(self, color='w', data=None, index=None, bottom=None, height=None, width=0.8):
-        if (index is None or height is None) and data is None:
+    def __init__(self, data=None, height=None, bottom=None, width=0.8, color='w'):
+        if data is None and height is None:
             raise ValueError("Either supply the data parameter or supply index AND height")
 
-        if data is not None:
-            if data.shape[1] == 2:
-                index = data[:, 0]
-                height = data[:, 1]
-            elif data.shape[1] == 3:
-                index = data[:, 0]
-                height = data[:, 1]
-                bottom = data[:, 2]
+        index = None
+
+        if bottom is not None and height is not None:
+            if isinstance(bottom, np.ndarray) and isinstance(height, np.ndarray):
+                if bottom.shape != height.shape:
+                    if len(height.shape) == 1:
+                        height = height.reshape(height.shape[0], 1)
+
+                    elif len(height.shape) == 2:
+                        if height.shape[1] != 1:
+                            raise ValueError('height should only have one column')
+
+                    else:
+                        raise ValueError('3d is not expected, just 2d')
+
+                    if len(bottom.shape) == 1:
+                        bottom = bottom.reshape(bottom.shape[0], 1)
+
+                    elif len(bottom.shape) == 2:
+                        if bottom.shape[1] != 1:
+                            raise ValueError('bottom should only have one column')
+
+                    else:
+                        raise ValueError('3d is not expected, just 2d')
+
+                    if bottom[0] != height[0]:
+                        raise ValueError('bottom and height have to have the same length')
             else:
-                raise ValueError("expected either 2 or 3 columns not " + data.shape[1] + "columns data array should "
-                                                                                         "be arranged as follows: 1st "
-                                                                                         "column is the index, "
-                                                                                         "2nd column is the height, "
-                                                                                         "optional 3rd column is "
-                                                                                         "bottom")
+                if len(bottom) != len(height):
+                    raise ValueError('bottom and height have to have the same length')
+                else:
+                    index = np.arange(len(bottom))
 
-        if len(index) != len(height):
-            raise ValueError("Index length is not equal to the length of height, which needs to be the same")
+        if isinstance(data, np.ndarray):
+            if len(data.shape) == 1:
+                height = data.reshape(data.shape[0], 1)
+                index = np.arange(height.shape[0])
+                bottom = np.zeros(height.shape)
 
-        if bottom is not None:
-            if len(index) != len(bottom):
-                raise ValueError("Index length is not equal to the length of height, which needs to be the same")
+            elif len(data.shape) == 2:
+                if data.shape[1] == 1:
+                    height = data
+                    index = np.arange(height.shape[0])
+                    bottom = np.zeros(height.shape)
 
-        if bottom is None:
-            bottom = np.zeros(len(index))
+                elif data.shape[1] == 2:
+                    tmp_height = data[:, 0]
+                    height = tmp_height.reshape(tmp_height.shape[0], 1)
+                    index = np.arange(height.shape[0])
+                    tmp_bottom = data[:, 1]
+                    bottom = tmp_bottom.reshape(tmp_bottom.shape[0], 1)
+
+                else:
+                    raise ValueError('data should have one column for height and an optionally a second for bottom of '
+                                     'bars')
+
+            else:
+                raise ValueError('3d is not expected, just 2d')
+
+        elif height is not None:
+            if isinstance(height, np.ndarray):
+                if len(height.shape) == 1:
+                    height = height.reshape(height.shape[0], 1)
+                    index = np.arange(height.shape[0])
+                    if bottom is None:
+                        bottom = np.zeros(height.shape)
+
+                elif len(height.shape) == 2:
+                    if height.shape[1] == 1:
+                        index = np.arange(height.shape[0])
+                        if bottom is None:
+                            bottom = np.zeros(height.shape)
+
+                    else:
+                        raise ValueError('height should only have one column')
+
+                else:
+                    raise ValueError('3d is not expected, just 2d')
+            else:
+                index = np.arange(len(height))
+        else:
+            ValueError("Unknown ValueError: height")
 
         # construct our vertices
 
         X, Y = (0, 1)
+
         rr = np.zeros((6 * len(index), 3), np.float32)
-        rr[:, X] = np.repeat(index, 6)
+        rr[:, X] = np.repeat(index - 0.5, 6)
         rr[0::6, X] += (1 - width) / 2
         rr[1::6, X] += (1 - width) / 2
         rr[2::6, X] += 1 - (1 - width) / 2
@@ -74,41 +130,98 @@ class BarVisual(MeshVisual):
 
         MeshVisual.__init__(self, rr, color=color)
 
-    def update_data(self, data=None, index=None, bottom=None, height=None, width=0.8):
-        if (index is None or height is None) and data is None:
+    def update_data(self, data=None, height=None, bottom=None, width=0.8, color='w'):
+        if data is None and height is None:
             raise ValueError("Either supply the data parameter or supply index AND height")
 
-        if data is not None:
-            if data.shape[1] == 2:
-                index = data[:, 0]
-                height = data[:, 1]
-            elif data.shape[1] == 3:
-                index = data[:, 0]
-                height = data[:, 1]
-                bottom = data[:, 2]
+        index = None
+
+        if bottom is not None and height is not None:
+            if isinstance(bottom, np.ndarray) and isinstance(height, np.ndarray):
+                if bottom.shape != height.shape:
+                    if len(height.shape) == 1:
+                        height = height.reshape(height.shape[0], 1)
+
+                    elif len(height.shape) == 2:
+                        if height.shape[1] != 1:
+                            raise ValueError('height should only have one column')
+
+                    else:
+                        raise ValueError('3d is not expected, just 2d')
+
+                    if len(bottom.shape) == 1:
+                        bottom = bottom.reshape(bottom.shape[0], 1)
+
+                    elif len(bottom.shape) == 2:
+                        if bottom.shape[1] != 1:
+                            raise ValueError('bottom should only have one column')
+
+                    else:
+                        raise ValueError('3d is not expected, just 2d')
+
+                    if bottom[0] != height[0]:
+                        raise ValueError('bottom and height have to have the same length')
             else:
-                raise ValueError("expected either 2 or 3 columns not " + data.shape[1] + "columns data array should "
-                                                                                         "be arranged as follows: 1st "
-                                                                                         "column is the index, "
-                                                                                         "2nd column is the height, "
-                                                                                         "optional 3rd column is "
-                                                                                         "bottom")
+                if len(bottom) != len(height):
+                    raise ValueError('bottom and height have to have the same length')
+                else:
+                    index = np.arange(len(bottom))
 
-        if len(index) != len(height):
-            raise ValueError("Index length is not equal to the length of height, which needs to be the same")
+        if isinstance(data, np.ndarray):
+            if len(data.shape) == 1:
+                height = data.reshape(data.shape[0], 1)
+                index = np.arange(height.shape[0])
+                bottom = np.zeros(height.shape)
 
-        if bottom is not None:
-            if len(index) != len(bottom):
-                raise ValueError("Index length is not equal to the length of height, which needs to be the same")
+            elif len(data.shape) == 2:
+                if data.shape[1] == 1:
+                    height = data
+                    index = np.arange(height.shape[0])
+                    bottom = np.zeros(height.shape)
 
-        if bottom is None:
-            bottom = np.zeros(len(index))
+                elif data.shape[1] == 2:
+                    tmp_height = data[:, 0]
+                    height = tmp_height.reshape(tmp_height.shape[0], 1)
+                    index = np.arange(height.shape[0])
+                    tmp_bottom = data[:, 1]
+                    bottom = tmp_bottom.reshape(tmp_bottom.shape[0], 1)
+
+                else:
+                    raise ValueError('data should have one column for height and an optionally a second for bottom of '
+                                     'bars')
+
+            else:
+                raise ValueError('3d is not expected, just 2d')
+
+        elif height is not None:
+            if isinstance(height, np.ndarray):
+                if len(height.shape) == 1:
+                    height = height.reshape(height.shape[0], 1)
+                    index = np.arange(height.shape[0])
+                    if bottom is None:
+                        bottom = np.zeros(height.shape)
+
+                elif len(height.shape) == 2:
+                    if height.shape[1] == 1:
+                        index = np.arange(height.shape[0])
+                        if bottom is None:
+                            bottom = np.zeros(height.shape)
+
+                    else:
+                        raise ValueError('height should only have one column')
+
+                else:
+                    raise ValueError('3d is not expected, just 2d')
+            else:
+                index = np.arange(len(height))
+        else:
+            ValueError("Unknown ValueError: height")
 
         # construct our vertices
 
         X, Y = (0, 1)
         rr = np.zeros((6 * len(index), 3), np.float32)
-        rr[:, X] = np.repeat(index, 6)
+        rr[:, X] = np.repeat(index - 0.5, 6)
         rr[0::6, X] += (1 - width) / 2
         rr[1::6, X] += (1 - width) / 2
         rr[2::6, X] += 1 - (1 - width) / 2
@@ -118,4 +231,4 @@ class BarVisual(MeshVisual):
         rr[:, Y] = np.repeat(bottom, 6)
         rr[::2, Y] += np.repeat(height, 3)
         index.astype(np.float32)
-        MeshVisual.set_data(self, vertices=rr)
+        MeshVisual.set_data(self, vertices=rr, color=color)
