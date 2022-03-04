2409accd12b6fbe11eb9acef4eb554e6155ec776
Lorenzo Gaifas <brisvag@gmail.com>
2022-03-04 16:44:31 +0100

    add cutoff for mip and minip

--- vispy/visuals/volume.py
+++ vispy/visuals/volume.py
@@ -86,6 +86,7 @@ uniform float gamma;
 uniform float u_threshold;
 uniform float u_attenuation;
 uniform float u_relative_step_size;
+uniform float u_mip_cutoff;
 
 //varyings
 varying vec3 v_position;
@@ -369,7 +370,7 @@ _RAYCASTING_SETUP_PLANE = """
 
 _MIP_SNIPPETS = dict(
     before_loop="""
-        float maxval = -99999.0; // The maximum encountered value
+        float maxval = u_mip_cutoff; // The maximum encountered value
         int maxi = -1;  // Where the maximum value was encountered
         """,
     in_loop="""
@@ -393,7 +394,7 @@ _MIP_SNIPPETS = dict(
 
 _ATTENUATED_MIP_SNIPPETS = dict(
     before_loop="""
-        float maxval = -99999.0; // The maximum encountered value
+        float maxval = u_mip_cutoff; // The maximum encountered value
         float sumval = 0.0; // The sum of the encountered values
         float scaled = 0.0; // The scaled value
         int maxi = 0;  // Where the maximum value was encountered
@@ -415,7 +416,7 @@ _ATTENUATED_MIP_SNIPPETS = dict(
 
 _MINIP_SNIPPETS = dict(
     before_loop="""
-        float minval = 99999.0; // The minimum encountered value
+        float minval = u_minip_cutoff; // The minimum encountered value
         int mini = -1;  // Where the minimum value was encountered
         """,
     in_loop="""
@@ -642,7 +643,8 @@ class VolumeVisual(Visual):
                  gamma=1.0, interpolation='linear', texture_format=None,
                  raycasting_mode='volume', plane_position=None,
                  plane_normal=None, plane_thickness=1.0, clipping_planes=None,
-                 clipping_planes_coord_system='scene'):
+                 clipping_planes_coord_system='scene', mip_cutoff=-99999,
+                 minip_cutoff=99999):
 
         tr = ['visual', 'scene', 'document', 'canvas', 'framebuffer', 'render']
         if clipping_planes_coord_system not in tr:
@@ -685,6 +687,8 @@ class VolumeVisual(Visual):
 
         # Set params
         self.raycasting_mode = raycasting_mode
+        self.mip_cutoff = mip_cutoff
+        self.minip_cutoff = minip_cutoff
         self.method = method
         self.relative_step_size = relative_step_size
         self.threshold = threshold if threshold is not None else vol.mean()
@@ -973,6 +977,8 @@ class VolumeVisual(Visual):
         self.shared_program.frag['sample'] = self._texture.glsl_sample
         self.shared_program.frag['cmap'] = Function(self._cmap.glsl_map)
         self.shared_program['texture2D_LUT'] = self.cmap.texture_lut()
+        self.shared_program['u_mip_cutoff'] = self._mip_cutoff
+        self.shared_program['u_minip_cutoff'] = self._minip_cutoff
         self.update()
 
     @property
@@ -1094,6 +1100,26 @@ class VolumeVisual(Visual):
             raise ValueError('plane_thickness should be at least 1.0')
         self._plane_thickness = value
         self.shared_program['u_plane_thickness'] = value
+        self.update()
+
+    @property
+    def mip_cutoff(self):
+        return self._mip_cutoff
+
+    @mip_cutoff.setter
+    def mip_cutoff(self, value):
+        self._mip_cutoff = float(value)
+        self.shared_program['u_mip_cutoff'] = self._mip_cutoff
+        self.update()
+
+    @property
+    def minip_cutoff(self):
+        return self._minip_cutoff
+
+    @minip_cutoff.setter
+    def minip_cutoff(self, value):
+        self._minip_cutoff = float(value)
+        self.shared_program['u_minip_cutoff'] = self._minip_cutoff
         self.update()
 
     def _create_vertex_data(self):
