2bafcbe2788be1a40f3c489a560cc1906cdd3e9d
Lorenzo Gaifas <brisvag@gmail.com>
2022-04-21 18:21:55 +0200

    moved example to separate file

--- examples/scene/image.py
+++ examples/scene/image.py
@@ -13,12 +13,9 @@ Simple use of SceneCanvas to display an Image.
 """
 
 import sys
-from itertools import cycle
 from vispy import scene
 from vispy import app
 from vispy.io import load_data_file, read_png
-from scipy.signal.windows import gaussian
-import numpy as np
 
 canvas = scene.SceneCanvas(keys='interactive')
 canvas.size = 800, 600
@@ -31,18 +28,8 @@ view = canvas.central_widget.add_view()
 img_data = read_png(load_data_file('mona_lisa/mona_lisa_sm.png'))
 interpolation = 'nearest'
 
-gaussian_window = gaussian(10, 5)
-gaussian_kernel = np.outer(gaussian_window, gaussian_window)
-
-h_prewitt_kernel = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
-v_prewitt_kernel = h_prewitt_kernel.T
-
-kernels = cycle([gaussian_kernel, h_prewitt_kernel, v_prewitt_kernel])
-
-image = scene.visuals.Image(
-    img_data, interpolation=interpolation, parent=view.scene,
-    method='subdivide', custom_kernel=next(kernels)
-)
+image = scene.visuals.Image(img_data, interpolation=interpolation,
+                            parent=view.scene, method='subdivide')
 
 canvas.title = 'Spatial Filtering using %s Filter' % interpolation
 
@@ -56,20 +43,23 @@ view.camera.zoom(0.1, (250, 200))
 # get interpolation functions from Image
 names = image.interpolation_functions
 names = sorted(names)
+act = 17
 
 
 # Implement key presses
 @canvas.events.key_press.connect
 def on_key_press(event):
+    global act
     if event.key in ['Left', 'Right']:
-        step = 1 if event.key == 'Right' else -1
-        idx = (names.index(image.interpolation) + step) % len(names)
-        interpolation = names[idx]
+        if event.key == 'Right':
+            step = 1
+        else:
+            step = -1
+        act = (act + step) % len(names)
+        interpolation = names[act]
         image.interpolation = interpolation
         canvas.title = 'Spatial Filtering using %s Filter' % interpolation
         canvas.update()
-    elif event.key == 'k':
-        image.custom_kernel = next(kernels)
 
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
