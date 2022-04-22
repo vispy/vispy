43a3a2d4d02ddaf25d9ad6ac1fb90a4ff44e2af2
Lorenzo Gaifas <brisvag@gmail.com>
2022-04-22 12:49:25 +0200

    tests (broken)

--- vispy/visuals/tests/test_image.py
+++ vispy/visuals/tests/test_image.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from unittest import mock
 
-from vispy.scene.visuals import Image
+from vispy.scene import Image, PanZoomCamera
 from vispy.testing import (requires_application, TestingCanvas,
                            run_tests_if_main, IS_CI)
 from vispy.testing.image_tester import assert_image_approved, downsample
@@ -296,5 +296,56 @@ def test_change_clim_float(dtype, init_clim):
 
         assert np.allclose(rendered1, rendered2)
 
+
+@requires_application()
+def test_image_interpolation():
+    """Test different interpolations"""
+    size = (80, 80)
+    data = np.array([[0, 1]], dtype=int)
+    left = (0, 40)
+    right = (79, 40)
+    center_left = (40, 40)
+    center_right = (41, 40)
+    white = (255, 255, 255, 255)
+    black = (255, 255, 255, 255)
+    gray = (130, 130, 130, 255)
+
+    with TestingCanvas(size=size[::-1], bgcolor="w") as c:
+        view = c.central_widget.add_view()
+        view.camera = PanZoomCamera((0, 0, 2, 1))
+        image = Image(data=data, cmap='grays',
+                      parent=view.scene)
+
+        image.interpolation = 'nearest'
+        render = c.render()
+        assert np.allclose(render[left], white)
+        assert np.allclose(render[right], black)
+        assert np.allclose(render[center_left], white)
+        assert np.allclose(render[center_right], black)
+
+        image.interpolation = 'bilinear'
+        render = c.render()
+        assert np.allclose(render[left], white)
+        assert np.allclose(render[right], black)
+        assert np.allclose(render[center_left], gray)
+
+        image.interpolation = 'custom'
+        image.custom_kernel = np.array([[0]])  # no sampling
+        render = c.render()
+        assert np.allclose(render[left], white)
+        assert np.allclose(render[right], white)
+        assert np.allclose(render[center_left], white)
+
+        image.custom_kernel = np.array([[1]])  # same as linear
+        render = c.render()
+        assert np.allclose(render[left], white)
+        assert np.allclose(render[right], black)
+        assert np.allclose(render[center_left], gray)
+
+        image.custom_kernel = np.array([[10, 1]])  # sample more on the left
+        render = c.render()
+        # assert np.allclose(render[left], white)
+        # assert np.allclose(render[right], black)
+        # assert np.allclose(render[center_left], gray)
 
 run_tests_if_main()
