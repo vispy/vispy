8e42c799e885bfb7656cf2828ddd46d300768689
Paddy Harrison <pj.harrison31@gmail.com>
2022-09-11 13:32:36 +0200

    update test

--- vispy/scene/cameras/tests/test_cameras.py
+++ vispy/scene/cameras/tests/test_cameras.py
@@ -13,7 +13,7 @@ from vispy.testing import run_tests_if_main
     "elevation, azimuth, roll, expected",
     [
         [0, 0, 0, np.eye(4)],
-        [90, 0, 0, [[1, 0, 0, 0], [0, 0, 1, 0], [0, -1, 0, 0], [0, 0, 0, 1]]],
+        [90, 0, 0, [[1, 0, 0, 0], [0, 0, -1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]],
         [0, 90, 0, [[0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]],
         [0, 0, 90, [[0, 0, -1, 0], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1]]],
     ],
@@ -21,7 +21,7 @@ from vispy.testing import run_tests_if_main
 def test_turntable_camera_transform(elevation, azimuth, roll, expected):
     camera = TurntableCamera(elevation=elevation, azimuth=azimuth, roll=roll)
     matrix = camera._get_rotation_tr()
-    np.testing.assert_allclose(matrix, expected, atol=1e-8)
+    np.testing.assert_allclose(matrix, expected, atol=1e-5)
 
 
 run_tests_if_main()
