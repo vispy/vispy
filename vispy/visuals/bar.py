7a21064e41fb8c3c4e9ae06dcfc4b9f7838e4897
Bliss3d <joel_oelsner@fastmail.fm>
2022-10-05 12:43:47 +0200

    flake8, requested changes and swaped h and v

--- vispy/visuals/bar.py
+++ vispy/visuals/bar.py
@@ -26,39 +26,50 @@ class BarVisual(MeshVisual):
     color : instance of Color
         Color of the bar.
     orientation : {'h', 'v'}
-        Orientation of the bars - 'h' is default
+        Orientation of the bars - 'v' is default 
+        'v' : |
+        'h' : -
     color_array : array-like
         [[1, 0, 0], [0, 1, 0], [0, 0, 1]] exactly one rgb array for each bar. This would be for 3 Bars 
     """
 
-    def __init__(self, height, bottom=None, width=0.8, shift=0.0, color='w', orientation='h', color_array=None):
+    def __init__(self, height, bottom=None, width=0.8, shift=0.0, color='w', orientation='v', color_array=None):
         if bottom is None:
             bottom = np.zeros(height.shape[0])
 
         if height.shape != bottom.shape:
-            raise ValueError("Height and Bottom must be same shape: Height: " + height.shape + " Bottom: " + bottom.shape)
+            raise ValueError("Height and Bottom must be same shape: Height: " + height.shape + " Bottom: "
+                             + bottom.shape)
         
         rr, tris = calc_vertices(height, bottom, width, shift, orientation)
 
-        if color_array is not None and color_array.shape[0] == height.shape[0] and color_array.shape[1] == 3:
-            MeshVisual.__init__(self, rr, tris, face_colors=np.repeat(color_array, 2, axis=0))
-        else:
-            MeshVisual.__init__(self, rr, tris, color=color)
+        if color_array is not None:
+            if color_array.shape[0] == height.shape[0] and color_array.shape[1] == 3:
+                color_array = np.repeat(color_array, 2, axis=0)
+            else:
+                raise ValueError('color_array needs to have the same length (' + color_array.shape[0] + ') as height ('
+                                 + height.shape[0] + ') and 3 rows: ' + color_array.shape)
 
-    def update_data(self, height, bottom=None, width=0.8, shift=0.0, color='w', orientation='h', color_array=None):
+        MeshVisual.__init__(self, rr, tris, color=color, face_colors=color_array)
+
+    def update_data(self, height, bottom=None, width=0.8, shift=0.0, color='w', orientation='v', color_array=None):
         if bottom is None:
             bottom = np.zeros(height.shape[0])
 
         if height.shape != bottom.shape:
-            raise ValueError("Height and Bottom must be same shape: Height: " + height.shape + " Bottom: " + bottom.shape)
+            raise ValueError("Height and Bottom must be same shape: Height: " + height.shape + " Bottom: "
+                             + bottom.shape)
 
         rr, tris = calc_vertices(height, bottom, width, shift, orientation)
 
-        if color_array is not None and color_array.shape[0] == height.shape[0] and color_array.shape[1] == 3:
-            MeshVisual.set_data(self, rr, tris, face_colors=np.repeat(color_array, 2, axis=0))
-        else:
-            MeshVisual.set_data(self, rr, tris, color=color)
-
+        if color_array is not None:
+            if color_array.shape[0] == height.shape[0] and color_array.shape[1] == 3:
+                color_array = np.repeat(color_array, 2, axis=0)
+            else:
+                raise ValueError('color_array needs to have the same length (' + color_array.shape[0] + ') as height ('
+                                 + height.shape[0] + ') and 3 rows: ' + color_array.shape)
+            
+        MeshVisual.set_data(self, rr, tris, color=color, face_colors=color_array)
 
 
 def calc_vertices(height, bottom, width, shift, orientation):
@@ -80,11 +91,11 @@ def calc_vertices(height, bottom, width, shift, orientation):
 
     vertices = np.tile(vertices, (height.shape[0], 1))
 
-    if orientation == 'h':
+    if orientation == 'v':
         vertices[:, 0] = vertices[:, 0] + stack_n_x2[:, 0].repeat(2)
         vertices[:, 1] = y_position
 
-    elif orientation == 'v':
+    elif orientation == 'h':
         vertices[:, 1] = vertices[:, 0] + stack_n_x2[:, 0].repeat(2)
         vertices[:, 0] = y_position
     else:
