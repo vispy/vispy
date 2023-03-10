d297de7adc9ff403d7f2010ba9d45d237736d1ad
Lorenzo Gaifas <brisvag@gmail.com>
2023-03-10 10:12:49 +0100

    add texture and face colors to the example and make interactive

--- examples/scene/instanced_mesh.py
+++ examples/scene/instanced_mesh.py
@@ -11,41 +11,55 @@ Shading a Mesh
 Show mesh filter usage for shading (lighting) a mesh and displaying a wireframe.
 """
 
+from itertools import cycle
+
 import numpy as np
 from scipy.spatial.transform import Rotation
 from vispy import app, scene, use
-from vispy.io import read_mesh, load_data_file
+from vispy.io import imread, load_data_file, read_mesh
 from vispy.scene.visuals import InstancedMesh
-from vispy.visuals.filters import InstancedShadingFilter, WireframeFilter
+from vispy.visuals.filters import InstancedShadingFilter, WireframeFilter, TextureFilter
 
 # needed for instanced rendering to work
 use(gl='gl+')
 
 
-triceratops = load_data_file('orig/triceratops.obj.gz')
-vertices, faces, normals, texcoords = read_mesh(triceratops)
+mesh_path = load_data_file('spot/spot.obj.gz')
+texture_path = load_data_file('spot/spot.png')
+vertices, faces, normals, texcoords = read_mesh(mesh_path)
+texture = np.flipud(imread(texture_path))
 
 canvas = scene.SceneCanvas(keys='interactive', bgcolor='white', show=True)
 view = canvas.central_widget.add_view()
 
 view.camera = 'arcball'
-view.camera.depth_value = 1e3
+view.camera.depth_value = 10 * (vertices.max() - vertices.min())
 
 n_instances = 100
+
+instance_colors = np.random.rand(n_instances, 3)
+instance_positions = (np.random.rand(n_instances, 3) - 0.5) * 10
+face_colors = np.random.rand(len(faces), 3)
+instance_transforms = Rotation.random(n_instances).as_matrix()
+
 # Create a colored `MeshVisual`.
 mesh = InstancedMesh(
     vertices,
     faces,
-    instance_colors=np.random.rand(n_instances, 3),
-    instance_positions=(np.random.rand(n_instances, 3) - 0.5) * 10,
-    instance_transforms=Rotation.random(n_instances).as_matrix(),
+    instance_colors=instance_colors,
+    face_colors=face_colors,
+    instance_positions=instance_positions,
+    instance_transforms=instance_transforms,
     parent=view.scene,
 )
 
+
 wireframe_filter = WireframeFilter(width=1)
-shading_filter = InstancedShadingFilter('smooth', shininess=100)
+shading_filter = InstancedShadingFilter('smooth', shininess=1)
+texture_filter = TextureFilter(texture, texcoords)
 mesh.attach(wireframe_filter)
 mesh.attach(shading_filter)
+mesh.attach(texture_filter)
 
 
 def attach_headlight(view):
@@ -60,6 +74,34 @@ def attach_headlight(view):
 
 
 attach_headlight(view)
+
+
+shading_cycle = cycle(['flat', None, 'smooth'])
+color_cycle = cycle([None, instance_colors])
+face_color_cycle = cycle([None, face_colors])
+
+
+@canvas.events.key_press.connect
+def on_key_press(event):
+    if event.key == "t":
+        texture_filter.enabled = not texture_filter.enabled
+        canvas.update()
+    if event.key == 's':
+        shading_filter.shading = next(shading_cycle)
+        canvas.update()
+    if event.key == 'c':
+        mesh.instance_colors = next(color_cycle)
+        canvas.update()
+    if event.key == 'f':
+        mesh.set_data(
+            vertices=vertices,
+            faces=faces,
+            face_colors=next(face_color_cycle),
+        )
+        canvas.update()
+    if event.key == 'w':
+        wireframe_filter.enabled = not wireframe_filter.enabled
+        canvas.update()
 
 
 if __name__ == "__main__":
