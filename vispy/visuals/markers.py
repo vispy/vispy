bb924a7029c3aaa42605c1bffd84ad0ec5b83a6a
Grzegorz Bokota <bokota+github@gmail.com>
2023-09-28 09:46:57 +0200

    linear markers validatoe

--- vispy/visuals/markers.py
+++ vispy/visuals/markers.py
@@ -617,7 +617,7 @@ class MarkersVisual(Visual):
                 raise ValueError('edge_width_rel cannot be negative')
 
         if symbol is not None:
-            if not np.all(np.isin(np.asarray(symbol), self.symbols)):
+            if not all(x in self._symbol_shader_values for x in set(np.asarray(symbol))):
                 raise ValueError(f'symbols must one of {self.symbols}')
 
         edge_color = ColorArray(edge_color).rgba
