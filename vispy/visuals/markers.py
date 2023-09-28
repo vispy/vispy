9c7c0813b21535ffdbafbf218e9dfb2843c87be4
Grzegorz Bokota <bokota+github@gmail.com>
2023-09-28 10:33:14 +0200

    fix name

--- vispy/visuals/markers.py
+++ vispy/visuals/markers.py
@@ -618,7 +618,7 @@ class MarkersVisual(Visual):
                 raise ValueError('edge_width_rel cannot be negative')
 
         if symbol is not None:
-            if not all(x in self._symbol_shader_values for x in set(x if isinstance(x, Iterable) else [x])):
+            if not all(x in self._symbol_shader_values for x in set(symbol if isinstance(symbol, Iterable) else [symbol])):
                 raise ValueError(f'symbols must one of {self.symbols}')
 
         edge_color = ColorArray(edge_color).rgba
