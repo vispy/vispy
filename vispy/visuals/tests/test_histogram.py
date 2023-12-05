dc457ed592bf743117616ce6acb2d30dbb619edd
Talley Lambert <talley.lambert@gmail.com>
2023-12-05 15:34:27 -0500

    update test

--- vispy/visuals/tests/test_histogram.py
+++ vispy/visuals/tests/test_histogram.py
@@ -3,8 +3,7 @@ import numpy as np
 
 from vispy.visuals.transforms import STTransform
 from vispy.scene.visuals import Histogram
-from vispy.testing import (requires_application, TestingCanvas,
-                           run_tests_if_main)
+from vispy.testing import requires_application, TestingCanvas, run_tests_if_main
 from vispy.testing.image_tester import assert_image_approved
 
 
@@ -12,12 +11,11 @@ from vispy.testing.image_tester import assert_image_approved
 def test_histogram():
     """Test histogram visual"""
     size = (200, 100)
-    with TestingCanvas(size=size, bgcolor='w') as c:
+    with TestingCanvas(size=size, bgcolor="w") as c:
         np.random.seed(2397)
-        data = np.random.normal(size=100)
-        hist = Histogram(data, bins=20, color='k', parent=c.scene)
-        hist.transform = STTransform((size[0] // 10, -size[1] // 20, 1),
-                                     (100, size[1]))
+        hist = Histogram(bins=20, color="k", parent=c.scene)
+        hist.set_raw_data(np.random.normal(size=100))
+        hist.transform = STTransform((size[0] // 10, -size[1] // 20, 1), (100, size[1]))
         assert_image_approved(c.render(), "visuals/histogram.png")
 
 
