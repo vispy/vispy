8994c4506154c0cb90f6d977e944479d3d3da281
Lorenzo Gaifas <brisvag@gmail.com>
2025-04-15 11:05:44 +0200

    add simple test for gridlines

---
+++ vispy/visuals/tests/test_gridlines.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+
+"""
+Tests for GridLines
+"""
+
+import numpy as np
+
+from vispy.scene import GridLines, STTransform
+from vispy.testing import (requires_application, TestingCanvas,
+                           run_tests_if_main)
+
+
+@requires_application()
+def test_gridlines():
+    with TestingCanvas(size=(80, 80)) as c:
+        grid = GridLines(parent=c.scene)
+        grid.transform = STTransform(translate=(40, 40))
+        render = c.render()
+        np.testing.assert_array_equal(render[40, 40], (151, 151, 151, 255))
+        np.testing.assert_array_equal(render[41, 41], (0, 0, 0, 255))
+
+
+run_tests_if_main()
