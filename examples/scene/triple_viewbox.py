ce4c3be8d0034e4a635ac8f99da9e8344cfbbd55
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2025-05-14 13:58:27 +0200

    empty viewboxes

--- examples/scene/triple_viewbox.py
+++ examples/scene/triple_viewbox.py
@@ -19,10 +19,10 @@ from vispy import scene
 
 # Create canvas
 canvas = scene.SceneCanvas(size=(800, 600), show=True, keys='interactive')
-grid = canvas.central_widget.add_grid(spacing=(50,150))
+grid = canvas.central_widget.add_grid(spacing=(50, 150))
 
 # Create two ViewBoxes, place side-by-side
-vb1 = grid.add_view(name='vb1', border_color='yellow', row=0, col=0, row_span=1, col_span=2)
+vb1 = grid.add_view(name='vb1', border_color='green', row=0, col=0, row_span=1, col_span=2)
 # First ViewBox uses a 2D pan/zoom camera
 vb1.camera = 'panzoom'
 
@@ -31,32 +31,9 @@ vb2 = grid.add_view(name='vb2', border_color='yellow', row=0, col=0, col_span=1)
 vb2.parent = canvas.scene
 vb2.camera = scene.TurntableCamera(elevation=30, azimuth=30, up='+y')
 # Third ViewBox uses a 3D perspective camera
-vb3 = grid.add_view(name='vb3', border_color='yellow', row=0, col=1)
+vb3 = grid.add_view(name='vb3', border_color='red', row=0, col=1)
 vb3.parent = canvas.scene
 vb3.camera = scene.TurntableCamera(elevation=30, azimuth=30, up='+y')
-
-# Now add visuals to the viewboxes.
-#
-
-# First a plot line:
-N = 1000
-color = np.ones((N, 4), dtype=np.float32)
-color[:, 0] = np.linspace(0, 1, N)
-color[:, 1] = color[::-1, 0]
-
-pos = np.empty((N, 2), np.float32)
-pos[:, 0] = np.linspace(-1., 1., N)
-pos[:, 1] = np.random.normal(0.0, 0.5, size=N)
-pos[:20, 1] = -0.5  # So we can see which side is down
-
-# make a single plot line and display in both viewboxes
-line1 = scene.visuals.Line(pos=pos.copy(), color=color, method='gl',
-                           antialias=False, name='line1', parent=vb1.scene)
-line2 = scene.visuals.Line(pos=pos.copy(), color=color, method='gl',
-                           antialias=False, name='line1', parent=vb2.scene)
-line3 = scene.visuals.Line(pos=pos.copy(), color=color, method='gl',
-                           antialias=False, name='line1', parent=vb3.scene)
-
 
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
