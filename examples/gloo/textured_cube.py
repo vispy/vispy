fe17fc740552a38cdd54f482d8b9d8ca1989c9d6
Wouter-Michiel Vierdag <w-mv@hotmail.com>
2026-08-22 01:47:55 +0200

    docs: fix docs textured_cube example

--- examples/gloo/textured_cube.py
+++ examples/gloo/textured_cube.py
@@ -50,7 +50,7 @@ void main()
 def checkerboard(grid_num=8, grid_size=32):
     row_even = grid_num // 2 * [0, 1]
     row_odd = grid_num // 2 * [1, 0]
-    Z = np.row_stack(grid_num // 2 * (row_even, row_odd)).astype(np.uint8)
+    Z = np.vstack(grid_num // 2 * (row_even, row_odd)).astype(np.uint8)
     return 255 * Z.repeat(grid_size, axis=0).repeat(grid_size, axis=1)
 
 
